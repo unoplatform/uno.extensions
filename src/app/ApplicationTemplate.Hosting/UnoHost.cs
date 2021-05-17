6ee71ad72b11c21db7aa81716c5b0d1a2a07f1b7
Nick Randolph <nick@builttoroam.com>
2021-05-17 10:31:32 +1000

    Removing eventlogprovider when running as uwp Moved Winui app to partial trust Updated to .net 5.0.203 sdk

--- src/app/ApplicationTemplate.Hosting/UnoHost.cs
+++ src/app/ApplicationTemplate.Hosting/UnoHost.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
+using Microsoft.Extensions.Logging.Debug;
+using Microsoft.Extensions.Logging.EventLog;
 
 namespace ApplicationTemplate.Hosting
 {
@@ -15,7 +20,7 @@ namespace ApplicationTemplate.Hosting
         }
             public static IHost CreateDefaultHost(IServiceConfigurer configurer=null)
         {
-            var builder = new HostBuilder()
+            var builder = Host.CreateDefaultBuilder()
                .ConfigureHostConfiguration(configBuilder =>
                {
                    configBuilder
@@ -29,6 +34,13 @@ namespace ApplicationTemplate.Hosting
                })
                .ConfigureServices((_, services) =>
                {
+//-:cnd:noEmit
+#if WINDOWS_UWP  // We only need to do this on Windows for UWP because of an assumption dotnet makes that every Windows app can access eventlog
+//+:cnd:noEmit
+                   services.RemoveAllIncludeImplementations<EventLogLoggerProvider>();
+//-:cnd:noEmit
+#endif
+//+:cnd:noEmit
                    configurer?.ConfigureServices(services);
                });
                return builder.Build();
@@ -48,6 +60,37 @@ namespace ApplicationTemplate.Hosting
             //        throw new InvalidOperationException("Starting mechanic not specified");
             //    }
             //}
+        }
+    }
+}
+
+
+namespace Microsoft.Extensions.DependencyInjection.Extensions
+{
+    public static class ServiceCollectionDescriptorExtensions
+    {
+        public static IServiceCollection RemoveAllIncludeImplementations<T>(this IServiceCollection collection)
+        {
+            return RemoveAllIncludeImplementations(collection, typeof(T));
+        }
+
+        public static IServiceCollection RemoveAllIncludeImplementations(this IServiceCollection collection, Type serviceType)
+        {
+            if (serviceType == null)
+            {
+                throw new ArgumentNullException(nameof(serviceType));
+            }
+
+            for (int i = collection.Count - 1; i >= 0; i--)
+            {
+                ServiceDescriptor? descriptor = collection[i];
+                if (descriptor.ServiceType == serviceType || descriptor.ImplementationType == serviceType)
+                {
+                    collection.RemoveAt(i);
+                }
+            }
+
+            return collection;
         }
     }
 }
