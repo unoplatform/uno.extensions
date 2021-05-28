59eff1e5902fccbecf40140d7dd95731a2a04e32
Nick Randolph <nick@builttoroam.com>
2021-05-28 15:59:33 +1000

    Tidying up configuration extensions

--- src/extensions/Uno.Extensions.Configuration/IWritableOptions.cs
+++ src/extensions/Uno.Extensions.Configuration/IWritableOptions.cs
@@ -3,10 +3,9 @@ using Microsoft.Extensions.Options;
 
 namespace Uno.Extensions.Configuration
 {
-    public interface IWritableOptions<out T> : IOptionsSnapshot<T> where T : class, new()
+    public interface IWritableOptions<out T> : IOptionsSnapshot<T>
+        where T : class, new()
     {
         void Update(Action<T> applyChanges);
     }
-
-
 }
