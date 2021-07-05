bf47e330529d5776b2fcb26564c24524a33feec8
Nick Randolph <nick@builttoroam.com>
2021-07-05 12:33:57 +1000

    Adding hostbuilder extension for adding serialization

---
+++ src/extensions/Uno.Extensions.Serialization/Content/Ext.Serialization/HostBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Uno.Extensions.Serialization
+{
+    public static class HostBuilderExtensions
+    {
+        public static IHostBuilder UseSerialization(this IHostBuilder hostBuilder)
+        {
+            return hostBuilder?
+                    .ConfigureServices((ctx, s) =>
+                    {
+                        _ = s.AddSystemTextJsonSerialization();
+                    });
+        }
+    }
+}
