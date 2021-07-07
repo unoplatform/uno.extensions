5d464aee55890a1140f5b3945fefecd4cff76352
Nick Randolph <nick@builttoroam.com>
2021-07-07 11:48:53 +1000

    Adding firebase support

--- src/extensions/Uno.Extensions.Http.Firebase/Content/Ext.Firebase/ServiceCollectionExtensions.cs
+++ src/extensions/Uno.Extensions.Http.Firebase/Content/Ext.Firebase/ServiceCollectionExtensions.cs
@@ -13,4 +13,25 @@ namespace Uno.Extensions.Http.Firebase
                 ;
         }
     }
+
+    public static class HttpClientBuilderExtensions
+    {
+
+        public static IHttpClientBuilder AddFirebaseHandler(this IHttpClientBuilder builder)
+        {
+            return builder
+#if __ANDROID__
+                .AddHttpMessageHandler<FirebasePerformanceHandler>()
+#endif
+                ;
+        }
+    }
+
+    public static class EndpointOptionsExtensions
+    {
+        public static bool UseFirebaseHandler(this EndpointOptions options)
+        {
+            return options.FeatureEnabled(nameof(UseFirebaseHandler));
+        }
+    }
 }
