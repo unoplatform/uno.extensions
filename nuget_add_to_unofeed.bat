01db5de551756c527c2b49df2c0f2fd100ce691b
Nick Randolph <nick@builttoroam.com>
2021-08-18 16:58:00 +1000

    Adding script to populate local nuget feed

---
+++ nuget_add_to_unofeed.bat
@@ -0,0 +1,43 @@
+rmdir .\unofeed\uno.extensions.configuration /s /q
+nuget add .\src\extensions\Uno.Extensions.Configuration\bin\Release\Uno.Extensions.Configuration.1.0.0.nupkg -Source .\unofeed
+
+rmdir .\unofeed\uno.extensions.hosting /s /q
+nuget add .\src\extensions\Uno.Extensions.Hosting\bin\Release\Uno.Extensions.Hosting.1.0.0.nupkg -Source .\unofeed
+
+rmdir .\unofeed\uno.extensions.hosting.wasm /s /q
+nuget add .\src\extensions\Uno.Extensions.Hosting.Wasm\bin\Release\Uno.Extensions.Hosting.Wasm.1.0.0.nupkg -Source .\unofeed
+
+rmdir .\unofeed\uno.extensions.http /s /q
+nuget add .\src\extensions\Uno.Extensions.Http\bin\Release\Uno.Extensions.Http.1.0.0.nupkg -Source .\unofeed
+
+rmdir .\unofeed\uno.extensions.http.firebase /s /q
+nuget add .\src\extensions\Uno.Extensions.Http.Firebase\bin\Release\Uno.Extensions.Http.Firebase.1.0.0.nupkg -Source .\unofeed
+
+rmdir .\unofeed\uno.extensions.http.refit /s /q
+nuget add .\src\extensions\Uno.Extensions.Http.Refit\bin\Release\Uno.Extensions.Http.Refit.1.0.0.nupkg -Source .\unofeed
+
+rmdir .\unofeed\uno.extensions.localization /s /q
+nuget add .\src\extensions\Uno.Extensions.Localization\bin\Release\Uno.Extensions.Localization.1.0.0.nupkg -Source .\unofeed
+
+rmdir .\unofeed\uno.extensions.logging /s /q
+nuget add .\src\extensions\Uno.Extensions.Logging\bin\Release\Uno.Extensions.Logging.1.0.0.nupkg -Source .\unofeed
+
+rmdir .\unofeed\uno.extensions.logging.wasm /s /q
+nuget add .\src\extensions\Uno.Extensions.Logging.Wasm\bin\Release\Uno.Extensions.Logging.Wasm.1.0.0.nupkg -Source .\unofeed
+
+rmdir .\unofeed\uno.extensions.logging.serilog /s /q
+nuget add .\src\extensions\Uno.Extensions.Logging.Serilog\bin\Release\Uno.Extensions.Logging.Serilog.1.0.0.nupkg -Source .\unofeed
+
+rmdir .\unofeed\uno.extensions.navigation /s /q
+nuget add .\src\extensions\Uno.Extensions.Navigation\bin\Release\Uno.Extensions.Navigation.1.0.0.nupkg -Source .\unofeed
+
+rmdir .\unofeed\uno.extensions.serialization /s /q
+nuget add .\src\extensions\Uno.Extensions.Serialization\bin\Release\Uno.Extensions.Serialization.1.0.0.nupkg -Source .\unofeed
+
+rmdir .\unofeed\uno.extensions.serialization.http /s /q
+nuget add .\src\extensions\Uno.Extensions.Serialization.Http\bin\Release\Uno.Extensions.Serialization.Http.1.0.0.nupkg -Source .\unofeed
+
+rmdir .\unofeed\uno.extensions.serialization.refit /s /q
+nuget add .\src\extensions\Uno.Extensions.Serialization.Refit\bin\Release\Uno.Extensions.Serialization.Refit.1.0.0.nupkg -Source .\unofeed
+
+
