547967ea35cd5eab698d41fd91956f0d3b44d2c7
Nick Randolph <nick@builttoroam.com>
2021-08-31 17:59:30 +1000

    Adding Path attached property for navigation

--- src/extensions/ExtensionsSampleApp/ExtensionsSampleApp.Shared/MainPage.xaml.cs
+++ src/extensions/ExtensionsSampleApp/ExtensionsSampleApp.Shared/MainPage.xaml.cs
@@ -1,27 +1,17 @@
-﻿using System;
-using CommunityToolkit.Mvvm.DependencyInjection;
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
 using Uno.Extensions.Navigation;
 using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace ExtensionsSampleApp
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage 
     {
         public MainPage()
         {
-            this.InitializeComponent();
-        }
-
-        private void NextPageNavigationRequestClick(object sender, RoutedEventArgs e)
-        {
-            var nav = Ioc.Default.GetService<INavigationService>();
-            nav.Navigate(new NavigationRequest(this, new NavigationRoute(new Uri("SecondPage", UriKind.Relative))));
+            InitializeComponent();
         }
 
         private void NextPageNavigateToViewClick(object sender, RoutedEventArgs e)
