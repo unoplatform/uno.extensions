787cc3b66d2d0a94930ee932928753768e99f4f1
Nick Randolph <nick@builttoroam.com>
2021-09-10 16:08:16 +1000

    Refactoring core navigation

--- src/extensions/Uno.Extensions.Navigation/Adapters/FrameNavigationAdapter.cs
+++ src/extensions/Uno.Extensions.Navigation/Adapters/FrameNavigationAdapter.cs
@@ -16,14 +16,8 @@ using Microsoft.UI.Xaml.Controls;
 
 namespace Uno.Extensions.Navigation.Adapters
 {
-    public interface IAdapterFactory
-    {
-        Type ControlType{get;}
-        INavigationAdapter Create();
-    }
-
-    public record AdapterFactory<TControl, TAdapter>(IServiceProvider Services): IAdapterFactory
-        where TAdapter: INavigationAdapter
+    public record AdapterFactory<TControl, TAdapter>(IServiceProvider Services) : IAdapterFactory
+        where TAdapter : INavigationAdapter
     {
         public Type ControlType => typeof(TControl);
 
@@ -40,12 +34,14 @@ namespace Uno.Extensions.Navigation.Adapters
         private IFrameWrapper Frame => ControlWrapper as IFrameWrapper;
         protected IList<NavigationContext> NavigationContexts { get; } = new List<NavigationContext>();
 
-        public override NavigationContext CurrentContext {
+        public override NavigationContext CurrentContext
+        {
             get
             {
                 return NavigationContexts.LastOrDefault();
             }
-            protected set { } } 
+            protected set { }
+        }
 
         public FrameNavigationAdapter(
             // INavigationService navigation, // Note: Don't pass in - implement INaviationAware instead
@@ -72,110 +68,24 @@ namespace Uno.Extensions.Navigation.Adapters
             }
         }
 
-        //protected override async Task<NavigationContext> AdapterNavigate(NavigationContext context, bool navBackRequired)
-        //{
-        //    var request = context.Request;
-        //    var path = context.Path;
-
-        //    var numberOfPagesToRemove = context.FramesToRemove;
-        //    bool removeCurrentPageFromBackStack = numberOfPagesToRemove > 0;
-        //    if (!context.IsBackNavigation && removeCurrentPageFromBackStack)
-        //    {
-        //        numberOfPagesToRemove--;
-        //    }
-
-        //    while (numberOfPagesToRemove > 0)
-        //    {
-        //        NavigationContexts.RemoveAt(NavigationContexts.Count - (context.IsBackNavigation ? 1 : 2));
-        //        Frame.RemoveLastFromBackStack();
-        //        numberOfPagesToRemove--;
-        //    }
-
-        //    if (context.Path == PreviousViewUri)
-        //    {
-        //        if (navBackRequired)
-        //        {
-        //            // Remove the last context (i.e the current context)
-        //            NavigationContexts.RemoveAt(NavigationContexts.Count - 1);
-        //        }
-
-        //        var vm = await InitializeViewModel(CurrentContext);
-
-        //        if (navBackRequired)
-        //        {
-        //            Frame.Navigate(context, vm);
-
-        //            //var view = Frame.GoBack(context.Data, vm);
-        //            //if (view is INavigationAware navAware)
-        //            //{
-        //            //    navAware.Navigation = Navigation;
-        //            //}
-        //        }
-
-        //        await ((vm as INavigationStart)?.Start(CurrentContext, false) ?? Task.CompletedTask);
-        //    }
-        //    else
-        //    {
-        //        await DoForwardNavigation(context, (ctx, vm) =>
-        //         {
-        //             NavigationContexts.Add(ctx);
-        //             Frame.Navigate(ctx, vm);
-        //             //var view = Frame.Navigate(ctx, ctx.Mapping.View, ctx.Data, vm);
-        //             //if (view is INavigationAware navAware)
-        //             //{
-        //             //    navAware.Navigation = Navigation;
-        //             //}
-
-        //             if (ctx.PathIsRooted)
-        //             {
-        //                 while (NavigationContexts.Count > 1)
-        //                 {
-        //                     NavigationContexts.RemoveAt(0);
-        //                 }
-
-        //                 Frame.ClearBackStack();
-        //             }
-
-        //             if (removeCurrentPageFromBackStack)
-        //             {
-        //                 NavigationContexts.RemoveAt(NavigationContexts.Count - 2);
-        //                 Frame.RemoveLastFromBackStack();
-        //             }
-        //         });
-        //    }
-
-        //    return context;
-        //}
-
-        protected override async Task<object> InitializeViewModel(NavigationContext context, bool navBackRequired)
+        protected override async Task DoBackNavigation(NavigationContext context)
         {
+            NavigationContexts.RemoveAt(NavigationContexts.Count - 1);
 
-            if (navBackRequired)
-            {
-                // Remove the last context (i.e the current context)
-                NavigationContexts.RemoveAt(NavigationContexts.Count - 1);
-            }
+            var currentVM = await CurrentContext.InitializeViewModel();
 
-            var vm = await base.InitializeViewModel(navBackRequired?CurrentContext:context, navBackRequired);
+            ControlWrapper.Navigate(context, true, currentVM);
 
-            if (navBackRequired)
-            {
-                Frame.Navigate(context, navBackRequired, vm);
-            }
+            await ((currentVM as INavigationStart)?.Start(CurrentContext, false) ?? Task.CompletedTask);
 
-            return vm;
         }
 
+        public override bool CanGoBack => NavigationContexts.Count > 1;
 
         protected override void AdapterNavigation(NavigationContext context, object viewModel)
         {
             NavigationContexts.Add(context);
             Frame.Navigate(context, false, viewModel);
-            //var view = Frame.Navigate(ctx, ctx.Mapping.View, ctx.Data, vm);
-            //if (view is INavigationAware navAware)
-            //{
-            //    navAware.Navigation = Navigation;
-            //}
 
             if (context.PathIsRooted)
             {
@@ -187,7 +97,7 @@ namespace Uno.Extensions.Navigation.Adapters
                 Frame.ClearBackStack();
             }
 
-            if (context.FramesToRemove>0)
+            if (context.FramesToRemove > 0)
             {
                 NavigationContexts.RemoveAt(NavigationContexts.Count - 2);
                 Frame.RemoveLastFromBackStack();
