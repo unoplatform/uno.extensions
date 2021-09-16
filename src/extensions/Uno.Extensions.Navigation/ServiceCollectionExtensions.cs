848c40ffcee850b698f81ed922bab5d8f8c51407
Nick Randolph <nick@builttoroam.com>
2021-09-16 11:23:56 +1000

    Renaming

--- src/extensions/Uno.Extensions.Navigation/ServiceCollectionExtensions.cs
+++ src/extensions/Uno.Extensions.Navigation/ServiceCollectionExtensions.cs
@@ -23,9 +23,9 @@ public static class ServiceCollectionExtensions
 
         return services
                     // Register the control navigation implementation (simple or stack based navigation)
-                    .AddTransient<IStackNavigation<Frame>, FrameStackNavigation>()
-                    .AddTransient<ISimpleNavigation<TabView>, TabSimpleNavigation>()
-                    .AddTransient<ISimpleNavigation<ContentControl>, ContentControlSimpleNavigation>()
+                    .AddTransient<IStackViewManager<Frame>, FrameStackNavigation>()
+                    .AddTransient<IViewManager<TabView>, TabSimpleNavigation>()
+                    .AddTransient<IViewManager<ContentControl>, ContentControlSimpleNavigation>()
 
                     // Register the adapter for each control type
                     .AddAdapter<Frame, StackNavigationAdapter<Frame>>()
