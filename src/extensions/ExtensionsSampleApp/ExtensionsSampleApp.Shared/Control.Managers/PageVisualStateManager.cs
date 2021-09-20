402b4443e89d88677685bf339c896c0bfc260b88
Nick Randolph <nick@builttoroam.com>
2021-09-20 11:47:08 +1000

    Added grid visibility and visual state implementations of view manager

---
+++ src/extensions/ExtensionsSampleApp/ExtensionsSampleApp.Shared/Control.Managers/PageVisualStateManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Uno.Extensions.Navigation;
+using Uno.Extensions.Navigation.Controls;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace ExtensionsSampleApp.Control.Managers
+{
+    public class PageVisualStateManager : BaseControlManager<Page>
+    {
+        public PageVisualStateManager(INavigationService navigation, RegionControlProvider controlProvider) : base(navigation, controlProvider.RegionControl as Page)
+        {
+        }
+
+        protected override object InternalShow(string path, Type view, object data, object viewModel)
+        {
+            VisualStateManager.GoToState(Control, path, true);
+            return null;
+        }
+    }
+}
