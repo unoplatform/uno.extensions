980333f038e4a3c7623b1b64d780fd85324c6a82
Nick Randolph <nick@builttoroam.com>
2021-09-20 15:36:22 +1000

    Removing generic type form base region manager

--- src/extensions/Uno.Extensions.Navigation/Regions/Managers/SimpleRegionManager.cs
+++ src/extensions/Uno.Extensions.Navigation/Regions/Managers/SimpleRegionManager.cs
@@ -1,23 +1,25 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Uno.Extensions.Navigation.Controls;
+﻿using Uno.Extensions.Navigation.Controls;
 using Uno.Extensions.Navigation.Dialogs;
 
 namespace Uno.Extensions.Navigation.Regions.Managers;
 
-public class SimpleRegionManager<TControl> : BaseRegionManager<TControl>
+public class SimpleRegionManager<TControl> : BaseRegionManager
     where TControl : IViewManager
 {
+    private TControl Control { get; }
+
     private NavigationContext currentContext;
 
     protected override NavigationContext CurrentContext => currentContext;
 
-    public SimpleRegionManager(INavigationService navigation, TControl control, IDialogFactory dialogFactory) : base(navigation, control, dialogFactory)
+    public SimpleRegionManager(INavigationService navigation, IDialogFactory dialogFactory, TControl control) : base(navigation, dialogFactory)
     {
+        Control = control;
     }
 
     protected override void RegionNavigate(NavigationContext context, object viewModel)
     {
         currentContext = context;
-        ControlWrapper.Show(context.Path, context.Mapping?.View, context.Data, viewModel);
+        Control.Show(context.Path, context.Mapping?.View, context.Data, viewModel);
     }
 }
