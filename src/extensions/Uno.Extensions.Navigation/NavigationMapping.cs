1c356f8d80c99c20181394be2f6ee7ac2bae8528
Nick Randolph <nick@builttoroam.com>
2021-09-21 12:17:33 +1000

    Improving default mappings

--- src/extensions/Uno.Extensions.Navigation/NavigationMapping.cs
+++ src/extensions/Uno.Extensions.Navigation/NavigationMapping.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 #if WINDOWS_UWP || UNO_UWP_COMPATIBILITY
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 #else
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 #endif
 
@@ -14,6 +17,12 @@ namespace Uno.Extensions.Navigation;
 public class NavigationMapping : INavigationMapping
 {
     public bool ReturnImplicitMapping { get; set; } = true;
+
+    public string[] ViewSuffixes { get; set; } = new[] { "View", "Page", "Control" };
+
+    public string[] ViewModelSuffixes { get; set; } = new[] { "ViewModel", "VM" };
+
+    private IDictionary<string, Type> loadedTypes;
 
     private IDictionary<string, NavigationMap> Mappings { get; } = new Dictionary<string, NavigationMap>();
 
@@ -24,17 +33,24 @@ public class NavigationMapping : INavigationMapping
 
     public NavigationMap LookupByPath(string path)
     {
-        return Mappings.TryGetValue(path, out var map) ? map : DefaultForViewTypePath(path);
+        if (path == NavigationConstants.RelativePath.BackPath ||
+            path == NavigationConstants.RelativePath.Current ||
+            path == NavigationConstants.RelativePath.Nested)
+        {
+            return null;
+        }
+
+        return Mappings.TryGetValue(path, out var map) ? map : DefaultMapping(path: path);
     }
 
     public NavigationMap LookupByViewModel(Type viewModelType)
     {
-        return (Mappings.FirstOrDefault(x => x.Value.ViewModel == viewModelType).Value) ?? DefaultForType(viewModelType, false, true);
+        return (Mappings.FirstOrDefault(x => x.Value.ViewModel == viewModelType).Value) ?? DefaultMapping(viewModel: viewModelType);
     }
 
     public NavigationMap LookupByView(Type viewType)
     {
-        return (Mappings.FirstOrDefault(x => x.Value.View == viewType).Value) ?? DefaultForType(viewType, true, false);
+        return (Mappings.FirstOrDefault(x => x.Value.View == viewType).Value) ?? DefaultMapping(view: viewType);
     }
 
     public NavigationMap LookupByData(Type dataType)
@@ -42,18 +58,105 @@ public class NavigationMapping : INavigationMapping
         return Mappings.First(x => x.Value.Data == dataType).Value;
     }
 
-    private NavigationMap DefaultForType(Type type, bool isView, bool isViewModel)
+    private NavigationMap DefaultMapping(string path = null, Type view = null, Type viewModel = null)
     {
-        return ReturnImplicitMapping ? new NavigationMap(type.Name, isView ? type : null, isViewModel ? type : null, null) : null;
+        if (!ReturnImplicitMapping)
+        {
+            return null;
+        }
+
+        if (string.IsNullOrWhiteSpace(path))
+        {
+            path = PathFromTypes(view, viewModel);
+        }
+
+        if (view is null)
+        {
+            view = TypeFromPath(path, true, ViewSuffixes, type => type.IsSubclassOf(typeof(FrameworkElement)));
+        }
+
+        if (viewModel is null)
+        {
+            viewModel = TypeFromPath(path, false, ViewModelSuffixes);
+        }
+
+        if (!string.IsNullOrWhiteSpace(path))
+        {
+            var defaultMap = new NavigationMap(path, view, viewModel, null);
+            Mappings[path] = defaultMap;
+            return defaultMap;
+        }
+
+        return null;
     }
 
-    private NavigationMap DefaultForViewTypePath(string path)
+    private Type TypeFromPath(string path, bool allowMatchExact, IEnumerable<string> suffixes, Func<Type, bool> condition = null)
     {
-        var viewType = (from asb in AppDomain.CurrentDomain.GetAssemblies()
-                        from t in asb.GetTypes()
-                        where t.Name == path
-                        select t).FirstOrDefault();
+        if (allowMatchExact && LoadedTypes.TryGetValue(path, out var type))
+        {
+            return type;
+        }
 
-        return ReturnImplicitMapping ? new NavigationMap(path, viewType, null, null) : null;
+        foreach (var suffix in suffixes)
+        {
+            if (LoadedTypes.TryGetValue($"{path}{suffix}", out type))
+            {
+                if (condition?.Invoke(type) ?? true)
+                {
+                    return type;
+                }
+            }
+        }
+
+        return null;
     }
+
+    private string PathFromTypes(Type view, Type viewModel)
+    {
+        var path = ViewTypeToPath(view);
+        if (!string.IsNullOrWhiteSpace(path))
+        {
+            return path;
+        }
+
+        return ViewModelTypeToPath(viewModel);
+    }
+
+    private string ViewTypeToPath(Type view)
+    {
+        return TypeToPath(view, ViewSuffixes);
+    }
+
+    private string ViewModelTypeToPath(Type view)
+    {
+        return TypeToPath(view, ViewModelSuffixes);
+    }
+
+    private string TypeToPath(Type view, IEnumerable<string> suffixes)
+    {
+        var path = view?.Name + string.Empty;
+        foreach (var item in suffixes)
+        {
+            path = path.TrimEnd(item, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        return path;
+    }
+
+    public IDictionary<string, Type> LoadedTypes
+    {
+        get
+        {
+            if (loadedTypes is null)
+            {
+                loadedTypes = (from asb in AppDomain.CurrentDomain.GetAssemblies()
+                               from t in asb.GetTypes()
+                               where t.IsClass
+                               select new { t.Name, Type = t }).ToDictionaryDistinct(x => x.Name, x => x.Type);
+            }
+
+            return loadedTypes;
+        }
+    }
+
 }
