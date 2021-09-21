49e6a62117393bf2b694d9abbb0339e823c6ef17
Nick Randolph <nick@builttoroam.com>
2021-09-21 12:48:21 +1000

    Adding logging to mapping

--- src/extensions/Uno.Extensions.Logging/Content/Ext.Logging/LoggerExtensions.cs
+++ src/extensions/Uno.Extensions.Logging/Content/Ext.Logging/LoggerExtensions.cs
@@ -6,19 +6,69 @@ namespace Uno.Extensions.Logging
 {
     public static class LoggerExtensions
     {
-        private static bool? _debugLogEnabled;
+        private static bool? _logEnabledDebug;
+        private static bool? _logEnabledTrace;
+        private static bool? _logEnabledInformation;
+        private static bool? _logEnabledWarning;
+        private static bool? _logEnabledError;
+        private static bool? _logEnabledCritical;
 
         public static void LazyLogDebug(this ILogger logger, Func<string> message, [CallerMemberName] string callerMethod = null)
         {
-            if (!_debugLogEnabled.HasValue)
-            {
-                _debugLogEnabled = logger?.IsEnabled(LogLevel.Debug);
-            }
-
-            if (_debugLogEnabled.Value)
+            if (Enabled(ref _logEnabledDebug, LogLevel.Debug, logger))
             {
                 logger?.LogDebug($"{callerMethod} - {message?.Invoke()}");
             }
+        }
+
+        public static void LazyLogTrace(this ILogger logger, Func<string> message, [CallerMemberName] string callerMethod = null)
+        {
+            if (Enabled(ref _logEnabledTrace, LogLevel.Trace, logger))
+            {
+                logger?.LogTrace($"{callerMethod} - {message?.Invoke()}");
+            }
+        }
+
+        public static void LazyLogInformation(this ILogger logger, Func<string> message, [CallerMemberName] string callerMethod = null)
+        {
+            if (Enabled(ref _logEnabledInformation, LogLevel.Information, logger))
+            {
+                logger?.LogInformation($"{callerMethod} - {message?.Invoke()}");
+            }
+        }
+
+        public static void LazyLogWarning(this ILogger logger, Func<string> message, [CallerMemberName] string callerMethod = null)
+        {
+            if (Enabled(ref _logEnabledWarning, LogLevel.Warning, logger))
+            {
+                logger?.LogWarning($"{callerMethod} - {message?.Invoke()}");
+            }
+        }
+
+        public static void LazyLogError(this ILogger logger, Func<string> message, [CallerMemberName] string callerMethod = null)
+        {
+            if (Enabled(ref _logEnabledError, LogLevel.Error, logger))
+            {
+                logger?.LogError($"{callerMethod} - {message?.Invoke()}");
+            }
+        }
+
+        public static void LazyLogCritical(this ILogger logger, Func<string> message, [CallerMemberName] string callerMethod = null)
+        {
+            if (Enabled(ref _logEnabledCritical, LogLevel.Critical, logger))
+            {
+                logger?.LogCritical($"{callerMethod} - {message?.Invoke()}");
+            }
+        }
+
+        private static bool Enabled(ref bool? enabled, LogLevel level, ILogger logger)
+        {
+            if (!enabled.HasValue)
+            {
+                enabled = logger?.IsEnabled(level);
+            }
+
+            return enabled ?? false;
         }
     }
 }
