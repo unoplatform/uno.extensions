026d879384bdde55125bc7a4aa659694f8a52064
Nick Randolph <nick@builttoroam.com>
2021-09-27 22:49:43 +1000

    Tidying up nav manager

--- src/extensions/Uno.Extensions.Navigation/INavigationManager.cs
+++ src/extensions/Uno.Extensions.Navigation/INavigationManager.cs
@@ -5,8 +5,4 @@ public interface INavigationManager
     INavigationRegionContainer Root { get; }
 
     INavigationRegionContainer CreateRegion(object control, object contentControl);
-
-    //INavigationService AddRegion(INavigationService parentRegion, string regionName, object control, INavigationService existingRegion);
-
-    //void RemoveRegion(INavigationService region);
 }
