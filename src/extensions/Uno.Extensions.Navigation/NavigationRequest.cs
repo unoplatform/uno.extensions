f2b4872e24a1f4f7f62b093ffbed07fdecd985ec
Nick Randolph <nick@builttoroam.com>
2021-10-04 14:48:22 +1100

    NavigationRoute -> Route

--- src/extensions/Uno.Extensions.Navigation/NavigationRequest.cs
+++ src/extensions/Uno.Extensions.Navigation/NavigationRequest.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 namespace Uno.Extensions.Navigation;
 
 #pragma warning disable SA1313 // Parameter names should begin with lower-case letter
-public record NavigationRequest(object Sender, NavigationRoute Route, CancellationToken? Cancellation = default, Type Result = null)
+public record NavigationRequest(object Sender, Route Route, CancellationToken? Cancellation = default, Type Result = null)
 #pragma warning restore SA1313 // Parameter names should begin with lower-case letter
 {
     public string FirstRouteSegment
