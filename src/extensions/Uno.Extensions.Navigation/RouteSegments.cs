1175a35ce968f7ef5d8a8d79f9fb1099f6f79e24
Nick Randolph <nick@builttoroam.com>
2021-10-04 14:49:59 +1100

    RouteSegments

---
+++ src/extensions/Uno.Extensions.Navigation/RouteSegments.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Uno.Extensions.Navigation;
+
+#pragma warning disable SA1313 // Parameter names should begin with lower-case letter
+public record RouteSegments(string NavigationPath, bool IsRooted, int NumberOfPagesToRemove, IDictionary<string, object> Parameters, NavigationRequest NextRequest)
+#pragma warning restore SA1313 // Parameter names should begin with lower-case letter
+{
+}
