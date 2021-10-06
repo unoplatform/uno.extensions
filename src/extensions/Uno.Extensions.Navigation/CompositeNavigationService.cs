c4d08895026396d3ef48edcee767e5183ade97b4
Nick Randolph <nick@builttoroam.com>
2021-10-06 15:16:44 +1100

    Separating navigation services

---
+++ src/extensions/Uno.Extensions.Navigation/CompositeNavigationService.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Uno.Extensions.Navigation.Dialogs;
+
+namespace Uno.Extensions.Navigation;
+
+public class CompositeNavigationService : NavigationService, IRegionNavigationService
+{
+    protected IDictionary<string, IRegionNavigationService> NestedServices { get; } = new Dictionary<string, IRegionNavigationService>();
+
+    private AsyncAutoResetEvent NestedServiceWaiter { get; } = new AsyncAutoResetEvent(false);
+
+    public CompositeNavigationService(ILogger logger, IRegionNavigationService parent, IDialogFactory dialogFactory) : base(logger, parent, dialogFactory)
+    {
+    }
+
+    public void Attach(string regionName, IRegionNavigationService childRegion)
+    {
+        var childService = childRegion as RegionNavigationService;
+        NestedServices[regionName + string.Empty] = childService;
+        NestedServiceWaiter.Set();
+    }
+
+    public void Detach(IRegionNavigationService childRegion)
+    {
+        NestedServices.Remove(kvp => kvp.Value == childRegion);
+    }
+
+    public async override Task<NavigationResponse> NavigateAsync(NavigationRequest request)
+    {
+
+        // At this point, any residual request needs to be handed
+        // down to the appropriate nested service
+        var nestedNavigationResponse = await RunNestedNavigation(request);
+
+        var baseNavigationResponse = await base.NavigateAsync(request);
+
+        return baseNavigationResponse ?? nestedNavigationResponse;
+    }
+
+    private IRegionNavigationService Nested(string regionName = null)
+    {
+        return NestedServices.TryGetValue(regionName + string.Empty, out var service) ? service : null;
+    }
+
+    protected async Task<NavigationResponse> RunNestedNavigation(NavigationRequest request)
+    {
+        var nestedRequest = request;
+        if (nestedRequest is null || !nestedRequest.Segments.IsNested)
+        {
+            await Task.CompletedTask;
+            return null;
+        }
+
+        var nestedRoute = nestedRequest.Segments.Base;
+
+        RegionNavigationService nested = null;
+        while (nested is null)
+        {
+            // Try to retrieve nested service based on route name
+            nested = Nested(nestedRoute) as RegionNavigationService;
+            if (nested is null)
+            {
+                // No match for named route, so grab any unnamed nested
+                nested = Nested() as RegionNavigationService;
+            }
+            else
+            {
+                // If we've been able to retrieve the nested service
+                // we need to remove the route from the request path
+                var nextRoute = nestedRequest.Route.Uri.OriginalString.TrimStart($"{RouteConstants.Schemes.Current}/{nestedRoute}/");
+                nestedRequest = nestedRequest.WithPath(nextRoute);
+            }
+
+            if (nested is null)
+            {
+                await NestedServiceWaiter.Wait();
+            }
+        }
+
+        var response = await nested.NavigateAsync(nestedRequest.MakeCurrentRequest());
+        return response;
+    }
+}
