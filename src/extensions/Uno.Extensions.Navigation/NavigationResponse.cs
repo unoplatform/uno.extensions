72d54583fba6c28ce17f264b8d3b0459fcc75287
Nick Randolph <nick@builttoroam.com>
2021-10-12 22:49:57 +1100

    Refactoring conversion to typed response

--- src/extensions/Uno.Extensions.Navigation/NavigationResponse.cs
+++ src/extensions/Uno.Extensions.Navigation/NavigationResponse.cs
@@ -3,32 +3,19 @@
 namespace Uno.Extensions.Navigation;
 
 #pragma warning disable SA1313 // Parameter names should begin with lower-case letter
-public record NavigationResponse(NavigationRequest Request, Task<Options.Option> Result) : BaseNavigationResponse(Request)
+public record NavigationResponse(NavigationRequest Request, Task<Options.Option> Result) : NavigationResponseBase(Request)
 #pragma warning restore SA1313 // Parameter names should begin with lower-case letter
 {
 }
 
 #pragma warning disable SA1313 // Parameter names should begin with lower-case letter
-public record NavigationResponse<TResult>(NavigationRequest Request, Task<Options.Option<TResult>> Result) : BaseNavigationResponse(Request)
+public record NavigationResponse<TResult>(NavigationRequest Request, Task<Options.Option<TResult>> Result) : NavigationResponseBase(Request)
 #pragma warning restore SA1313 // Parameter names should begin with lower-case letter
 {
-    public static NavigationResponse<TResult> FromResponse(NavigationResponse response)
-    {
-        if (response is null)
-        {
-            return null;
-        }
-
-        return new NavigationResponse<TResult>(response.Request, response.Result.ContinueWith(x =>
-  (x.Result.MatchSome(out var val) && val is TResult tval) ?
-      Options.Option.Some(tval) :
-      Options.Option.None<TResult>())
-        );
-    }
 }
 
 #pragma warning disable SA1313 // Parameter names should begin with lower-case letter
-public record BaseNavigationResponse(NavigationRequest Request)
+public record NavigationResponseBase(NavigationRequest Request)
 #pragma warning restore SA1313 // Parameter names should begin with lower-case letter
 {
 }
