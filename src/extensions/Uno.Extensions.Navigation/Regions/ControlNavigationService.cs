66acfdfbe8ae980b6f06d271a4b2838b284992c4
Nick Randolph <nick@builttoroam.com>
2021-10-13 16:43:50 +1100

    removing regionnavigationservice

--- src/extensions/Uno.Extensions.Navigation/Regions/ControlNavigationService.cs
+++ src/extensions/Uno.Extensions.Navigation/Regions/ControlNavigationService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -49,7 +50,7 @@ public abstract class ControlNavigationService<TControl> : ControlNavigationServ
     }
 }
 
-public abstract class ControlNavigationService : RegionNavigationService
+public abstract class ControlNavigationService : CompositeNavigationService
 {
     protected IServiceProvider ScopedServices { get; }
 
@@ -78,7 +79,7 @@ public abstract class ControlNavigationService : RegionNavigationService
     {
     }
 
-    protected override async Task<NavigationResponse> ControlNavigateAsync(NavigationRequest request)
+    protected async Task<NavigationResponse> ControlNavigateAsync(NavigationRequest request)
     {
         if (request.Route.Base == CurrentPath)
         {
@@ -199,5 +200,101 @@ public abstract class ControlNavigationService : RegionNavigationService
     public override string ToString()
     {
         return $"Region Path='{CurrentPath}'";
+    }
+
+
+    protected async override Task<NavigationResponse> CoreNavigateAsync(NavigationRequest request)
+    {
+        NavigationResponse regionResponse = null;
+        if (request.Route.IsCurrent)
+        {
+            regionResponse = await RegionNavigateAsync(request);
+
+            if (regionResponse is not null)
+            {
+                request = request.Route.NextRequest(request.Sender);
+            }
+        }
+
+        if (!(request?.Route?.IsNested ?? false))
+        {
+            return regionResponse;
+        }
+
+        return await base.CoreNavigateAsync(request);
+    }
+
+    private async Task<NavigationResponse> RegionNavigateAsync(NavigationRequest request)
+    {
+        var taskCompletion = new TaskCompletionSource<Options.Option>();
+        // Temporarily detach all nested services to prevent accidental
+        // navigation to the wrong child
+        // eg switching tabs, frame on tab1 won't get detached until some
+        // time after navigating to tab2, meaning that the wrong nexted
+        // child will be used for any subsequent navigations.
+        var children = DetachAll();
+        var regionTask = await ControlNavigateAsync(request);
+        if (regionTask is null)
+        {
+            // If a null result task was returned, then no
+            // navigation took place, so just reattach the existing
+            // nav services
+            AttachAll(children);
+        }
+        else
+        {
+            _ = regionTask.Result?.ContinueWith((Task<Options.Option> t) =>
+            {
+                if (t.Status == TaskStatus.RanToCompletion)
+                {
+                    taskCompletion.TrySetResult(t.Result);
+                }
+                else
+                {
+                    taskCompletion.TrySetResult(Options.Option.None<object>());
+                }
+            },
+              TaskScheduler.Current);
+        }
+        return new NavigationResponse(request, taskCompletion.Task);
+    }
+
+    //public override string ToString()
+    //{
+    //    var sb = new StringBuilder();
+    //    PrintAllRegions(sb, this);
+    //    return sb.ToString();
+    //}
+
+    protected override void PrintAllRegions(StringBuilder builder, IRegionNavigationService nav, int indent = 0, string regionName = null)
+    {
+        //    if (nav is RegionNavigationService rns)
+        //    {
+        //        if (rns.Region is null)
+        //        {
+        //            builder.AppendLine(string.Empty);
+        //            builder.AppendLine("------------------------------------------------------------------------------------------------");
+        //            builder.AppendLine($"ROOT");
+        //        }
+        //        else
+        //        {
+        //            var ans = nav;
+        //            var prefix = string.Empty;
+        //            if (indent > 0)
+        //            {
+        //                prefix = new string(' ', indent * 2) + "|-";
+        //            }
+        //            var reg = !string.IsNullOrWhiteSpace(regionName) ? $"({regionName}) " : null;
+        //            builder.AppendLine($"{prefix}{reg}{rns.Region?.ToString()}");
+        //        }
+        //    }
+
+        //    base.PrintAllRegions(builder, nav, indent, regionName);
+
+        //    if (nav is RegionNavigationService rns2 &&
+        //        rns2.Region is null)
+        //    {
+        //        builder.AppendLine("------------------------------------------------------------------------------------------------");
+        //    }
     }
 }
