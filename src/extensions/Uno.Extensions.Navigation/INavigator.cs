37461fcfc81d5a382fddfea80791377d66033fd7
Nick Randolph <nick@builttoroam.com>
2021-10-21 14:41:32 +1100

    Removing Default attribute proof of concept code

--- src/extensions/Uno.Extensions.Navigation/INavigator.cs
+++ src/extensions/Uno.Extensions.Navigation/INavigator.cs
@@ -4,8 +4,5 @@ namespace Uno.Extensions.Navigation;
 
 public interface INavigator
 {
-    // TODO: Discuss alternatives for how to prevent default routes being executed if navigation in progress (eg deeplink)
-    Task WaitForPendingNavigation();
-
     Task<NavigationResponse> NavigateAsync(NavigationRequest request);
 }
