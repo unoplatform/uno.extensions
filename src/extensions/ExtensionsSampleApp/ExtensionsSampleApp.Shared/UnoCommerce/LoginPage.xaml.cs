ddd9780d783b482e0002bc4cb8ad899575d16b54
Nick Randolph <nick@builttoroam.com>
2021-10-29 10:29:54 +1100

    Adding basics for commerce app sample

--- src/extensions/ExtensionsSampleApp/ExtensionsSampleApp.Shared/UnoCommerce/LoginPage.xaml.cs
+++ src/extensions/ExtensionsSampleApp/ExtensionsSampleApp.Shared/UnoCommerce/LoginPage.xaml.cs
@@ -19,7 +19,7 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace ExtensionsSampleApp.Views.Twitter
+namespace ExtensionsSampleApp.UnoCommerce
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
