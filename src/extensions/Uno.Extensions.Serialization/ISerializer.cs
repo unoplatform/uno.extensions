161810de3047b667eb2a9df7617de190e84de369
Nick Randolph <nick@builttoroam.com>
2021-11-03 17:48:40 +1100

    Splitting ISerializer

--- src/extensions/Uno.Extensions.Serialization/ISerializer.cs
+++ src/extensions/Uno.Extensions.Serialization/ISerializer.cs
@@ -1,42 +1,9 @@
 ﻿using System;
-using System.IO;
 
 namespace Uno.Extensions.Serialization;
 
 public interface ISerializer
 {
-    // Summary:
-    //     Creates an instance of targetType from a serialized representation.
-    //
-    // Parameters:
-    //   source:
-    //     A serialized representation of a targetType.
-    //
-    //   targetType:
-    //     The type to use to deserialize the source.
-    //
-    // Returns:
-    //     The instance of targetType deserialized from the source.
-    object? ReadFromStream(Stream source, Type targetType);
-
-    // Summary:
-    //     Write a serialized representation of an object to a given System.IO.Stream.
-    //
-    // Parameters:
-    //   value:
-    //     The object to serialize.
-    //
-    //   valueType:
-    //     The type to use to serialize the object. value must be convertible to this type.
-    //
-    //   stream:
-    //     The stream to which the value should be written.
-    //
-    //   canDisposeStream:
-    //     A bool which indicates if the stream can be disposed after having written the
-    //     object or not.
-    void WriteToStream(Stream stream, object value, Type valueType);
-
     // Summary:
     //     Creates a serialized representation of an object.
     //
---
+++ src/extensions/Uno.Extensions.Serialization/SerializerExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Uno.Extensions.Serialization;
+
+public static class SerializerExtensions
+{
+    public static string ToString<T>(this ISerializer serializer, T value)
+    {
+        if (value is not null)
+        {
+            return serializer.ToString(value, typeof(T));
+        }
+
+        return string.Empty;
+    }
+
+    public static T? FromString<T>(this ISerializer serializer, string valueAsString)
+    {
+        return serializer is not null ?
+        (serializer.FromString(valueAsString, typeof(T)) is T tvalue) ?
+                tvalue :
+                default :
+            default;
+    }
+}
