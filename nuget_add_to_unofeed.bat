5a81d6bc8f1ee77a17cc4c25d46869cec354c84a
Nick Randolph <nick@builttoroam.com>
2021-11-12 08:55:53 +1100

    adding uno-check

--- nuget_add_to_unofeed.bat
+++ nuget_add_to_unofeed.bat
@@ -1,47 +1,47 @@
 rmdir .\unofeed\uno.extensions.configuration /s /q
-copy .\src\extensions\Uno.Extensions.Configuration\bin\Release\Uno.Extensions.Configuration.1.0.0.nupkg .\unofeed\
+copy .\src\extensions\Uno.Extensions.Configuration\bin\Release\Uno.Extensions.Configuration.255.255.255.255.nupkg .\unofeed\
 
 rmdir .\unofeed\uno.extensions.core /s /q
-copy .\src\extensions\Uno.Extensions.Core\bin\Release\Uno.Extensions.Core.1.0.0.nupkg .\unofeed\
+copy .\src\extensions\Uno.Extensions.Core\bin\Uno.Extensions.Core\Release\Uno.Extensions.Core.255.255.255.255.nupkg .\unofeed\
 
 rmdir .\unofeed\uno.extensions.hosting /s /q
-copy .\src\extensions\Uno.Extensions.Hosting\bin\Release\Uno.Extensions.Hosting.1.0.0.nupkg .\unofeed\
+copy .\src\extensions\Uno.Extensions.Hosting\bin\Release\Uno.Extensions.Hosting.255.255.255.255.nupkg .\unofeed\
 
 rmdir .\unofeed\uno.extensions.hosting.wasm /s /q
-copy .\src\extensions\Uno.Extensions.Hosting.Wasm\bin\Release\Uno.Extensions.Hosting.Wasm.1.0.0.nupkg .\unofeed\
+copy .\src\extensions\Uno.Extensions.Hosting.Wasm\bin\Release\Uno.Extensions.Hosting.Wasm.255.255.255.255.nupkg .\unofeed\
 
 rmdir .\unofeed\uno.extensions.http /s /q
-copy .\src\extensions\Uno.Extensions.Http\bin\Release\Uno.Extensions.Http.1.0.0.nupkg .\unofeed\
+copy .\src\extensions\Uno.Extensions.Http\bin\Release\Uno.Extensions.Http.255.255.255.255.nupkg .\unofeed\
 
 rmdir .\unofeed\uno.extensions.http.firebase /s /q
-copy .\src\extensions\Uno.Extensions.Http.Firebase\bin\Release\Uno.Extensions.Http.Firebase.1.0.0.nupkg .\unofeed\
+copy .\src\extensions\Uno.Extensions.Http.Firebase\bin\Release\Uno.Extensions.Http.Firebase.255.255.255.255.nupkg .\unofeed\
 
 rmdir .\unofeed\uno.extensions.http.refit /s /q
-copy .\src\extensions\Uno.Extensions.Http.Refit\bin\Release\Uno.Extensions.Http.Refit.1.0.0.nupkg .\unofeed\
+copy .\src\extensions\Uno.Extensions.Http.Refit\bin\Release\Uno.Extensions.Http.Refit.255.255.255.255.nupkg .\unofeed\
 
 rmdir .\unofeed\uno.extensions.localization /s /q
-copy .\src\extensions\Uno.Extensions.Localization\bin\Release\Uno.Extensions.Localization.1.0.0.nupkg .\unofeed\
+copy .\src\extensions\Uno.Extensions.Localization\bin\Release\Uno.Extensions.Localization.255.255.255.255.nupkg .\unofeed\
 
 rmdir .\unofeed\uno.extensions.logging /s /q
-copy .\src\extensions\Uno.Extensions.Logging\bin\Release\Uno.Extensions.Logging.1.0.0.nupkg .\unofeed\
+copy .\src\extensions\Uno.Extensions.Logging\bin\Release\Uno.Extensions.Logging.255.255.255.255.nupkg .\unofeed\
 
 rmdir .\unofeed\uno.extensions.logging.serilog /s /q
-copy .\src\extensions\Uno.Extensions.Logging.Serilog\bin\Release\Uno.Extensions.Logging.Serilog.1.0.0.nupkg .\unofeed\
+copy .\src\extensions\Uno.Extensions.Logging.Serilog\bin\Release\Uno.Extensions.Logging.Serilog.255.255.255.255.nupkg .\unofeed\
 
 rmdir .\unofeed\uno.extensions.logging.wasm /s /q
-copy .\src\extensions\Uno.Extensions.Logging.Wasm\bin\Release\Uno.Extensions.Logging.Wasm.1.0.0.nupkg .\unofeed\
+copy .\src\extensions\Uno.Extensions.Logging.Wasm\bin\Release\Uno.Extensions.Logging.Wasm.255.255.255.255.nupkg .\unofeed\
 
 rmdir .\unofeed\uno.extensions.navigation /s /q
-copy .\src\extensions\Uno.Extensions.Navigation\bin\Release\Uno.Extensions.Navigation.1.0.0.nupkg .\unofeed\
+copy .\src\extensions\Uno.Extensions.Navigation\bin\Release\Uno.Extensions.Navigation.255.255.255.255.nupkg .\unofeed\
 
 rmdir .\unofeed\uno.extensions.serialization /s /q
-copy .\src\extensions\Uno.Extensions.Serialization\bin\Release\Uno.Extensions.Serialization.1.0.0.nupkg .\unofeed\
+copy .\src\extensions\Uno.Extensions.Serialization\bin\Release\Uno.Extensions.Serialization.255.255.255.255.nupkg .\unofeed\
 
 rmdir .\unofeed\uno.extensions.serialization.http /s /q
-copy .\src\extensions\Uno.Extensions.Serialization.Http\bin\Release\Uno.Extensions.Serialization.Http.1.0.0.nupkg .\unofeed\
+copy .\src\extensions\Uno.Extensions.Serialization.Http\bin\Release\Uno.Extensions.Serialization.Http.255.255.255.255.nupkg .\unofeed\
 
 rmdir .\unofeed\uno.extensions.serialization.refit /s /q
-copy .\src\extensions\Uno.Extensions.Serialization.Refit\bin\Release\Uno.Extensions.Serialization.Refit.1.0.0.nupkg .\unofeed\
+copy .\src\extensions\Uno.Extensions.Serialization.Refit\bin\Release\Uno.Extensions.Serialization.Refit.255.255.255.255.nupkg .\unofeed\
 
 rmdir .\unofeed\Uno.UI.Toolkit /s /q
 copy .\unofeed\Uno.UI.Toolkit.255.255.255.255.nupkg .\unofeed\
