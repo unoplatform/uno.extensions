ecf1e71571705994133452a826673d569eaef03a
David Oliver <david.oliver@nventive.com>
2021-11-17 10:12:38 -0500

    Add ProfileService, feed-ify ProfilePage

--- samples/Commerce/Commerce.Shared/ViewModels/ProfileViewModel.cs
+++ samples/Commerce/Commerce.Shared/ViewModels/ProfileViewModel.cs
@@ -1,15 +1,20 @@
 ﻿using Commerce.Models;
+using Commerce.Services;
+using Uno.Extensions.Reactive;
 
 namespace Commerce.ViewModels
 {
 	public class ProfileViewModel
-    {
-		private Profile _person;
-		public ProfileViewModel()
+	{
+		private readonly IProfileService _profileService;
+		//private Profile _person;
+		public ProfileViewModel(IProfileService profileService)
 		{
-			_person = new Profile { FirstName = "Fred", LastName = "Jobs" };
+			//_person = new Profile { FirstName = "Fred", LastName = "Jobs" };
+			_profileService = profileService;
 		}
 
-		public string FullName => $"{_person.FirstName} {_person.LastName}";
-    }
+		public IFeed<ProfileModel> Profile => Feed.Async<ProfileModel>(async ct => new ProfileModel(await _profileService.GetProfile(ct)));
+		//public string FullName => $"{_person.FirstName} {_person.LastName}";
+	}
 }
