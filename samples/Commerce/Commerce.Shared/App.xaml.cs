9c3e72316ae58f5f1753b06343521a4d765e6a87
Nick Randolph <nick@builttoroam.com>
2021-11-18 09:23:35 +1100

    fix: deep link url not updating

--- samples/Commerce/Commerce.Shared/App.xaml.cs
+++ samples/Commerce/Commerce.Shared/App.xaml.cs
@@ -101,6 +101,10 @@ namespace Commerce
 			_window = Windows.UI.Xaml.Window.Current;
 #endif
 
+			var notif = Host.Services.GetService<IRouteNotifier>();
+			notif.RouteChanged += RouteUpdated;
+
+
 			_window.Content = new ShellView().WithNavigation(Host.Services);
 			_window.Activate();
 
@@ -115,8 +119,6 @@ namespace Commerce
 			Windows.UI.Core.SystemNavigationManager.GetForCurrentView().BackRequested += AppGoBack;
 
 #endif
-			var notif = Host.Services.GetService<IRouteNotifier>();
-			notif.RouteChanged += RouteUpdated;
 
 			// TODO: Work out how to reapply deeplink now that we're using a ShellView
 //#if __WASM__
