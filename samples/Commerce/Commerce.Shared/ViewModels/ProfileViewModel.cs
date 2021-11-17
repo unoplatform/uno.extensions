db51dc04ee27c8ab5d4d18c1127bd1defaf19adb
David Oliver <david.oliver@nventive.com>
2021-11-17 10:34:04 -0500

    Remove commented code

--- samples/Commerce/Commerce.Shared/ViewModels/ProfileViewModel.cs
+++ samples/Commerce/Commerce.Shared/ViewModels/ProfileViewModel.cs
@@ -7,14 +7,11 @@ namespace Commerce.ViewModels
 	public class ProfileViewModel
 	{
 		private readonly IProfileService _profileService;
-		//private Profile _person;
 		public ProfileViewModel(IProfileService profileService)
 		{
-			//_person = new Profile { FirstName = "Fred", LastName = "Jobs" };
 			_profileService = profileService;
 		}
 
 		public IFeed<ProfileModel> Profile => Feed.Async<ProfileModel>(async ct => new ProfileModel(await _profileService.GetProfile(ct)));
-		//public string FullName => $"{_person.FirstName} {_person.LastName}";
 	}
 }
