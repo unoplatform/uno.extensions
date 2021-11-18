06e7aa29bb1d4560dcf709bb535800b74d7fdba8
David <github.site@torick.net>
2021-11-18 16:03:57 -0500

    fix(commerce): Fix bindings and nav for the Filters picker

--- samples/Commerce/Commerce.Shared/ViewModels/FiltersViewModel.g.cs
+++ samples/Commerce/Commerce.Shared/ViewModels/FiltersViewModel.g.cs
@@ -10,11 +10,11 @@ namespace Commerce.ViewModels;
 
 partial class FiltersViewModel : IAsyncDisposable
 {
-	public class BindableFilterViewModel : BindableViewModelBase
+	public class BindableFiltersViewModel : BindableViewModelBase
 	{
 		private readonly BindableFilter _filter;
 
-		public BindableFilterViewModel(
+		public BindableFiltersViewModel(
 			Filters? defaultFilter = default)
 		{
 			_filter = new BindableFilter(Property(nameof(Filter), defaultFilter, out var filterSubject));
@@ -34,41 +34,33 @@ partial class FiltersViewModel : IAsyncDisposable
 
 	public class BindableFilter : Bindable<Filters>
 	{
-		private readonly Bindable<bool?> _shoes;
-		private readonly Bindable<bool?> _accessories;
-		private readonly Bindable<bool?> _headwear;
+		private readonly Bindable<bool> _shoes;
+		private readonly Bindable<bool> _accessories;
+		private readonly Bindable<bool> _headwear;
 
 		public BindableFilter(BindablePropertyInfo<Filters> property)
-			: base(property)
+			: base(property, true)
 		{
-			_shoes = new Bindable<bool?>(Property<bool?>(nameof(Shoes), p => p?.Shoes, (p, shoes) => (p ?? new()) with { Shoes = shoes ?? default(bool) }));
-			_accessories = new Bindable<bool?>(Property<bool?>(nameof(Accessories), p => p?.Accessories, (p, accessories) => (p ?? new()) with { Accessories = accessories ?? default(bool) }));
-			_headwear = new Bindable<bool?>(Property<bool?>(nameof(Headwear), p => p?.Headwear, (p, headwear) => (p ?? new()) with { Headwear = headwear ?? default(bool) }));
-
-			PropertyChanged += (snd, e) =>
-			{
-				if (e.PropertyName != "Value")
-				{
-					this.GetType().GetEvent(nameof(PropertyChanged)).RaiseMethod?.Invoke(this, new object[] { this, new PropertyChangedEventArgs(nameof(Value)) });
-				}
-			};
+			_shoes = new Bindable<bool>(Property<bool>(nameof(Shoes), p => p?.Shoes ?? default(bool), (p, shoes) => (p ?? new()) with { Shoes = shoes }));
+			_accessories = new Bindable<bool>(Property<bool>(nameof(Accessories), p => p?.Accessories ?? default(bool), (p, accessories) => (p ?? new()) with { Accessories = accessories }));
+			_headwear = new Bindable<bool>(Property<bool>(nameof(Headwear), p => p?.Headwear ?? default(bool), (p, headwear) => (p ?? new()) with { Headwear = headwear }));
 		}
 
 		public Filters Value => GetValue();
 
-		public bool? Shoes
+		public bool Shoes
 		{
 			get => _shoes.GetValue();
 			set => _shoes.SetValue(value);
 		}
 
-		public bool? Accessories
+		public bool Accessories
 		{
 			get => _accessories.GetValue();
 			set => _accessories.SetValue(value);
 		}
 
-		public bool? Headwear
+		public bool Headwear
 		{
 			get => _headwear.GetValue();
 			set => _headwear.SetValue(value);
