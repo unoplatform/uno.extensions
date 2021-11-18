2566d96e5ca9f7d15f367f37ff3248cdf9605bee
xiaoy312 <xiaoyao312@gmail.com>
2021-11-17 20:21:10 -0500

    feat: dark mode toggle

--- samples/Commerce/Commerce.Shared/Views/ProfilePage.xaml.cs
+++ samples/Commerce/Commerce.Shared/Views/ProfilePage.xaml.cs
@@ -12,19 +12,36 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Uno.Toolkit.UI.Helpers;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Commerce
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class ProfilePage : Page
-    {
-        public ProfilePage()
-        {
-            this.InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class ProfilePage : Page
+	{
+		public ProfilePage()
+		{
+			this.InitializeComponent();
+
+			this.Loaded += (s, e) =>
+			{
+				// Initialize the toggle to the current theme.
+				darkModeToggle.IsEnabled = false;
+				darkModeToggle.IsOn = SystemThemeHelper.IsAppInDarkMode();
+				darkModeToggle.IsEnabled = true;
+			};
+		}
+
+		private void ToggleDarkMode()
+		{
+			if (darkModeToggle.IsEnabled)
+			{
+				SystemThemeHelper.SetApplicationTheme(darkModeToggle.IsOn);
+			}
+		}
+	}
 }
