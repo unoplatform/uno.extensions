6762dc63f832ddf35cb5eb286e4203a1aae565ca
David <github.site@torick.net>
2021-11-19 09:38:30 -0500

    fix(commerce): Fix casing serach

--- samples/Commerce/Commerce.Shared/Services/ProductServices.cs
+++ samples/Commerce/Commerce.Shared/Services/ProductServices.cs
@@ -5,24 +5,20 @@ using System.Threading;
 using System.Threading.Tasks;
 using Commerce.Models;
 
-
 namespace Commerce.Services;
 
 public class ProductService : JsonDataService<Product>, IProductService
 {
 	public ProductService(string dataFile) : base(dataFile)
 	{
-
 	}
 
 	public async Task<IEnumerable<Product>> GetProducts(string? term, CancellationToken ct)
 	{
-		await Task.Delay(new Random(DateTime.Now.Millisecond).Next(100, 500), ct);
-
 		var products = (await GetEntities()).AsEnumerable();
 		if (term is not null)
 		{
-			products = products.Where(p => p.Name.Contains(term));
+			products = products.Where(p => p.Name.Contains(term, StringComparison.OrdinalIgnoreCase));
 		}
 
 		return products;
