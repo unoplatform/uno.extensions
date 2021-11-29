1f2a15e916497b25143bf027f1c5d6431d9f8df1
Nick Randolph <nick@builttoroam.com>
2021-11-29 14:16:12 +1100

    fix: navigation building

--- src/Uno.Extensions.Navigation.UI/Controls/IRequestHandler.cs
+++ src/Uno.Extensions.Navigation.UI/Controls/IRequestHandler.cs
@@ -1,17 +1,8 @@
-﻿#if !WINUI
-using Windows.UI.Xaml;
-#else
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-#endif
+﻿namespace Uno.Extensions.Navigation.Controls;
 
-namespace Uno.Extensions.Navigation.Controls
+public interface IRequestHandler
 {
-    public interface IRequestHandler
-    {
-        bool CanBind(FrameworkElement view);
+	bool CanBind(FrameworkElement view);
 
-        void Bind(FrameworkElement view);
-    }
+	void Bind(FrameworkElement view);
 }
--- src/Uno.Extensions.Navigation.UI/Controls/NavigationRequestBinder.cs
+++ src/Uno.Extensions.Navigation.UI/Controls/NavigationRequestBinder.cs
@@ -1,12 +1,5 @@
 ﻿using System.Linq;
 using Microsoft.Extensions.DependencyInjection;
-#if !WINUI
-using Windows.UI.Xaml;
-#else
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-#endif
 
 namespace Uno.Extensions.Navigation.Controls
 {
