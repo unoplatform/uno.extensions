2d66f15ad05a65a1d2d221ab8ef32098025b21f8
David <github.site@torick.net>
2021-11-29 22:37:00 -0500

    fix(reactive): Fix misc minor issues with FeedView style

--- src/Uno.Extensions.Reactive/AssemblyInfo.cs
+++ src/Uno.Extensions.Reactive/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Uno.Extensions.Reactive.Tests")]
-[assembly: InternalsVisibleTo("Uno.Extensions.Reactive.View")]
+[assembly: InternalsVisibleTo("Uno.Extensions.Reactive.UI")]
