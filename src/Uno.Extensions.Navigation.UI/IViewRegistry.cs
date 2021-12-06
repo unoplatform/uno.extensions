8bf978469323cdc32e04629a0b6bd3730bf71227
Nick Randolph <nick@builttoroam.com>
2021-12-06 18:31:06 +1100

    chore: code cleanup

---
+++ src/Uno.Extensions.Navigation.UI/IViewRegistry.cs
@@ -0,0 +1,13 @@
+﻿namespace Uno.Extensions.Navigation;
+
+public interface IViewRegistry
+{
+	IViewRegistry Register(ViewMap view);
+
+	IViewRegistry Register<TData>(ViewMap<TData> route)
+		where TData : class;
+
+	IViewRegistry Register<TData, TResultData>(ViewMap<TData, TResultData> route)
+		where TData : class
+		where TResultData : class;
+}
