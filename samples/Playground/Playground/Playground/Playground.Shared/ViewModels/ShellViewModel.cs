f4c0a0d92203057d012405614345030f34426ee8
Nick Randolph <nick@builttoroam.com>
2021-12-16 18:30:29 +1100

    feat: Adding playground sample

---
+++ samples/Playground/Playground/Playground/Playground.Shared/ViewModels/ShellViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Playground.Views;
+using Uno.Extensions.Navigation;
+
+namespace Playground.ViewModels
+{
+    public class ShellViewModel
+    {
+		public ShellViewModel(INavigator navigator)
+		{
+			navigator.NavigateViewAsync<HomePage>(this);
+		}
+	}
+}
