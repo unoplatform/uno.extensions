f1d6a916800b2a5a5cb49def3bae57fc8efbd1b7
Nick Randolph <nick@builttoroam.com>
2021-12-16 19:20:44 +1100

    feat: Adding navigation examples

---
+++ samples/Playground/Playground/Playground/Playground.Shared/ViewModels/FourthViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Uno.Extensions.Navigation;
+
+namespace Playground.ViewModels
+{
+	public class FourthViewModel
+    {
+		public string Title => "Fourth page with View Model";
+
+		private INavigator _navigator;
+		public FourthViewModel(INavigator navigator)
+		{
+			_navigator = navigator;
+		}
+
+		public async Task NavigateToFifth()
+		{
+			await _navigator.NavigateViewModelAsync<FifthViewModel>(this, Schemes.Nested);
+		}
+	}
+}
---
+++ samples/Playground/Playground/Playground/Playground.Shared/Views/FourthPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Playground.ViewModels;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Playground.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class FourthPage : Page
+    {
+		public FourthViewModel ViewModel { get; set; }
+        public FourthPage()
+        {
+            this.InitializeComponent();
+
+			DataContextChanged += FourthPage_DataContextChanged;
+        }
+
+		private void FourthPage_DataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args)
+		{
+			ViewModel = args.NewValue as FourthViewModel;	
+		}
+	}
+}
