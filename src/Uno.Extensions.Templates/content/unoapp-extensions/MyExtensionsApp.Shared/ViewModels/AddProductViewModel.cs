7511a2591b3413bd3ff4c79216be6c5713f42cf7
Nick Randolph <nick@builttoroam.com>
2022-01-18 12:45:25 +1100

    fix: Adding missing source files for template

---
+++ src/Uno.Extensions.Templates/content/unoapp-extensions/MyExtensionsApp.Shared/ViewModels/AddProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Uno.Extensions.Reactive;
+
+namespace MyExtensionsApp.ViewModels;
+
+public partial class AddProductViewModel
+{
+	
+}
--- src/Uno.Extensions.Templates/content/unoapp-extensions/MyExtensionsApp.Shared/Views/AddProductPage.xaml.cs
+++ src/Uno.Extensions.Templates/content/unoapp-extensions/MyExtensionsApp.Shared/Views/AddProductPage.xaml.cs
@@ -1,4 +1,4 @@
-//-:cnd:noEmit
+﻿//-:cnd:noEmit
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -7,6 +7,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
+#if WINUI
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -14,17 +15,26 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
-
-// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+#else
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+#endif
 
 namespace MyExtensionsApp
 {
-    public sealed partial class FilterPopup : Flyout
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AddProductPage : Page
     {
-        public FilterPopup()
+        public AddProductPage()
         {
             this.InitializeComponent();
         }
-
     }
 }
