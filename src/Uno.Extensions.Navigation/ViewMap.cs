804f4e8eb740495a5bfadfe196ebc81466a0270c
Nick Randolph <nick@builttoroam.com>
2022-02-23 17:21:40 +1100

    fix: Fixing playground app

--- src/Uno.Extensions.Navigation/ViewMap.cs
+++ src/Uno.Extensions.Navigation/ViewMap.cs
@@ -19,7 +19,20 @@ public record ViewMap(
 	}
 }
 
+public record ViewMap<TView>(
+	Type? ViewModel = null,
+	DataMap? Data = null,
+	Type? ResultData = null
+) : ViewMap(typeof(TView), ViewModel, Data, ResultData)
+{
+}
 
+public record ViewMap<TView, TViewModel>(
+	DataMap? Data = null,
+	Type? ResultData = null
+) : ViewMap(typeof(TView), typeof(TViewModel), Data, ResultData)
+{
+}
 
 #pragma warning restore SA1313 // Parameter names should begin with lower-case letter
 
