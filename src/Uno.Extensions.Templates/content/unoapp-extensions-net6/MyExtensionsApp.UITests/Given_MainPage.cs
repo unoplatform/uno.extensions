b3c61adc97f2c43c266ed20b480151cbc457f735
Nick Randolph <nick@builttoroam.com>
2022-03-16 16:21:37 +1100

    feat: Adding net6 test projects

---
+++ src/Uno.Extensions.Templates/content/unoapp-extensions-net6/MyExtensionsApp.UITests/Given_MainPage.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Query = System.Func<Uno.UITest.IAppQuery, Uno.UITest.IAppQuery>;
+
+namespace MyExtensionsApp.UITests
+{
+	public class Given_MainPage : TestBase
+    {
+        [Test]
+        public void When_SmokeTest()
+        {
+
+			// Make sure the ViewModelButton has rendered
+			App.WaitForElement(q => q.Marked("ViewModelButton"));
+
+			// Query for the XamlButton and then tap it
+			Query xamlButton = q => q.All().Marked("XamlButton");
+			App.WaitForElement(xamlButton);
+			App.Tap(xamlButton);
+
+			// Take a screenshot and add it to the test results
+			TakeScreenshot("After tapped");
+        }
+    }
+}
