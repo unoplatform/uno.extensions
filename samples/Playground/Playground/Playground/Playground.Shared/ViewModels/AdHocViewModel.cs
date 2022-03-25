dfd3bec028dc96247476b3c3da795aab43316d2f
Nick Randolph <nick@builttoroam.com>
2022-03-25 16:40:45 +1100

    feat: Adding generic ISerializer and IStreamSerializer

--- samples/Playground/Playground/Playground/Playground.Shared/ViewModels/AdHocViewModel.cs
+++ samples/Playground/Playground/Playground/Playground.Shared/ViewModels/AdHocViewModel.cs
@@ -1,16 +1,22 @@
 ﻿using Playground.Services.Endpoints;
 
+
+using System.Diagnostics;
+
 namespace Playground.ViewModels;
 
 public class AdHocViewModel
 {
 	private readonly INavigator _navigator;
 	private readonly ITodoTaskEndpoint _todoTaskEndpoint;
+	private readonly ISerializer<Widget> _serializer;
 	public AdHocViewModel(
 		INavigator navigator,
-		ITodoTaskEndpoint todoTaskEndpoint)
+		ITodoTaskEndpoint todoTaskEndpoint, 
+		ISerializer<Widget> serializer)
 	{
 		_navigator = navigator;
+		_serializer = serializer;
 		_todoTaskEndpoint = todoTaskEndpoint;
 	}
 
@@ -18,7 +24,7 @@ public class AdHocViewModel
 	{
 		await Task.Run(async () =>
 		{
-			await _navigator.NavigateRouteAsync(this,"./One");
+			await _navigator.NavigateRouteAsync(this, "./One");
 			await Task.Delay(1000);
 			await _navigator.NavigateRouteAsync(this, "./Two");
 			await Task.Delay(1000);
@@ -27,5 +33,13 @@ public class AdHocViewModel
 			await _navigator.NavigateRouteAsync(this, "./One");
 
 		});
+	}
+
+	public async Task RunSerializer()
+	{
+		var w = new Widget { Name = "Bob", Weight = 60 };
+	var str = 	_serializer.ToString(w);
+		var newW = _serializer.FromString(str);
+		Debug.Assert(w == newW);
 	}
 }
--- src/Uno.Extensions.Serialization/SystemTextJsonStreamSerializer.cs
+++ src/Uno.Extensions.Serialization/SystemTextJsonStreamSerializer.cs
@@ -6,30 +6,63 @@ namespace Uno.Extensions.Serialization;
 
 public class SystemTextJsonStreamSerializer : ISerializer, IStreamSerializer
 {
-    private readonly JsonSerializerOptions? _serializerOptions;
+	private readonly JsonSerializerOptions? _serializerOptions;
 
-    public SystemTextJsonStreamSerializer(JsonSerializerOptions? serializerOptions = null)
-    {
-        _serializerOptions = serializerOptions;
-    }
+	public SystemTextJsonStreamSerializer(JsonSerializerOptions? serializerOptions = null)
+	{
+		_serializerOptions = serializerOptions;
+	}
 
-    public object? ReadFromStream(Stream source, Type targetType)
-    {
-        return JsonSerializer.Deserialize(source, targetType, _serializerOptions);
-    }
+	public object? ReadFromStream(Stream source, Type targetType)
+	{
+		return JsonSerializer.Deserialize(source, targetType, _serializerOptions);
+	}
 
-    public void WriteToStream(Stream stream, object value, Type valueType)
-    {
-        JsonSerializer.Serialize(stream, value, valueType, _serializerOptions);
-    }
+	public void WriteToStream(Stream stream, object value, Type valueType)
+	{
+		JsonSerializer.Serialize(stream, value, valueType, _serializerOptions);
+	}
 
-    public string ToString(object value, Type valueType)
-    {
-        return JsonSerializer.Serialize(value, valueType, _serializerOptions);
-    }
+	public string ToString(object value, Type valueType)
+	{
+		return JsonSerializer.Serialize(value, valueType, _serializerOptions);
+	}
 
-    public object? FromString(string source, Type targetType)
-    {
-        return JsonSerializer.Deserialize(source, targetType, _serializerOptions);
-    }
+	public object? FromString(string source, Type targetType)
+	{
+		return JsonSerializer.Deserialize(source, targetType, _serializerOptions);
+	}
+}
+
+public class SystemTextJsonStreamSerializer<T> : SystemTextJsonStreamSerializer, ISerializer<T>, IStreamSerializer<T>
+{
+	public T? FromString(string source)
+	{
+		return FromString(source, typeof(T)) is T value ? value : default;
+	}
+
+	public T? ReadFromStream(Stream source)
+	{
+		return ReadFromStream(source, typeof(T)) is T value ? value : default;
+	}
+
+	public string ToString(T value)
+	{
+		if(value is null)
+		{
+			return String.Empty;
+		}
+
+		return ToString(value, typeof(T));
+	}
+
+	public void WriteToStream(Stream stream, T value)
+	{
+		if (value is null)
+		{
+			return;
+		}
+
+		WriteToStream(stream, value, typeof(T));
+	}
 }
