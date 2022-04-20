c595fc26d017bd0569766dfbc84ea6694800c514
David <github.site@torick.net>
2022-04-20 15:13:42 -0400

    feat: Add ListState.Add extension

---
+++ src/Uno.Extensions.Reactive/Core/ListState.Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Uno.Extensions.Reactive;
+
+/// <summary>
+/// Provides a set of static methods to create and manipulate <see cref="IListState{T}"/>.
+/// </summary>
+public static class ListState
+{
+	#region Operators
+	/// <summary>
+	/// Adds an items into the state
+	/// </summary>
+	/// <typeparam name="T">The type of the items in the list.</typeparam>
+	/// <param name="state">The list state onto which the item should be added.</param>
+	/// <param name="item">The item to add.</param>
+	/// <param name="ct">A token to abort the async add operation.</param>
+	/// <returns></returns>
+	public static ValueTask Add<T>(this IListState<T> state, T item, CancellationToken ct)
+		=> state.UpdateValue(items => items.SomeOrDefault(ImmutableList<T>.Empty).Add(item), ct);
+	#endregion
+}
