525de66233ca18ead5b67eac491f18c82c6ca107
David <github.site@torick.net>
2022-04-25 18:41:46 -0400

    fix: Make sure to go in None state if source is null or empty

---
+++ src/Uno.Extensions.Reactive.Tests/Operators/Given_FeedToListFeedAdapter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Uno.Extensions.Reactive.Testing;
+
+namespace Uno.Extensions.Reactive.Tests.Operators;
+
+[TestClass]
+public class Given_FeedToListFeedAdapter : FeedTests
+{
+	[TestMethod]
+	public async Task When_Null_Then_None()
+	{
+		var source = Feed.Async(async ct => default(IImmutableList<int>)!);
+		var sut = source.AsListFeed();
+		var result = await sut.Record();
+
+		result.Should().Be(r => r
+			.Message(Changed.Data, Data.None, Error.No, Progress.Final)
+		);
+	}
+
+	[TestMethod]
+	public async Task When_Empty_Then_None()
+	{
+		var source = Feed.Async(async ct => ImmutableList<int>.Empty as IImmutableList<int>);
+		var sut = source.AsListFeed();
+		var result = await sut.Record();
+
+		result.Should().Be(r => r
+			.Message(Changed.Data, Data.None, Error.No, Progress.Final)
+		);
+	}
+}
