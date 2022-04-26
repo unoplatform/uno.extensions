15898ba89fe45cbc97fb8d0e63cc368c78a4ea93
Nick Randolph <nick@builttoroam.com>
2022-04-26 19:03:53 +1000

    fix: Change View property to be optional

--- src/Uno.Extensions.Navigation/RouteMap.cs
+++ src/Uno.Extensions.Navigation/RouteMap.cs
@@ -3,7 +3,7 @@
 #pragma warning disable SA1313 // Parameter names should begin with lower-case letter
 public record RouteMap(
 	string Path,
-	ViewMap View,
+	ViewMap? View = null,
 	bool IsDefault = false,
 	string DependsOn = "",
 	Func<NavigationRequest, NavigationRequest>? Init = null,
