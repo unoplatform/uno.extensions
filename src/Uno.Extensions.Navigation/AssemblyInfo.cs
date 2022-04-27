605814980ea581dc45480f7fe477897474b588fe
Nick Randolph <nick@builttoroam.com>
2022-04-27 19:30:49 +1000

    fix: Replacing missing assemblyinfo file

--- src/Uno.Extensions.Navigation/AssemblyInfo.cs
+++ src/Uno.Extensions.Navigation/AssemblyInfo.cs
@@ -1,7 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Uno.Extensions.Configuration")]
-[assembly: InternalsVisibleTo("Uno.Extensions.Navigation")]
 [assembly: InternalsVisibleTo("Uno.Extensions.Navigation.UI")]
 [assembly: InternalsVisibleTo("Uno.Extensions.Navigation.WinUI")]
 [assembly: InternalsVisibleTo("Uno.Extensions.Navigation.Toolkit.UI")]
