2555bff634577ac8908883385323cbcf7a80b68e
David <github.site@torick.net>
2022-04-27 10:54:01 -0400

    fix(feeds): Fix pragma def

---
+++ src/Uno.Extensions.Reactive.UI/Presentation/Bindings/Collections/NullCollectionViewGroup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using Windows.Foundation.Collections;
+
+namespace Uno.Extensions.Reactive.Bindings.Collections;
+
+/// <summary>
+/// Null pattern implementation of <see cref="ICollectionViewGroup"/>.
+/// </summary>
+internal class NullCollectionViewGroup : ICollectionViewGroup
+{
+	/// <inhertidoc />
+	public object? Group { get; }
+
+	/// <inhertidoc />
+	public IObservableVector<object> GroupItems { get; } = new NullObservableVector<object>();
+}
