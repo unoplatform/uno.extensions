bd02bce90796e2e3240d0c7d9a115932ba0e036b
Nick Randolph <nick@builttoroam.com>
2022-05-17 13:35:37 +1000

    feat: Adding configure callbacks

--- src/Uno.Extensions.Localization/HostBuilderExtensions.cs
+++ src/Uno.Extensions.Localization/HostBuilderExtensions.cs
@@ -1,23 +1,32 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Localization;
 using Uno.Extensions.Configuration;
 
-namespace Uno.Extensions.Localization
-{
-    public static class HostBuilderExtensions
-    {
-        public static IHostBuilder UseLocalization(this IHostBuilder builder)
-        {
-            return builder
-                .UseSettings<LocalizationSettings>(ctx => ctx.Configuration.GetSection(nameof(LocalizationSettings)))
+namespace Uno.Extensions.Localization;
 
-                .ConfigureServices((ctx, services) =>
-            {
-                _ = services
-                .AddHostedService<LocalizationService>()
-                .AddSingleton<IStringLocalizer, ResourceLoaderStringLocalizer>();
-            });
-        }
-    }
+public static class HostBuilderExtensions
+{
+	public static IHostBuilder UseLocalization(
+		this IHostBuilder hostBuilder,
+		Action<IServiceCollection> configure)
+	{
+		return hostBuilder.UseConfiguration((context, builder) => configure.Invoke(builder));
+	}
+
+	public static IHostBuilder UseLocalization(
+		this IHostBuilder hostBuilder,
+		Action<HostBuilderContext, IServiceCollection>? configure = default)
+	{
+		return hostBuilder
+			.UseSettings<LocalizationSettings>(ctx => ctx.Configuration.GetSection(nameof(LocalizationSettings)))
+
+			.ConfigureServices((ctx, services) =>
+		{
+			_ = services
+			.AddHostedService<LocalizationService>()
+			.AddSingleton<IStringLocalizer, ResourceLoaderStringLocalizer>();
+		});
+	}
 }
--- src/Uno.Extensions.Serialization/HostBuilderExtensions.cs
+++ src/Uno.Extensions.Serialization/HostBuilderExtensions.cs
@@ -2,13 +2,18 @@
 
 public static class HostBuilderExtensions
 {
-    public static IHostBuilder UseSerialization(this IHostBuilder hostBuilder, Action<IServiceCollection>? jsonTypeCallback = default)
+	public static IHostBuilder UseSerialization(this IHostBuilder hostBuilder, Action<IServiceCollection> configure)
+	{
+		return hostBuilder.UseSerialization((context, builder) => configure.Invoke(builder));
+	}
+
+	public static IHostBuilder UseSerialization(this IHostBuilder hostBuilder, Action<HostBuilderContext, IServiceCollection>? configure = default)
     {
         return hostBuilder
                 .ConfigureServices((ctx, s) =>
 				{
 					_=s.AddSystemTextJsonSerialization();
-					jsonTypeCallback?.Invoke(s);
+					configure?.Invoke(ctx,s);
 				});
     }
 }
