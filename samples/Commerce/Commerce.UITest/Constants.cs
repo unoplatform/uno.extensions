00c7db347a449fb8540d4b6b9fbd241d0bb6908e
Nick Randolph <nick@builttoroam.com>
2022-05-25 17:03:46 +1000

    chore: Code tidyup

--- samples/Commerce/Commerce.UITest/Constants.cs
+++ samples/Commerce/Commerce.UITest/Constants.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Uno.UITest.Helpers.Queries;
+﻿using Uno.UITest.Helpers.Queries;
 
-namespace Uno.Gallery.UITests
+namespace Uno.Gallery.UITests;
+
+public class Constants
 {
-    public class Constants
-    {
-        public readonly static string WebAssemblyDefaultUri = "https://localhost:64215";
-        public readonly static string iOSAppName = "uno.platform.extensions.demo";
-        public readonly static string AndroidAppName = "uno.platform.extensions.demo";
-        public readonly static string iOSDeviceNameOrId = "iPad Pro (12.9-inch) (4th generation)";
+	public readonly static string WebAssemblyDefaultUri = "https://localhost:64215";
+	public readonly static string iOSAppName = "uno.platform.extensions.demo";
+	public readonly static string AndroidAppName = "uno.platform.extensions.demo";
+	public readonly static string iOSDeviceNameOrId = "iPad Pro (12.9-inch) (4th generation)";
 
-        public readonly static Platform CurrentPlatform = Platform.Browser;
-    }
+	public readonly static Platform CurrentPlatform = Platform.Browser;
 }
