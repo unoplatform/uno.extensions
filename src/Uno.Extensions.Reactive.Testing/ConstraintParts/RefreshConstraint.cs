13bd69ee7fa1436410a012502dec8d16a5ac7f3a
David <github.site@torick.net>
2022-06-02 17:27:56 -0400

    feat(feed): Add ability to test the refresh axis

---
+++ src/Uno.Extensions.Reactive.Testing/ConstraintParts/RefreshConstraint.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Uno.Extensions.Reactive.Sources;
+
+namespace Uno.Extensions.Reactive.Testing;
+
+public record RefreshConstraint
+{
+	public object? Source { get; init; }
+
+	public uint? RootContextId { get; init; }
+
+	public uint? Version { get; init; }
+
+	internal int Match(RefreshToken version)
+	{
+		var score = 0;
+
+		if (Source is not null)
+		{
+			score += Source == version.Source ? 1 : -100;
+		}
+
+		if (RootContextId is not null)
+		{
+			score += RootContextId == version.RootContextId ? 1 : -100;
+		}
+
+		if (Version is not null)
+		{
+			score += Version == version.Version ? 1 : -100;
+		}
+
+		return score;
+	}
+}
---
+++ src/Uno.Extensions.Reactive.Testing/ConstraintParts/Refreshed.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using static Uno.Extensions.Reactive.Core.FeedHelper;
+
+namespace Uno.Extensions.Reactive.Testing;
+
+public sealed class Refreshed : MessageAxisConstraint
+{
+	public static Refreshed Is(uint version)
+		=> new(new RefreshConstraint { Version = version });
+
+	public static Refreshed By(object source, uint version)
+		=> new(new RefreshConstraint { Source = source, Version = version });
+
+	private readonly RefreshConstraint[] _expected;
+
+	public Refreshed(params RefreshConstraint[] expected)
+	{
+		_expected = expected;
+	}
+
+	/// <inheritdoc />
+	public override void Assert(IMessageEntry entry)
+	{
+		var actual = MessageAxis.Refresh.FromMessageValue(entry[Axis]);
+		if (actual is null or {IsEmpty: true})
+		{
+			if (_expected.Length != 0)
+			{
+				AssertionScope.Current.FailWith(AssertionScope.Current.Context.Value + " no source version has been defined in the message.");
+			}
+			return;
+		}
+
+		actual.Versions.Count.Should().Be(_expected.Length, "all source versions should be defined.");
+
+		var expected = _expected.ToList();
+		foreach (var actualVersion in actual.Versions)
+		{
+			var expectedVersion = expected
+				.Select(version => (version, score: version.Match(actualVersion)))
+				.Where(x => x.score > 0)
+				.OrderByDescending(x => x.score)
+				.FirstOrDefault()
+				.version;
+
+			if (expectedVersion is null)
+			{
+				AssertionScope.Current.FailWith($"{AssertionScope.Current.Context.Value} feed {GetDebugIdentifier(actualVersion.Source)} version {actualVersion.Version} for context #{actualVersion.RootContextId} was not expected.");
+			}
+			else
+			{
+				expected.Remove(expectedVersion);
+			}
+		}
+	}
+
+	/// <inheritdoc />
+	public override MessageAxis Axis => MessageAxis.Refresh;
+}
