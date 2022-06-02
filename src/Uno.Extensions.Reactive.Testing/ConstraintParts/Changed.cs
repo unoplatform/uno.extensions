7f913dca8eb365a99b20a6e9dfa1ac8c24da1b18
David <github.site@torick.net>
2022-06-02 17:27:56 -0400

    feat(feed): Add ability to test the refresh axis

--- src/Uno.Extensions.Reactive.Testing/ConstraintParts/Changed.cs
+++ src/Uno.Extensions.Reactive.Testing/ConstraintParts/Changed.cs
@@ -16,6 +16,8 @@ public class Changed : ConstraintPart<IMessage>
 
 	public static Changed Progress { get; } = new(MessageAxis.Progress);
 
+	public static Changed Version { get; } = new(MessageAxis.Refresh);
+
 	public static Changed Axes(params MessageAxis[] axes) 
 		=> new(axes);
 
