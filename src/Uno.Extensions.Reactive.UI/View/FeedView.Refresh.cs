b50230afe9b3920c4ea0af3880f95935f65d0347
David <github.site@torick.net>
2022-06-02 17:30:09 -0400

    feat(feed): Add refresh command on the FeedView

---
+++ src/Uno.Extensions.Reactive.UI/View/FeedView.Refresh.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Uno.Extensions.Reactive.UI;
+
+public partial class FeedView
+{
+	internal class RefreshCommand : IAsyncCommand
+	{
+		private static readonly PropertyChangedEventArgs _isExecutingChanged = new(nameof(IsExecuting));
+
+		/// <inheritdoc />
+		public event EventHandler? CanExecuteChanged;
+
+		/// <inheritdoc />
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		private readonly FeedView _view;
+		private bool _isExecuting;
+
+		public RefreshCommand(FeedView view)
+		{
+			_view = view;
+		}
+
+		/// <inheritdoc />
+		public bool IsExecuting
+		{
+			get => _isExecuting;
+			internal set
+			{
+				if (value != _isExecuting)
+				{
+					_isExecuting = value;
+					CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+					PropertyChanged?.Invoke(this, _isExecutingChanged);
+				}
+			}
+		}
+
+		/// <inheritdoc />
+		public bool CanExecute(object? parameter)
+			=> !IsExecuting;
+
+		/// <inheritdoc />
+		public void Execute(object? parameter)
+			=> IsExecuting = _view._subscription?.RequestRefresh() ?? false;
+	}
+}
