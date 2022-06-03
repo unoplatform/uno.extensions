4ebe291b29817db8608a297f8e5cd2990720c3f1
David <github.site@torick.net>
2022-06-03 11:36:42 -0400

    chore(feeds): Add more details about the None SourceContext errors

--- src/Uno.Extensions.Reactive/Core/Internal/StateStore.None.cs
+++ src/Uno.Extensions.Reactive/Core/Internal/StateStore.None.cs
@@ -10,11 +10,11 @@ internal class NoneStateStore : IStateStore
 	public TState GetOrCreateState<TSource, TState>(TSource source, Func<SourceContext, TSource, TState> factory)
 		where TSource : class
 		where TState : IStateImpl, IAsyncDisposable
-		=> throw new InvalidOperationException("Cannot create a state on SourceContext.None");
+		=> throw new InvalidOperationException("Cannot create a state on SourceContext.None. " + SourceContext.NoneContextErrorDesc);
 
 	/// <inheritdoc />
 	public IState<T> CreateState<T>(Option<T> initialValue)
-		=> throw new InvalidOperationException("Cannot create a state on SourceContext.None");
+		=> throw new InvalidOperationException("Cannot create a state on SourceContext.None. " + SourceContext.NoneContextErrorDesc);
 
 	/// <inheritdoc />
 	public async ValueTask DisposeAsync()
