2d3e954541fab428eb1bce894c7130aeb594596b
Nick Randolph <nick@builttoroam.com>
2022-06-09 18:15:42 +1000

    fix: Fix test cases

--- testing/TestHarness/TestHarness.UITest/Ext/Navigation/PageNavigation/Given_PageNavigation.cs
+++ testing/TestHarness/TestHarness.UITest/Ext/Navigation/PageNavigation/Given_PageNavigation.cs
@@ -10,14 +10,14 @@ public class Given_PageNavigation : NavigationTestBase
 		App.WaitThenTap("ShowOnePageButton");
 
 		var screenBefore = TakeScreenshot("When_PageNavigationXAML_Before");
-		App.Tap("OnePageToTwoPageButton");
-		App.Tap("TwoPageToThreePageButton");
-		App.Tap("ThreePageToFourPageButton");
-		App.Tap("FourPageToFivePageButton");
-		App.Tap("FivePageBackButton");
-		App.Tap("FourPageBackButton");
-		App.Tap("ThreePageBackButton");
-		App.Tap("TwoPageBackButton");
+		App.WaitThenTap("OnePageToTwoPageButton");
+		App.WaitThenTap("TwoPageToThreePageButton");
+		App.WaitThenTap("ThreePageToFourPageButton");
+		App.WaitThenTap("FourPageToFivePageButton");
+		App.WaitThenTap("FivePageBackButton");
+		App.WaitThenTap("FourPageBackButton");
+		App.WaitThenTap("ThreePageBackButton");
+		App.WaitThenTap("TwoPageBackButton");
 
 
 		var screenAfter = TakeScreenshot("When_PageNavigationXAML_After");
@@ -33,14 +33,14 @@ public class Given_PageNavigation : NavigationTestBase
 		App.WaitThenTap("ShowOnePageButton");
 
 		var screenBefore = TakeScreenshot("When_PageNavigationCodebehind_Before");
-		App.Tap("OnePageToTwoPageCodebehindButton");
-		App.Tap("TwoPageToThreePageCodebehindButton");
-		App.Tap("ThreePageToFourPageCodebehindButton");
-		App.Tap("FourPageToFivePageCodebehindButton");
-		App.Tap("FivePageBackCodebehindButton");
-		App.Tap("FourPageBackCodebehindButton");
-		App.Tap("ThreePageBackCodebehindButton");
-		App.Tap("TwoPageBackCodebehindButton");
+		App.WaitThenTap("OnePageToTwoPageCodebehindButton");
+		App.WaitThenTap("TwoPageToThreePageCodebehindButton");
+		App.WaitThenTap("ThreePageToFourPageCodebehindButton");
+		App.WaitThenTap("FourPageToFivePageCodebehindButton");
+		App.WaitThenTap("FivePageBackCodebehindButton");
+		App.WaitThenTap("FourPageBackCodebehindButton");
+		App.WaitThenTap("ThreePageBackCodebehindButton");
+		App.WaitThenTap("TwoPageBackCodebehindButton");
 
 
 		var screenAfter = TakeScreenshot("When_PageNavigationCodebehind_After");
@@ -56,14 +56,14 @@ public class Given_PageNavigation : NavigationTestBase
 		App.WaitThenTap("ShowOnePageButton");
 
 		var screenBefore = TakeScreenshot("When_PageNavigationViewModel_Before");
-		App.Tap("OnePageToTwoPageViewModelButton");
-		App.Tap("TwoPageToThreePageViewModelButton");
-		App.Tap("ThreePageToFourPageViewModelButton");
-		App.Tap("FourPageToFivePageViewModelButton");
-		App.Tap("FivePageBackViewModelButton");
-		App.Tap("FourPageBackViewModelButton");
-		App.Tap("ThreePageBackViewModelButton");
-		App.Tap("TwoPageBackViewModelButton");
+		App.WaitThenTap("OnePageToTwoPageViewModelButton");
+		App.WaitThenTap("TwoPageToThreePageViewModelButton");
+		App.WaitThenTap("ThreePageToFourPageViewModelButton");
+		App.WaitThenTap("FourPageToFivePageViewModelButton");
+		App.WaitThenTap("FivePageBackViewModelButton");
+		App.WaitThenTap("FourPageBackViewModelButton");
+		App.WaitThenTap("ThreePageBackViewModelButton");
+		App.WaitThenTap("TwoPageBackViewModelButton");
 
 
 		var screenAfter = TakeScreenshot("When_PageNavigationViewModel_After");
