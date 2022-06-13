2897cafc98681b968132051727d74e5213d2ccf1
Nick Randolph <nick@builttoroam.com>
2022-06-13 21:02:48 +1000

    feat: Adding Auth libraries

---
+++ src/Uno.Extensions.Authentication/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using Microsoft.Extensions.DependencyInjection;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using System.Reflection;
