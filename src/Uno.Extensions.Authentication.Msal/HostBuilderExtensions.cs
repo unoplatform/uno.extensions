56d639c0458c698c313ced1762066ed1ec92a5b1
Nick Randolph <nick@builttoroam.com>
2022-06-15 23:27:24 +1000

    feat: Adding MSAL package

---
+++ src/Uno.Extensions.Authentication.Msal/HostBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Uno.Extensions.Authentication.MSAL;
+
+public static class HostBuilderExtensions
+{
+	//public static IHostBuilder UseAuthenticationFlow(
+	//	this IHostBuilder builder,
+	//	Action<IAuthenticationFlowBuilder>? configure = default)
+	//{
+	//	var authBuilder = builder.AsBuilder<AuthenticationFlowBuilder>();
+	//	configure?.Invoke(authBuilder);
+
+	//	return builder
+	//		.ConfigureServices(services =>
+	//		{
+	//			services.AddSingleton<IAuthenticationFlow, AuthenticationFlow>();
+	//			services.AddSingleton(authBuilder.Settings);
+	//		});
+	//}
+}
