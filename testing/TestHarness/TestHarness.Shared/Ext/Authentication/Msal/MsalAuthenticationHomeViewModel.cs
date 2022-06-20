d18bf254a5f124999b37283c6417fcbd48333dbc
Nick Randolph <nick@builttoroam.com>
2022-06-20 18:43:12 +1000

    feat: Testing refresh token

--- testing/TestHarness/TestHarness.Shared/Ext/Authentication/Msal/MsalAuthenticationHomeViewModel.cs
+++ testing/TestHarness/TestHarness.Shared/Ext/Authentication/Msal/MsalAuthenticationHomeViewModel.cs
@@ -9,19 +9,33 @@ public partial class MsalAuthenticationHomeViewModel : ObservableObject
 
 	public IMsalAuthenticationTaskListEndpoint TaskEndpoint { get; init; }
 
+	public ITokenCache Tokens { get; }
+
 	[ObservableProperty]
 	private MsalAuthenticationToDoTaskListData[]? tasks;
 
-	public MsalAuthenticationHomeViewModel(INavigator navigator, IAuthenticationFlow flow, IMsalAuthenticationTaskListEndpoint taskEndpoint)
+	public MsalAuthenticationHomeViewModel(
+		INavigator navigator,
+		IAuthenticationFlow flow,
+		IMsalAuthenticationTaskListEndpoint taskEndpoint,
+		ITokenCache tokens)
 	{
 		Navigator = navigator;
 		Flow = flow;
 		TaskEndpoint = taskEndpoint;
+		Tokens = tokens;
 	}
 
 	public async Task Logout()
 	{
 		await Flow.LogoutAsync(CancellationToken.None);
+	}
+
+	public async Task ClearAccessToken()
+	{
+		var creds = await Tokens.GetAsync();
+		creds.Remove(TokenCacheExtensions.AccessTokenKey);
+		await Tokens.SaveAsync(creds);
 	}
 
 	public async Task RetrieveTasks()
