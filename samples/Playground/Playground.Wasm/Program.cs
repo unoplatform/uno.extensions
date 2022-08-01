4becda9a68ba6523c88a9c44b1db0f81a3eec6a7
Nick Randolph <nick@builttoroam.com>
2022-08-01 17:00:22 +1000

    fix: Addressing warnings

--- samples/Playground/Playground.Wasm/Program.cs
+++ samples/Playground/Playground.Wasm/Program.cs
@@ -5,7 +5,7 @@ namespace Playground.Wasm
 {
     public class Program
     {
-        private static App _app;
+        private static App? _app;
 
         static int Main(string[] args)
         {
