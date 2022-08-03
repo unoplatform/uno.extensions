26bc26db87bf3fe3f385275d34f6de6dab1c4834
David <github.site@torick.net>
2022-08-03 15:08:47 -0400

    fix(reactive): Workaround compilation issue with AOT on Xamarin

--- src/Uno.Extensions.Reactive/Operators/FeedToListFeedAdapter.cs
+++ src/Uno.Extensions.Reactive/Operators/FeedToListFeedAdapter.cs
@@ -11,24 +11,41 @@ using Uno.Extensions.Reactive.Core;
 
 namespace Uno.Extensions.Reactive.Operators;
 
-internal record FeedToListFeedAdapter<T>(
-	IFeed<IImmutableList<T>> Source,
-	ItemComparer<T> ItemComparer = default)
-	: FeedToListFeedAdapter<IImmutableList<T>, T>(Source, list => list, ItemComparer);
-
-internal record FeedToListFeedAdapter<TCollection, TItem>(
-	IFeed<TCollection> Source,
-	Func<TCollection, IImmutableList<TItem>> ToImmutable,
-	ItemComparer<TItem> ItemComparer = default)
-	: IListFeed<TItem>
+internal class FeedToListFeedAdapter<T> : FeedToListFeedAdapter<IImmutableList<T>, T>
 {
-	private readonly CollectionAnalyzer<TItem> _analyzer = new(ItemComparer);
+	public FeedToListFeedAdapter(IFeed<IImmutableList<T>> source, ItemComparer<T> itemComparer = default)
+		: base(source, list => list, itemComparer)
+	{
+	}
+}
+
+// Note: This should **not** be a record as it causes some issues with mono runtime for Android and iOS
+// which crashes when using instances of this record in dictionaries (caching) and break the AOT build.
+// cf. https://github.com/unoplatform/Uno.Samples/issues/139
+
+internal class FeedToListFeedAdapter<TCollection, TItem> : IListFeed<TItem>
+{
+	private readonly IFeed<TCollection> _source;
+	private readonly Func<TCollection, IImmutableList<TItem>> _toImmutable;
+	private readonly ItemComparer<TItem> _itemComparer;
+	private readonly CollectionAnalyzer<TItem> _analyzer;
+
+	public FeedToListFeedAdapter(
+		IFeed<TCollection> source,
+		Func<TCollection, IImmutableList<TItem>> toImmutable,
+		ItemComparer<TItem> itemComparer = default)
+	{
+		_source = source;
+		_toImmutable = toImmutable;
+		_itemComparer = itemComparer;
+		_analyzer = new(itemComparer);
+	}
 
 	/// <inheritdoc />
 	public async IAsyncEnumerable<Message<IImmutableList<TItem>>> GetSource(SourceContext context, [EnumeratorCancellation] CancellationToken ct = default)
 	{
 		var localMsg = new MessageManager<TCollection, IImmutableList<TItem>>();
-		await foreach (var parentMsg in Source.GetSource(context, ct).WithCancellation(ct).ConfigureAwait(false))
+		await foreach (var parentMsg in _source.GetSource(context, ct).WithCancellation(ct).ConfigureAwait(false))
 		{
 			if (localMsg.Update(DoUpdate, parentMsg))
 			{
@@ -44,7 +61,7 @@ internal record FeedToListFeedAdapter<TCollection, TItem>(
 		if (parentMsg.Changes.Contains(MessageAxis.Data, out var changeSet))
 		{
 			var error = default(Exception);
-			var updatedData = parentMsg.Current.Data.Map(ToImmutable);
+			var updatedData = parentMsg.Current.Data.Map(_toImmutable);
 
 			if (updatedData.IsSome(out var items) && items is null or { Count: 0 })
 			{
@@ -55,7 +72,7 @@ internal record FeedToListFeedAdapter<TCollection, TItem>(
 			{
 				try // As we might invoke the app Equality implementation, we make sure to try / catch it
 				{
-					changeSet = GetChangeSet(_analyzer, parentMsg.Previous.Data.Map(ToImmutable), updatedData);
+					changeSet = GetChangeSet(_analyzer, parentMsg.Previous.Data.Map(_toImmutable), updatedData);
 				}
 				catch (Exception e)
 				{
@@ -90,10 +107,24 @@ internal record FeedToListFeedAdapter<TCollection, TItem>(
 		};
 	}
 
-	// Workaround an issue with mono runtime for Android and iOS which crashes when using instances of this record in dictionaries (caching).
-	// cf. https://github.com/unoplatform/Uno.Samples/issues/139
 	/// <inheritdoc />
 	public override int GetHashCode()
-		=> Source.GetHashCode() ^ ToImmutable.GetHashCode() ^ ItemComparer.GetHashCode();
+		=> _source.GetHashCode()
+			^ _toImmutable.GetHashCode()
+			^ _itemComparer.GetHashCode();
 
+	/// <inheritdoc />
+	public override bool Equals(object obj)
+		=> obj is FeedToListFeedAdapter<TCollection, TItem> other && Equals(this, other);
+
+	public static bool operator ==(FeedToListFeedAdapter<TCollection, TItem> left, FeedToListFeedAdapter<TCollection, TItem> right)
+		=> Equals(left, right);
+
+	public static bool operator !=(FeedToListFeedAdapter<TCollection, TItem> left, FeedToListFeedAdapter<TCollection, TItem> right)
+		=> Equals(left, right);
+
+	private protected static bool Equals(FeedToListFeedAdapter<TCollection, TItem> left, FeedToListFeedAdapter<TCollection, TItem> right)
+		=> left._source.Equals(right._source)
+			&& left._toImmutable.Equals(right._toImmutable)
+			&& left._itemComparer.Equals(right._itemComparer);
 }
