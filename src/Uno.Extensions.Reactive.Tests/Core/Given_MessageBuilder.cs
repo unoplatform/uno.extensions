917a20f8906d924d3a5f5fc0144d9a03a47e33e6
David <github.site@torick.net>
2022-08-25 10:53:03 -0400

    test(feeds): Add few tests to ensure behavior regarding transient axis on parent Feed

--- src/Uno.Extensions.Reactive.Tests/Core/Given_MessageBuilder.cs
+++ src/Uno.Extensions.Reactive.Tests/Core/Given_MessageBuilder.cs
@@ -44,6 +44,44 @@ public class Given_MessageBuilder : FeedTests
 	}
 
 	[TestMethod]
+	public async Task When_TransientAxisByParent_Then_NotDismissed_OfT()
+	{
+		var myAxis = new MessageAxis<object>("my test axis", _ => new object()) { IsTransient = true };
+		var parentMsg = Message<object>.Initial.With().Set(myAxis, new object()).Build();
+		var manager = new MessageManager<object, object>();
+		manager.Update(current => current.With(parentMsg), CT);
+		var original = manager.Current;
+
+		// Note: Even if we don't set any axis in Update, the Current message should be updated only by the fact that the transient axis has been automatically removed
+		MessageBuilder<object, object> builder = default;
+		manager.Update(current => builder = current.With(), CT);
+		var updated = manager.Current;
+
+		original.Current[myAxis].IsSet.Should().BeTrue();
+		builder.Get(myAxis).value.IsSet.Should().BeTrue("transient axis should have been kept as it was defined on parent");
+		updated.Current[myAxis].IsSet.Should().BeTrue("transient axis should have been kept as it was defined on parent");
+	}
+
+	[TestMethod]
+	public async Task When_TransientAxisByParentAndLocally_Then_NotDismissed_OfT()
+	{
+		var myAxis = new MessageAxis<object>("my test axis", _ => new object()) { IsTransient = true };
+		var parentMsg = Message<object>.Initial.With().Set(myAxis, new object()).Build();
+		var manager = new MessageManager<object, object>();
+		manager.Update(current => current.With(parentMsg).Set(myAxis, new object()), CT);
+		var original = manager.Current;
+
+		// Note: Even if we don't set any axis in Update, the Current message should be updated only by the fact that the transient axis has been automatically removed
+		MessageBuilder<object, object> builder = default;
+		manager.Update(current => builder = current.With(), CT);
+		var updated = manager.Current;
+
+		original.Current[myAxis].IsSet.Should().BeTrue();
+		builder.Get(myAxis).value.IsSet.Should().BeTrue("transient axis should have been kept as it was also defined on parent");
+		updated.Current[myAxis].IsSet.Should().BeTrue("transient axis should have been kept as it was also defined on parent");
+	}
+
+	[TestMethod]
 	public async Task When_TransientAxis_Then_Dismissed_TransactionOfT()
 	{
 		var myAxis = new MessageAxis<object>("my test axis", _ => new object()) { IsTransient = true };
@@ -61,5 +99,47 @@ public class Given_MessageBuilder : FeedTests
 		original.Current[myAxis].IsSet.Should().BeTrue();
 		builder.Get(myAxis).value.IsSet.Should().BeFalse("transient axis should have been cleared at beginning to reflect resulting state");
 		updated.Current[myAxis].IsSet.Should().BeFalse("transient axis should have been cleared on update");
+	}
+
+	[TestMethod]
+	public async Task When_TransientAxisByParent_Then_NotDismissed_TransactionOfT()
+	{
+		var myAxis = new MessageAxis<object>("my test axis", _ => new object()) { IsTransient = true };
+		var parentMsg = Message<object>.Initial.With().Set(myAxis, new object()).Build();
+		var manager = new MessageManager<object, object>();
+		manager.Update(current => current.With(parentMsg).Set(myAxis, new object()), CT);
+		var original = manager.Current;
+
+		// Note: Even if we don't set any axis in Update, the Current message should be updated only by the fact that the transient axis has been automatically removed
+		// Note 2: No needs to Commit the transaction, manager.Current should already reflect changes
+		MessageManager<object, object>.UpdateTransaction.MessageBuilder builder = default;
+		using var transaction = manager.BeginUpdate(CT);
+		transaction.Update(current => builder = current.With());
+		var updated = manager.Current;
+
+		original.Current[myAxis].IsSet.Should().BeTrue();
+		builder.Get(myAxis).value.IsSet.Should().BeTrue("transient axis should have been kept as it was defined on parent");
+		updated.Current[myAxis].IsSet.Should().BeTrue("transient axis should have been kept as it was defined on parent");
+	}
+
+	[TestMethod]
+	public async Task When_TransientAxisByParentAndLocally_Then_NotDismissed_TransactionOfT()
+	{
+		var myAxis = new MessageAxis<object>("my test axis", _ => new object()) { IsTransient = true };
+		var parentMsg = Message<object>.Initial.With().Set(myAxis, new object()).Build();
+		var manager = new MessageManager<object, object>();
+		manager.Update(current => current.With(parentMsg).Set(myAxis, new object()), CT);
+		var original = manager.Current;
+
+		// Note: Even if we don't set any axis in Update, the Current message should be updated only by the fact that the transient axis has been automatically removed
+		// Note 2: No needs to Commit the transaction, manager.Current should already reflect changes
+		MessageManager<object, object>.UpdateTransaction.MessageBuilder builder = default;
+		using var transaction = manager.BeginUpdate(CT);
+		transaction.Update(current => builder = current.With());
+		var updated = manager.Current;
+
+		original.Current[myAxis].IsSet.Should().BeTrue();
+		builder.Get(myAxis).value.IsSet.Should().BeTrue("transient axis should have been kept as it was also defined on parent");
+		updated.Current[myAxis].IsSet.Should().BeTrue("transient axis should have been kept as it was also defined on parent");
 	}
 }
