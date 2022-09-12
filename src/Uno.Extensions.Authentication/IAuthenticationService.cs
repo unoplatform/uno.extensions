62b830e8a2f5124369bacb365cc4f1074ca1cfaa
Nick Randolph <nick@builttoroam.com>
2022-09-12 16:06:14 +1000

    feat: Adding LoginResponse

--- src/Uno.Extensions.Authentication/IAuthenticationService.cs
+++ src/Uno.Extensions.Authentication/IAuthenticationService.cs
@@ -1,4 +1,6 @@
-﻿namespace Uno.Extensions.Authentication;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Uno.Extensions.Authentication;
 
 public interface IAuthenticationService
 {
@@ -8,4 +10,11 @@ public interface IAuthenticationService
 	ValueTask<bool> LogoutAsync(IDispatcher? dispatcher, CancellationToken? cancellationToken = default);
 	ValueTask<bool> IsAuthenticated(CancellationToken? cancellationToken = default);
 	event EventHandler LoggedOut;
+}
+
+
+public record LoginResponse(bool Success, ValidationResult[] Validations)
+{
+	public static implicit operator bool(LoginResponse response) => response.Success;
+	public static implicit operator LoginResponse(bool success) => new LoginResponse(success);
 }
