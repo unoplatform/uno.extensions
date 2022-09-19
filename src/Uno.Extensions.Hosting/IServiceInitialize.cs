bd20f7a7c20bfd425f8cdf4e7e356f257a5ad828
Nick Randolph <nick@builttoroam.com>
2022-09-19 11:06:55 +1000

    fix: Initializing language immediately after IHost is built

---
+++ src/Uno.Extensions.Hosting/IServiceInitialize.cs
@@ -0,0 +1,12 @@
+﻿namespace Uno.Extensions.Hosting;
+
+/// <summary>
+/// Defines services that require initialization immediately after the IHost is built
+/// </summary>
+public interface IServiceInitialize
+{
+	/// <summary>
+	/// Initializes service immediately after IHost is built
+	/// </summary>
+	void Initialize();
+}
