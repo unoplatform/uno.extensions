29ceda477a966fcd45f64fa737c696b1304b1456
Nick Randolph <nick@builttoroam.com>
2022-10-03 14:36:07 +1100

    feat: Exposing supported cultures and current culture on ILocalizationService

--- testing/TestHarness/TestHarness.Shared/Ext/Localization/LocalizationOneViewModel.cs
+++ testing/TestHarness/TestHarness.Shared/Ext/Localization/LocalizationOneViewModel.cs
@@ -8,25 +8,31 @@ namespace TestHarness.Ext.Navigation.Localization;
 public partial class LocalizationOneViewModel : ObservableObject
 {
 	private readonly IWritableOptions<LocalizationSettings> _localization;
+	private readonly ILocalizationService _localizationService;
 	public LocalizationOneViewModel(
+		ILocalizationService localizationService,
 		IOptions<LocalizationConfiguration> configuration,
 		IWritableOptions<LocalizationSettings> localization,
 		IStringLocalizer localizer)
 	{
+		_localizationService = localizationService;
 		_localization = localization;
-		SupportedCultures = configuration.Value?.Cultures?.AsCultures() ?? new[] { "en-US".AsCulture()! };
+		SupportedCultures = _localizationService.SupportedCultures;
 
-		var language = localizer[_localization.Value?.CurrentCulture ?? "en"];
+			//configuration.Value?.Cultures?.AsCultures() ?? new[] { "en-US".AsCulture()! };
+
+		var language = localizer[_localizationService.CurrentCulture.Name ?? "en"];
 	}
 
 	public CultureInfo[] SupportedCultures { get; }
 
 	public CultureInfo SelectedCulture
 	{
-		get => SupportedCultures.FirstOrDefault(x => x.Name == _localization.Value?.CurrentCulture) ?? SupportedCultures.First();
+		get => SupportedCultures.FirstOrDefault(x => x.Name == _localizationService.CurrentCulture.Name) ?? SupportedCultures.First();
 		set
 		{
-			_ = _localization.UpdateAsync(settings => settings with { CurrentCulture = value.Name });
+			_ = _localizationService.UpdateCurrentCulture(value);
+//			_ = _localization.UpdateAsync(settings => settings with { CurrentCulture = value.Name });
 		}
 	}
 
