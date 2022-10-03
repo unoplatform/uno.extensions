61a27b1d75ccc47744534fa6ef6864123cbcdd82
David <github.site@torick.net>
2022-10-03 15:38:43 -0400

    chore: Update Auth and Nav packages project to support Uno.Core code gen

--- src/Uno.Extensions.Navigation/AssemblyInfo.cs
+++ src/Uno.Extensions.Navigation/AssemblyInfo.cs
@@ -1,7 +1,9 @@
 ﻿using System.Runtime.CompilerServices;
+using Uno.Extensions.Equality;
 
 [assembly: InternalsVisibleTo("Uno.Extensions.Navigation.UI")]
 [assembly: InternalsVisibleTo("Uno.Extensions.Navigation.WinUI")]
 [assembly: InternalsVisibleTo("Uno.Extensions.Navigation.Toolkit.UI")]
 [assembly: InternalsVisibleTo("Uno.Extensions.Navigation.Toolkit.WinUI")]
 
+[assembly: ImplicitKeyEquality(IsEnabled = false)]
