ff61854503cb6505da1e41c631d8d7f22dfc66e8
David <github.site@torick.net>
2022-10-24 14:49:38 -0400

    feat(selection): Add the .Selection operator

--- src/Uno.Extensions.Reactive/Core/ListFeed.cs
+++ src/Uno.Extensions.Reactive/Core/ListFeed.cs
@@ -4,7 +4,6 @@ using System.Collections.Immutable;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading;
-using System.Threading.Tasks;
 using Uno.Extensions.Edition;
 using Uno.Extensions.Equality;
 using Uno.Extensions.Reactive.Operators;
@@ -95,5 +94,129 @@ public static partial class ListFeed
 		AsyncFunc<TSource?, TResult?> selector)
 		=> default!);
 	*/
+
+	/// <summary>
+	/// Creates a ListState from a ListFeed onto which the selected items is being synced with the provided external state.
+	/// </summary>
+	/// <typeparam name="TSource">Type of the items of the list feed.</typeparam>
+	/// <param name="source">The source list feed.</param>
+	/// <param name="selectionState">The external state from and onto which the selection of the resulting list state is going to be synced.</param>
+	/// <param name="caller">For debug purposes, the name of this subscription. DO NOT provide anything here, let the compiler provide this.</param>
+	/// <param name="line">For debug purposes, the name of this subscription. DO NOT provide anything here, let the compiler provide this.</param>
+	/// <returns>A ListState from and onto which the selection is going to be synced.</returns>
+	public static IListState<TSource> Selection<TSource>(
+		this IListFeed<TSource> source,
+		IState<IImmutableList<TSource>> selectionState,
+		[CallerMemberName] string caller = "",
+		[CallerLineNumber] int line = -1)
+		=> AttachedProperty.GetOrCreate(
+			source,
+			(selectionState, caller, line),
+			static (src, args) => ListFeedSelection<TSource>.Create(src, args.selectionState, $"Selection defined in {args.caller} at line {args.line}."));
+
+	/// <summary>
+	/// Creates a ListState from a ListFeed onto which the selected items is being synced with the provided external state.
+	/// </summary>
+	/// <typeparam name="TSource">Type of the items of the list feed.</typeparam>
+	/// <param name="source">The source list feed.</param>
+	/// <param name="selectionState">The external state from and onto which the selection of the resulting list state is going to be synced.</param>
+	/// <param name="caller">For debug purposes, the name of this subscription. DO NOT provide anything here, let the compiler provide this.</param>
+	/// <param name="line">For debug purposes, the name of this subscription. DO NOT provide anything here, let the compiler provide this.</param>
+	/// <returns>A ListState from and onto which the selection is going to be synced.</returns>
+	public static IListState<TSource> Selection<TSource>(
+		this IListFeed<TSource> source,
+		IState<TSource> selectionState,
+		[CallerMemberName] string caller = "",
+		[CallerLineNumber] int line = -1)
+		=> AttachedProperty.GetOrCreate(
+			source,
+			(selectionState, caller, line),
+			static (src, args) => ListFeedSelection<TSource>.Create(src, args.selectionState, $"Selection defined in {args.caller} at line {args.line}."));
+
+	/// <summary>
+	/// Creates a ListState from a ListFeed onto which the selected item is being synced with the provided external state using projection.
+	/// </summary>
+	/// <typeparam name="TSource">Type of the items of the list feed.</typeparam>
+	/// <typeparam name="TSourceKey">Type of the key of items of the list feed.</typeparam>
+	/// <typeparam name="TOther">Type of the entity which hold the selection.</typeparam>
+	/// <param name="source">The source list feed.</param>
+	/// <param name="selectionState">The external state from and onto which the selection of the resulting list state is going to be synced.</param>
+	/// <param name="keySelector">The selector to get and set the key of the selected item on a <typeparamref name="TOther"/>.</param>
+	/// <param name="path">
+	/// The path of the file where this operator is being used.
+	/// This is used to resolve the <paramref name="keySelector"/> (cf. <see cref="PropertySelector{TEntity,TValue}"/> for more info).
+	/// DO NOT provide anything here, let the compiler provide this.
+	/// </param>
+	/// <param name="caller">For debug purposes, the name of this subscription. DO NOT provide anything here, let the compiler provide this.</param>
+	/// <param name="line">
+	/// The line number of the file where this operator is being used.
+	/// This is used to resolve the <paramref name="keySelector"/> (cf. <see cref="PropertySelector{TEntity,TValue}"/> for more info).
+	/// DO NOT provide anything here, let the compiler provide this.
+	/// </param>
+	/// <returns>A ListState from and onto which the selection is going to be synced.</returns>
+	public static IListState<TSource> Selection<TSource, TSourceKey, TOther>(
+		this IListFeed<TSource> source,
+		IState<TOther> selectionState,
+		PropertySelector<TOther, TSourceKey?> keySelector,
+		[CallerFilePath] string path = "",
+		[CallerMemberName] string caller = "",
+		[CallerLineNumber] int line = -1)
+		where TSource : IKeyed<TSourceKey>
+		where TSourceKey : notnull
+		where TOther : new()
+		=> AttachedProperty.GetOrCreate(
+			source,
+			(selectionState, keySelector, path, caller, line),
+			static (src, args) => ListFeedSelection<TSource>.Create(
+				src,
+				args.selectionState,
+				i => i.Key,
+				PropertySelectors.Get(args.keySelector, nameof(keySelector), args.path, args.line),
+				() => new(),
+				default,
+				$"Selection defined in {args.caller} at line {args.line}."));
+
+	/// <summary>
+	/// Creates a ListState from a ListFeed onto which the selected item is being synced with the provided external state using projection.
+	/// </summary>
+	/// <typeparam name="TSource">Type of the items of the list feed.</typeparam>
+	/// <typeparam name="TSourceKey">Type of the key of items of the list feed.</typeparam>
+	/// <typeparam name="TOther">Type of the entity which hold the selection.</typeparam>
+	/// <param name="source">The source list feed.</param>
+	/// <param name="selectionState">The external state from and onto which the selection of the resulting list state is going to be synced.</param>
+	/// <param name="keySelector">The selector to get and set the key of the selected item on a <typeparamref name="TOther"/>.</param>
+	/// <param name="path">
+	/// The path of the file where this operator is being used.
+	/// This is used to resolve the <paramref name="keySelector"/> (cf. <see cref="PropertySelector{TEntity,TValue}"/> for more info).
+	/// DO NOT provide anything here, let the compiler provide this.
+	/// </param>
+	/// <param name="caller">For debug purposes, the name of this subscription. DO NOT provide anything here, let the compiler provide this.</param>
+	/// <param name="line">
+	/// The line number of the file where this operator is being used.
+	/// This is used to resolve the <paramref name="keySelector"/> (cf. <see cref="PropertySelector{TEntity,TValue}"/> for more info).
+	/// DO NOT provide anything here, let the compiler provide this.
+	/// </param>
+	/// <returns>A ListState from and onto which the selection is going to be synced.</returns>
+	public static IListState<TSource> Selection<TSource, TSourceKey, TOther>(
+		this IListFeed<TSource> source,
+		IState<TOther> selectionState,
+		PropertySelector<TOther, TSourceKey?> keySelector,
+		[CallerFilePath] string path = "",
+		[CallerMemberName] string caller = "",
+		[CallerLineNumber] int line = -1)
+		where TSource : IKeyed<TSourceKey>
+		where TSourceKey : struct
+		where TOther : new()
+		=> AttachedProperty.GetOrCreate(
+			source,
+			(selectionState, keySelector, path, caller, line),
+			static (src, args) => ListFeedSelection<TSource>.CreateValueType(
+				src,
+				args.selectionState,
+				i => i.Key,
+				PropertySelectors.Get(args.keySelector, nameof(keySelector), args.path, args.line),
+				() => new(),
+				default,
+				$"Selection defined in {args.caller} at line {args.line}."));
 	#endregion
 }
