6c9a55a5ef480cbeb85c017c0c6334e2e3afea4b
Nick Randolph <nick@builttoroam.com>
2022-11-11 14:11:11 +1100

    fix: Enabling all ui tests

--- testing/TestHarness/TestHarness.UITest/Ext/Navigation/PageNavigation/Given_PageNavigation.cs
+++ testing/TestHarness/TestHarness.UITest/Ext/Navigation/PageNavigation/Given_PageNavigation.cs
@@ -52,7 +52,7 @@ public class Given_PageNavigation : NavigationTestBase
 	}
 
 	[Test]
-	[Ignore("Failing CI - runs ok locally")]
+	//[Ignore("Failing CI - runs ok locally")]
 	public void When_PageNavigationViewModel()
 	{
 		InitTestSection(TestSections.Navigation_PageNavigation);
