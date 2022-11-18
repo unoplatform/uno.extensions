aa2d95f48d191fd466640aa67d6a44da0e2a7e87
David <github.site@torick.net>
2022-11-18 11:29:30 -0500

    feat: Add runtime tests app

---
+++ src/Uno.Extensions.Reactive.UI.Tests/Given_FeedView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Uno.Extensions.Reactive.UI;
+
+namespace Uno.Extensions.Reactive.WinUI.Tests;
+
+[TestClass]
+public class Given_FeedView
+{
+	[TestMethod]
+	public void Bla()
+	{
+		var sut = new FeedView();
+
+
+
+	}
+}
