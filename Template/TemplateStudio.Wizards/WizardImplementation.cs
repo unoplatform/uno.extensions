f7ab869ec77ccecd67a22659514844b3b06db118
Nick Randolph <nick@builttoroam.com>
2022-12-06 11:48:05 +1100

    fix: Adding nuget package and passing replacements through

--- Template/TemplateStudio.Wizards/WizardImplementation.cs
+++ Template/TemplateStudio.Wizards/WizardImplementation.cs
@@ -75,21 +75,22 @@ namespace TemplateStudio.Wizards
 				//window.ShowDialog();
 
 
-				//replacementsDictionary.Add("passthrough:skia-gtk", page1.ContextViewModel.DataReplacement.skiaGtk.ToString());
-				//replacementsDictionary.Add("passthrough:wasm", page1.ContextViewModel.DataReplacement.wasm.ToString());
-				//replacementsDictionary.Add("passthrough:ios", page1.ContextViewModel.DataReplacement.ios.ToString());
-				//replacementsDictionary.Add("passthrough:android", page1.ContextViewModel.DataReplacement.android.ToString());
-				//replacementsDictionary.Add("passthrough:macos", page1.ContextViewModel.DataReplacement.macos.ToString());
-				//replacementsDictionary.Add("passthrough:maccatalyst", page1.ContextViewModel.DataReplacement.maccatalyst.ToString());
-				//replacementsDictionary.Add("passthrough:tests", page1.ContextViewModel.DataReplacement.tests.ToString());
-				//replacementsDictionary.Add("passthrough:skia-wpf", page1.ContextViewModel.DataReplacement.skiaWpf.ToString());
-				//replacementsDictionary.Add("passthrough:skia-linux-fb", page1.ContextViewModel.DataReplacement.skiaLinuxFb.ToString());
-				//replacementsDictionary.Add("passthrough:winAppSdk", page1.ContextViewModel.DataReplacement.winAppSdk.ToString());
-				//replacementsDictionary.Add("passthrough:reactive", page1.ContextViewModel.DataReplacement.reactive.ToString());
-				//replacementsDictionary.Add("passthrough:cpm", page1.ContextViewModel.DataReplacement.cpm.ToString());
-				//replacementsDictionary.Add("passthrough:wasm-pwa-manifest", page1.ContextViewModel.DataReplacement.wasmPpwaManifest.ToString());
-				//replacementsDictionary.Add("passthrough:vscode", page1.ContextViewModel.DataReplacement.vscode.ToString());
-				//replacementsDictionary.Add("passthrough:skipRestore", page1.ContextViewModel.DataReplacement.skiaGtk.ToString());
+				replacementsDictionary.Add("passthrough:skia-gtk", page1.ContextViewModel.DataReplacement.skiaGtk.ToString());
+				replacementsDictionary.Add("passthrough:wasm", page1.ContextViewModel.DataReplacement.wasm.ToString());
+				replacementsDictionary.Add("passthrough:ios", page1.ContextViewModel.DataReplacement.ios.ToString());
+				replacementsDictionary.Add("passthrough:android", page1.ContextViewModel.DataReplacement.android.ToString());
+				replacementsDictionary.Add("passthrough:macos", page1.ContextViewModel.DataReplacement.macos.ToString());
+				replacementsDictionary.Add("passthrough:maccatalyst", page1.ContextViewModel.DataReplacement.maccatalyst.ToString());
+				replacementsDictionary.Add("passthrough:tests", page1.ContextViewModel.DataReplacement.tests.ToString());
+				replacementsDictionary.Add("passthrough:skia-wpf", page1.ContextViewModel.DataReplacement.skiaWpf.ToString());
+				replacementsDictionary.Add("passthrough:skia-linux-fb", page1.ContextViewModel.DataReplacement.skiaLinuxFb.ToString());
+				replacementsDictionary.Add("passthrough:winAppSdk", page1.ContextViewModel.DataReplacement.winAppSdk.ToString());
+				replacementsDictionary.Add("passthrough:reactive", page1.ContextViewModel.DataReplacement.reactive.ToString());
+				replacementsDictionary.Add("passthrough:cpm", page1.ContextViewModel.DataReplacement.cpm.ToString());
+				replacementsDictionary.Add("passthrough:wasm-pwa-manifest", page1.ContextViewModel.DataReplacement.wasmPpwaManifest.ToString());
+				replacementsDictionary.Add("passthrough:vscode", page1.ContextViewModel.DataReplacement.vscode.ToString());
+				replacementsDictionary.Add("passthrough:skipRestore", page1.ContextViewModel.DataReplacement.skiaGtk.ToString());
+				replacementsDictionary.Add("passthrough:is-visx", true.ToString());
 
 
 			}
