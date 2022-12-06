ebc2b9954e469f17f2aab79ada21698859e150b6
Nick Randolph <nick@builttoroam.com>
2022-12-06 15:29:44 +1100

    fix: Getting Uno to build

---
+++ Template/TemplateStudio.Wizards/Uno/MainUnoPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TemplateStudio.Wizards
+{
+	public partial class MainUnoPage : Page
+	{
+		public MainUnoPage()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
