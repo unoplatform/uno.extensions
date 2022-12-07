94f393f916c714bd3af9627537b9f04b6d1d5d04
Ricardo <arquero_r@hotmail.com>
2022-12-07 13:22:39 -0600

    feat: included MvvmToolkit support

---
+++ src/Uno.Extensions.Validation.CommunityToolkit/CommunityToolkitValidator.cs
@@ -0,0 +1,22 @@
+﻿namespace Uno.Extensions.Validation;
+
+/// <summary>
+/// Class that can be used to validate objects, properties and methods based for a given ObservableValidator class. 
+/// </summary>
+public class CommunityToolkitValidator<T>: IValidator<T> where T : ObservableValidator
+{
+	///<inheritdoc/>
+	public ValueTask<IEnumerable<ValidationResult>> ValidateAsync(
+		T instance,
+		ValidationContext? context = null,
+		CancellationToken cancellationToken = default)
+	{
+		ICollection<ValidationResult> results = new List<ValidationResult>();
+		foreach (var error in instance.GetErrors())
+		{
+			results.Add(new ValidationResult(error.ErrorMessage));
+		}
+
+		return new ValueTask<IEnumerable<ValidationResult>>(results.ToList());
+	}
+}
---
+++ src/Uno.Extensions.Validation.CommunityToolkit/HostBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Uno.Extensions.Validation;
+
+public static class HostBuilderExtensions
+{
+	public static IHostBuilder UseCommunityToolkitValidation(
+	this IHostBuilder hostBuilder)
+	{
+		return hostBuilder
+		.ConfigureServices((ctx, services) =>
+		{
+			_ = services
+			.AddScoped(typeof(IValidator<>), typeof(CommunityToolkitValidator<>));
+		});
+	}
+}
