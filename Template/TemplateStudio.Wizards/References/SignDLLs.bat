42b513a82508f8fa31e778c1bf071d339fd34268
rafael-rosa-knowcode <116665025+rafael-rosa-knowcode@users.noreply.github.com>
2022-12-07 11:15:47 -0300

    feat: Script to Sign DLLs on project

---
+++ Template/TemplateStudio.Wizards/References/SignDLLs.bat
@@ -0,0 +1,13 @@
+﻿
+#copy files to Unsigned folder, to by Sign Assemblies 
+copy /Y ..\bin\Debug\net472\TemplateStudio.Wizards.dll DLLs\Unsigned\
+copy /Y ..\bin\Debug\net472\UNO*.dll DLLs\Unsigned\
+copy /Y C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msc*.dll DLLs\Unsigned\
+copy /Y C:\Windows\Microsoft.NET\Framework64\v4.0.30319\netstandard.dll DLLs\Unsigned\
+copy /Y C:\Windows\Microsoft.NET\Framework64\v4.0.30319\System*.dll DLLs\Unsigned\
+
+#Strong-name sign all assemblies DLL found in DLLs\Unsigned\ and copy the signed versions to DLLs\Signed with your own personal strong-name key.snk file
+"C:\Program Files\BrutalDev\.NET Assembly Strong-Name Signer\StrongNameSigner.Console.exe" -in DLLs\Unsigned\ -out DLLs\Signed  -k ..\Keys\key.snk
+
+#Copy the files from the signed directory into the "Libs" folder
+copy /Y DLLs\Signed  ..\..\Libs
