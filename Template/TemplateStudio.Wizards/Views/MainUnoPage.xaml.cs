da48a43712f2af1428b7a4a1d53f2502bb79ab56
rafael-rosa-knowcode <116665025+rafael-rosa-knowcode@users.noreply.github.com>
2022-12-08 15:45:18 -0300

    Feat: clean and optimize solution

---
+++ Template/TemplateStudio.Wizards/Views/MainUnoPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using TemplateStudio.Wizards.ViewModel;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TemplateStudio.Wizards
+{
+	public partial class MainUnoPage : Page
+	{
+		SequentialFlowvViewModel sequentialFlowvViewModel = null;
+		public MainUnoPage()
+		{
+			sequentialFlowvViewModel = new SequentialFlowvViewModel();
+			DataContext = this;
+			this.InitializeComponent();
+		}
+	}
+}
--- Template/TemplateStudio.Wizards/WizardImplementation.cs
+++ Template/TemplateStudio.Wizards/WizardImplementation.cs
@@ -16,7 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using TemplateStudio.Wizards.Views;
+using TemplateStudio.Wizards.Host;
 using Microsoft.VisualStudio.Telemetry;
 using TemplateStudio.Wizards.Helpers;
 using TemplateStudio.Wizards.Model;
@@ -58,38 +58,27 @@ namespace TemplateStudio.Wizards
 		{
 			try
 			{
+				var host = new WizardHost();
+				host.Focus();
+				UI.ShowModal(host);
+
+				replacementsDictionary.Add("passthrough:skia-gtk", host.ContextViewModel.DataReplacement.skiaGtk.ToString());
+				replacementsDictionary.Add("passthrough:wasm", host.ContextViewModel.DataReplacement.wasm.ToString());
+				replacementsDictionary.Add("passthrough:ios", host.ContextViewModel.DataReplacement.ios.ToString());
+				replacementsDictionary.Add("passthrough:android", host.ContextViewModel.DataReplacement.android.ToString());
+				replacementsDictionary.Add("passthrough:macos", host.ContextViewModel.DataReplacement.macos.ToString());
+				replacementsDictionary.Add("passthrough:maccatalyst", host.ContextViewModel.DataReplacement.maccatalyst.ToString());
+				replacementsDictionary.Add("passthrough:tests", host.ContextViewModel.DataReplacement.tests.ToString());
+				replacementsDictionary.Add("passthrough:skia-wpf", host.ContextViewModel.DataReplacement.skiaWpf.ToString());
+				replacementsDictionary.Add("passthrough:skia-linux-fb", host.ContextViewModel.DataReplacement.skiaLinuxFb.ToString());
+				replacementsDictionary.Add("passthrough:winAppSdk", host.ContextViewModel.DataReplacement.winAppSdk.ToString());
+				replacementsDictionary.Add("passthrough:reactive", host.ContextViewModel.DataReplacement.reactive.ToString());
+				replacementsDictionary.Add("passthrough:wasm-pwa-manifest", host.ContextViewModel.DataReplacement.wasmPpwaManifest.ToString());
+				replacementsDictionary.Add("passthrough:vscode", host.ContextViewModel.DataReplacement.vscode.ToString());
 
 
-				// Display a form to the user. The form collects
-				// input for the custom message.
-				//inputForm = new UserInputForm();
-				//inputForm.ShowDialog();
-
-				var page1 = new MainPage();
-				//var page1 = new MainUnoPage();
-				page1.Focus();
-				UI.ShowModal(page1);
-
-				//Page1 window = new Page1();
-
-				//window.ShowDialog();
-
-
-				replacementsDictionary.Add("passthrough:skia-gtk", page1.ContextViewModel.DataReplacement.skiaGtk.ToString());
-				replacementsDictionary.Add("passthrough:wasm", page1.ContextViewModel.DataReplacement.wasm.ToString());
-				replacementsDictionary.Add("passthrough:ios", page1.ContextViewModel.DataReplacement.ios.ToString());
-				replacementsDictionary.Add("passthrough:android", page1.ContextViewModel.DataReplacement.android.ToString());
-				replacementsDictionary.Add("passthrough:macos", page1.ContextViewModel.DataReplacement.macos.ToString());
-				replacementsDictionary.Add("passthrough:maccatalyst", page1.ContextViewModel.DataReplacement.maccatalyst.ToString());
-				replacementsDictionary.Add("passthrough:tests", page1.ContextViewModel.DataReplacement.tests.ToString());
-				replacementsDictionary.Add("passthrough:skia-wpf", page1.ContextViewModel.DataReplacement.skiaWpf.ToString());
-				replacementsDictionary.Add("passthrough:skia-linux-fb", page1.ContextViewModel.DataReplacement.skiaLinuxFb.ToString());
-				replacementsDictionary.Add("passthrough:winAppSdk", page1.ContextViewModel.DataReplacement.winAppSdk.ToString());
-				replacementsDictionary.Add("passthrough:reactive", page1.ContextViewModel.DataReplacement.reactive.ToString());
-				replacementsDictionary.Add("passthrough:cpm", page1.ContextViewModel.DataReplacement.cpm.ToString());
-				replacementsDictionary.Add("passthrough:wasm-pwa-manifest", page1.ContextViewModel.DataReplacement.wasmPpwaManifest.ToString());
-				replacementsDictionary.Add("passthrough:vscode", page1.ContextViewModel.DataReplacement.vscode.ToString());
-				replacementsDictionary.Add("passthrough:skipRestore", page1.ContextViewModel.DataReplacement.skiaGtk.ToString());
+				//replacementsDictionary.Add("passthrough:cpm", host.ContextViewModel.DataReplacement.cpm.ToString());
+				//replacementsDictionary.Add("passthrough:skipRestore", false.ToString());
 				replacementsDictionary.Add("passthrough:is-visx", true.ToString());
 
 
