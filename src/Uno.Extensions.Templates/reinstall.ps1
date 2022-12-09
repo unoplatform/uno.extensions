93cb7180cf581a49e851f21c53f21893a3e746ec
Dan Siegel <me@dansiegel.net>
2022-12-09 08:51:35 -0800

    chore: cleaning up template

--- src/Uno.Extensions.Templates/reinstall.ps1
+++ src/Uno.Extensions.Templates/reinstall.ps1
@@ -1,3 +1,7 @@
-﻿dotnet new uninstall Uno.Extensions.Templates
-dotnet pack
-dotnet new install .\bin\Uno.Extensions.Templates\Debug\Uno.Extensions.Templates.255.255.255.255.nupkg
+﻿param(
+    # Version of published Uno.Extensions packages
+    [string]$Version = "255.255.255.255"
+)
+dotnet new uninstall Uno.Extensions.Templates
+dotnet pack -p:Version=$Version
+dotnet new install .\bin\Uno.Extensions.Templates\Debug\Uno.Extensions.Templates.$Version.nupkg
