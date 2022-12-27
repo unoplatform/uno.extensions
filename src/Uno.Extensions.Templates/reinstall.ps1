152ad72eca80b0ae064e8d153ac45e92d0908f2d
Dan Siegel <me@dansiegel.net>
2022-12-27 08:24:04 -0600

    fix: resource paths

--- src/Uno.Extensions.Templates/reinstall.ps1
+++ src/Uno.Extensions.Templates/reinstall.ps1
@@ -4,4 +4,4 @@
 )
 dotnet new uninstall Uno.Extensions.Templates
 dotnet pack -p:Version=$Version
-dotnet new install .\bin\Uno.Extensions.Templates\Debug\Uno.Extensions.Templates.$Version.nupkg
+dotnet new install $PSScriptRoot\bin\Uno.Extensions.Templates\Debug\Uno.Extensions.Templates.$Version.nupkg
