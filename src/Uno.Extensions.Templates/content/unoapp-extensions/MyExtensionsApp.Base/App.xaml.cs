ae4ca638bb1170b418523501254a355a50ad7621
Dan Siegel <me@dansiegel.net>
2022-12-28 10:46:50 -0600

    chore: add frame navigation with extensions

--- src/Uno.Extensions.Templates/content/unoapp-extensions/MyExtensionsApp.Base/App.xaml.cs
+++ src/Uno.Extensions.Templates/content/unoapp-extensions/MyExtensionsApp.Base/App.xaml.cs
@@ -210,6 +210,7 @@ public sealed partial class App : global::Microsoft.UI.Xaml.Application
 					services
 //-:cnd:noEmit
 #if DEBUG
+						// DelegatingHandler will be automatically injected into Refit Client
 						.AddTransient<DelegatingHandler, DebugHttpHandler>()
 #endif
 //+:cnd:noEmit
@@ -228,6 +229,42 @@ public sealed partial class App : global::Microsoft.UI.Xaml.Application
 		_window = builder.Window;
 
 #if use-frame-nav
+//-:cnd:noEmit
+		_host = builder.Build();
+
+		// Do not repeat app initialization when the Window already has content,
+		// just ensure that the window is active
+		if (_window.Content is not Frame rootFrame)
+		{
+			// Create a Frame to act as the navigation context and navigate to the first page
+			rootFrame = new Frame();
+
+			rootFrame.NavigationFailed += OnNavigationFailed;
+
+			if (args.UWPLaunchActivatedEventArgs.PreviousExecutionState == ApplicationExecutionState.Terminated)
+			{
+				// TODO: Load state from previously suspended application
+			}
+
+			// Place the frame in the current Window
+			_window.Content = rootFrame;
+		}
+
+#if !(NET6_0_OR_GREATER && WINDOWS)
+		if (args.UWPLaunchActivatedEventArgs.PrelaunchActivated == false)
+#endif
+		{
+			if (rootFrame.Content == null)
+			{
+				// When the navigation stack isn't restored navigate to the first page,
+				// configuring the new page by passing required information as a navigation
+				// parameter
+				rootFrame.Navigate(typeof(MainPage), args.Arguments);
+			}
+			// Ensure the current window is active
+			_window.Activate();
+		}
+//+:cnd:noEmit
 #else
 		_host = await builder.NavigateAsync<Shell>();
 #endif
