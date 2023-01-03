24d152d0daf135207f5c0f01b09b9b6137c76a9e
Dan Siegel <me@dansiegel.net>
2023-01-03 15:04:58 -0600

    chore: update package file bundling

--- src/Uno.Extensions.Templates/reinstall.ps1
+++ src/Uno.Extensions.Templates/reinstall.ps1
@@ -3,5 +3,8 @@
     [string]$Version = "255.255.255.255"
 )
 dotnet new uninstall Uno.Extensions.Templates
-dotnet pack -p:Version=$Version
-dotnet new install $PSScriptRoot\bin\Uno.Extensions.Templates\Debug\Uno.Extensions.Templates.$Version.nupkg
+dotnet build -p:Version=$Version -c Release
+if($LASTEXITCODE -ne 0) {
+    exit $LASTEXITCODE
+}
+dotnet new install $PSScriptRoot\bin\Uno.Extensions.Templates\Release\Uno.Extensions.Templates.$Version.nupkg
