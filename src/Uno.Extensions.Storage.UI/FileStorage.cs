0da073279c0c393ddfabcdf269c758f3a9d5dfaf
Ricardo <arquero_r@hotmail.com>
2023-01-04 09:08:30 -0600

    fix: updated Toolkit version and implemented StorageFileHelper

--- src/Uno.Extensions.Storage.UI/FileStorage.cs
+++ src/Uno.Extensions.Storage.UI/FileStorage.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Reflection;
+using Uno.UI.Toolkit;
 #if __ANDROID__
 using Android.Content.Res;
 #elif __IOS__ || MACCATALYST || MACOS
@@ -10,112 +11,6 @@ namespace Uno.Extensions.Storage;
 
 public class FileStorage : IStorage
 {
-	private async Task<bool> FileExistsInPackage(string filename)
-	{
-#if __ANDROID__
-		//Look in assets first***
-		var context = global::Android.App.Application.Context;
-		var assets = context.Assets;
-
-		//Get only the filename with extension and apply internal UNO naming rules so we are able to find the compiled resources by replacing '.' and '-' by underscore.
-		var normalizedFileName = Path.GetFileNameWithoutExtension(filename).Replace('.', '_').Replace('-','_') + Path.GetExtension(filename);
-		var files = new List<string>();
-		ScanPackageAssets(assets, files);
-		if (files.Contains(normalizedFileName))
-		{
-			return true;
-		}
-
-		//If not asset found and we detect "/" in filename means we are looking for a nested resource file.
-		//In this case we need to replace '/, -, .' by '_' and remove filename extension before trying to pull it from app resources.
-		var nameArray = filename.ToLower().Split("/")?.ToList();
-		var normalizedResName = Path.GetFileNameWithoutExtension(filename).Replace('.', '_').Replace('-','_');
-		if(nameArray?.Count() > 1 )
-		{
-			//Replace original filename in our array by our normalized resource filename.
-			nameArray[nameArray.Count() -1] = normalizedResName;
-			//Join our normalized elements without extension
-			normalizedResName = string.Join("_", nameArray);
-		}
-
-		//Look in drawable resources***
-		var resources = context.Resources;
-		int resId =0;
-		resId = resources?.GetIdentifier(normalizedResName, "drawable", context.PackageName) ?? 0;
-		if (resId != 0)
-		{
-			return true;
-		}
-
-		//Look in mipmap resources***
-		resId = resources?.GetIdentifier(normalizedResName, "mipmap", context.PackageName) ?? 0;
-		if (resId != 0)
-		{
-			return true;
-		}
-
-		
-
-		return false;
-#elif __IOS__ || MACCATALYST || MACOS
-		var directoryName = global::System.IO.Path.GetDirectoryName(filename) + string.Empty;
-		var fileName = global::System.IO.Path.GetFileNameWithoutExtension(filename);
-		var fileExtension = global::System.IO.Path.GetExtension(filename);
-
-		var resourcePathname = NSBundle.MainBundle.PathForResource(global::System.IO.Path.Combine(directoryName, fileName), fileExtension.Substring(1));
-
-		return resourcePathname != null;
-#elif WINDOWS
-		var executingPath = Assembly.GetExecutingAssembly().Location;
-		if (!string.IsNullOrWhiteSpace(executingPath))
-		{
-			var path = Path.GetDirectoryName(executingPath);
-			if (path is not null &&
-				!string.IsNullOrWhiteSpace(path))
-			{
-				var fullPath = Path.Combine(path, filename);
-				return File.Exists(fullPath);
-			}
-		}
-		return true;
-#else
-		return true;
-#endif
-
-	}
-
-#if __ANDROID__
-	//This method will scan for all the assets within current package
-	//This method will return a list of {file}.{extension}
-	private bool ScanPackageAssets(AssetManager? assets, List<string> files, string rootPath = "")
-	{
-		try
-		{
-			var Paths = assets?.List(rootPath);
-			if (Paths?.Length > 0)
-			{
-				foreach (var file in Paths)
-				{
-					string path = string.IsNullOrWhiteSpace(rootPath) ? file : Path.Combine(rootPath, file);
-					if (!ScanPackageAssets(assets, files, path))
-					{
-						return false;
-					}
-
-					if (path.Contains('.'))
-					{
-						files.Add(Path.GetFileNameWithoutExtension(path) + Path.GetExtension(path));
-					}
-				}
-			}
-			return true;
-		}
-		catch {
-			return false;
-		}
-	}
-#endif
-
 	public async Task<string> CreateFolderAsync(string foldername)
 	{
 		var localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
@@ -127,10 +22,11 @@ public class FileStorage : IStorage
 	{
 		try
 		{
-			if (!await FileExistsInPackage(filename))
+			if (!await StorageFileHelper.ExistsInPackage(filename))
 			{
 				return default;
 			}
+
 			var storageFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri($"ms-appx:///{filename}"));
 			if (File.Exists(storageFile.Path))
 			{
@@ -151,7 +47,7 @@ public class FileStorage : IStorage
 	{
 		try
 		{
-			if (!await FileExistsInPackage(filename))
+			if (!await StorageFileHelper.ExistsInPackage(filename))
 			{
 				return default;
 			}
