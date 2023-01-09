3942420d73b0e1c45b301a1939947d71483387cd
Nick Randolph <nick@builttoroam.com>
2023-01-09 15:42:58 +1100

    feat: Updating to latest uno packages and changing FileStorage to use StorageHelpers

--- src/Uno.Extensions.Storage.UI/FileStorage.cs
+++ src/Uno.Extensions.Storage.UI/FileStorage.cs
@@ -1,46 +1,8 @@
-﻿
-using System.Reflection;
-#if __IOS__ || MACCATALYST || MACOS
-using Foundation;
-#endif
-
-namespace Uno.Extensions.Storage;
+﻿namespace Uno.Extensions.Storage;
 
 public class FileStorage : IStorage
 {
-	private async Task<bool> FileExistsInPackage(string filename)
-	{
-#if __ANDROID__
-		var assets = global::Android.App.Application.Context.Assets;
-		var files = assets?.List("");
-		filename = Path.GetFileNameWithoutExtension(filename).Replace('.', '_') + Path.GetExtension(filename);
-		return files?.Contains(filename)??false;
-#elif __IOS__ || MACCATALYST || MACOS
-		var directoryName = global::System.IO.Path.GetDirectoryName(filename) + string.Empty;
-		var fileName = global::System.IO.Path.GetFileNameWithoutExtension(filename);
-		var fileExtension = global::System.IO.Path.GetExtension(filename);
-
-		var resourcePathname = NSBundle.MainBundle.PathForResource(global::System.IO.Path.Combine(directoryName, fileName), fileExtension.Substring(1));
-
-		return resourcePathname != null;
-#elif WINDOWS
-		var executingPath = Assembly.GetExecutingAssembly().Location;
-		if (!string.IsNullOrWhiteSpace(executingPath))
-		{
-			var path = Path.GetDirectoryName(executingPath);
-			if (path is not null &&
-				!string.IsNullOrWhiteSpace(path))
-			{
-				var fullPath = Path.Combine(path, filename);
-				return File.Exists(fullPath);
-			}
-		}
-		return true;
-#else
-		return true;
-#endif
-
-	}
+	private Task<bool> FileExistsInPackage(string fileName) => Uno.UI.Toolkit.StorageFileHelper.ExistsInPackage(fileName);
 
 	public async Task<string> CreateFolderAsync(string foldername)
 	{
@@ -57,6 +19,7 @@ public class FileStorage : IStorage
 			{
 				return default;
 			}
+
 			var storageFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri($"ms-appx:///{filename}"));
 			if (File.Exists(storageFile.Path))
 			{
@@ -75,14 +38,21 @@ public class FileStorage : IStorage
 
 	public async Task<Stream?> OpenPackageFileAsync(string filename)
 	{
-		if (!await FileExistsInPackage(filename))
+		try
+		{
+			if (!await FileExistsInPackage(filename))
+			{
+				return default;
+			}
+
+			var storageFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri($"ms-appx:///{filename}"));
+			var stream = await storageFile.OpenStreamForReadAsync();
+			return stream;
+		}
+		catch
 		{
 			return default;
 		}
-
-		var storageFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri($"ms-appx:///{filename}"));
-		var stream = await storageFile.OpenStreamForReadAsync();
-		return stream;
 	}
 
 	public async Task WriteFileAsync(string filename, string text, bool overwrite)
