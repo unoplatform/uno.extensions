19dfedc360388bc8eb74b867e758eaeaf8f566ca
Dan Siegel <me@dansiegel.net>
2023-02-03 09:54:59 -0600

    chore: simplify Frame navigation boilerplate in template

--- src/Uno.Extensions.Templates/content/unoapp-extensions/MyExtensionsApp/App.blank.cs
+++ src/Uno.Extensions.Templates/content/unoapp-extensions/MyExtensionsApp/App.blank.cs
@@ -9,7 +9,6 @@ public class App : Application
 	{
 #if NET6_0_OR_GREATER && WINDOWS && !HAS_UNO
 		_window = new Window();
-		_window.Activate();
 #else
 		_window = Microsoft.UI.Xaml.Window.Current;
 #endif
@@ -21,40 +20,19 @@ public class App : Application
 			// Create a Frame to act as the navigation context and navigate to the first page
 			rootFrame = new Frame();
 
-			rootFrame.NavigationFailed += OnNavigationFailed;
-
-			if (args.UWPLaunchActivatedEventArgs.PreviousExecutionState == ApplicationExecutionState.Terminated)
-			{
-				// TODO: Load state from previously suspended application
-			}
-
 			// Place the frame in the current Window
 			_window.Content = rootFrame;
 		}
 
-#if !(NETSTANDARD && WINDOWS)
-		if (args.UWPLaunchActivatedEventArgs.PrelaunchActivated == false)
-#endif
+		if (rootFrame.Content == null)
 		{
-			if (rootFrame.Content == null)
-			{
-				// When the navigation stack isn't restored navigate to the first page,
-				// configuring the new page by passing required information as a navigation
-				// parameter
-				rootFrame.Navigate(typeof(MainPage), args.Arguments);
-			}
-			// Ensure the current window is active
-			_window.Activate();
+			// When the navigation stack isn't restored navigate to the first page,
+			// configuring the new page by passing required information as a navigation
+			// parameter
+			rootFrame.Navigate(typeof(MainPage), args.Arguments);
 		}
-	}
 
-	/// <summary>
-	/// Invoked when Navigation to a certain page fails
-	/// </summary>
-	/// <param name="sender">The Frame which failed navigation</param>
-	/// <param name="e">Details about the navigation failure</param>
-	private static void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
-	{
-		throw new InvalidOperationException($"Failed to load {e.SourcePageType.FullName}: {e.Exception}");
+		// Ensure the current window is active
+		_window.Activate();
 	}
 }
--- src/Uno.Extensions.Templates/content/unoapp-extensions/MyExtensionsApp/App.recommended.cs
+++ src/Uno.Extensions.Templates/content/unoapp-extensions/MyExtensionsApp/App.recommended.cs
@@ -86,48 +86,26 @@ public class App : Application
 			// Create a Frame to act as the navigation context and navigate to the first page
 			rootFrame = new Frame();
 
-			rootFrame.NavigationFailed += OnNavigationFailed;
-
-			if (args.UWPLaunchActivatedEventArgs.PreviousExecutionState == ApplicationExecutionState.Terminated)
-			{
-				// TODO: Load state from previously suspended application
-			}
-
 			// Place the frame in the current Window
 			_window.Content = rootFrame;
 		}
 
-#if !(NETSTANDARD && WINDOWS)
-		if (args.UWPLaunchActivatedEventArgs.PrelaunchActivated == false)
-#endif
+		if (rootFrame.Content == null)
 		{
-			if (rootFrame.Content == null)
-			{
-				// When the navigation stack isn't restored navigate to the first page,
-				// configuring the new page by passing required information as a navigation
-				// parameter
-				rootFrame.Navigate(typeof(MainPage), args.Arguments);
-			}
-			// Ensure the current window is active
-			_window.Activate();
+			// When the navigation stack isn't restored navigate to the first page,
+			// configuring the new page by passing required information as a navigation
+			// parameter
+			rootFrame.Navigate(typeof(MainPage), args.Arguments);
 		}
+		// Ensure the current window is active
+		_window.Activate();
 //+:cnd:noEmit
 #else
 		Host = await builder.NavigateAsync<Shell>();
 #endif
 	}
 
-#if useFrameNav
-	/// <summary>
-	/// Invoked when Navigation to a certain page fails
-	/// </summary>
-	/// <param name="sender">The Frame which failed navigation</param>
-	/// <param name="e">Details about the navigation failure</param>
-	private static void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
-	{
-		throw new InvalidOperationException($"Failed to load {e.SourcePageType.FullName}: {e.Exception}");
-	}
-#else
+#if !useFrameNav
 	private static void RegisterRoutes(IViewRegistry views, IRouteRegistry routes)
 	{
 #if (useDefaultNavMvux)
