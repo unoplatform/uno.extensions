8f1159fe8704fd8e9802d2c2d7effd7f6c4673f6
Nick Randolph <nick@builttoroam.com>
2023-02-17 15:39:30 +1100

    feat: Removing fonts and updating overrides

--- src/Uno.Extensions.Templates/content/unoapp-extensions/MyExtensionsApp/Styles/MaterialFontsOverride.cs
+++ src/Uno.Extensions.Templates/content/unoapp-extensions/MyExtensionsApp/Styles/MaterialFontsOverride.cs
@@ -6,8 +6,8 @@ public sealed class MaterialFontsOverride : ResourceDictionary
 	public MaterialFontsOverride()
 	{
 		this.Build(r => r
-			.Add<FontFamily>("MaterialLightFontFamily", "ms-appx:///MyExtensionsApp/Assets/Fonts/Material/Roboto-Light.ttf#Roboto")
-			.Add<FontFamily>("MaterialMediumFontFamily", "ms-appx:///MyExtensionsApp/Assets/Fonts/Material/Roboto-Medium.ttf#Roboto")
-			.Add<FontFamily>("MaterialRegularFontFamily", "ms-appx:///MyExtensionsApp/Assets/Fonts/Material/Roboto-Regular.ttf#Roboto"));
+			.Add<FontFamily>("MaterialLightFontFamily", "ms-appx:///Uno.Fonts.Roboto/Fonts/Material/Roboto-Light.ttf#Roboto")
+			.Add<FontFamily>("MaterialMediumFontFamily", "ms-appx:///Uno.Fonts.Roboto/Fonts/Material/Roboto-Medium.ttf#Roboto")
+			.Add<FontFamily>("MaterialRegularFontFamily", "ms-appx:///Uno.Fonts.Roboto/Fonts/Material/Roboto-Regular.ttf#Roboto"));
 	}
 }
