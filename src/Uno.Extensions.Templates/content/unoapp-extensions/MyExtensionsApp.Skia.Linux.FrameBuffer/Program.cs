e69fa20280b5c3fe50b32dd303f400eda230b31f
Nick Randolph <nick@builttoroam.com>
2023-02-27 16:44:27 +1100

    fix: Add keybindings

--- src/Uno.Extensions.Templates/content/unoapp-extensions/MyExtensionsApp.Skia.Linux.FrameBuffer/Program.cs
+++ src/Uno.Extensions.Templates/content/unoapp-extensions/MyExtensionsApp.Skia.Linux.FrameBuffer/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.UI.Xaml;
+using System;
 using Uno.UI.Runtime.Skia;
+using Windows.UI.Core;
 
 namespace MyExtensionsApp.Skia.Framebuffer;
 
@@ -10,7 +13,26 @@ public class Program
 		{
 			Console.CursorVisible = false;
 
-			var host = new FrameBufferHost(() => new AppHead(), args);
+			var host = new FrameBufferHost(() =>
+			{
+				if (CoreWindow.GetForCurrentThread() is { } window)
+				{
+					// Framebuffer applications don't have a WindowManager to rely
+					// on. To close the application, we can hook onto CoreWindow events
+					// which dispatch keyboard input, and close the application as a result.
+					// This block can be moved to App.xaml.cs if it does not interfere with other
+					// platforms that may use the same keys.
+					window.KeyDown += (s, e) =>
+					{
+						if (e.VirtualKey == Windows.System.VirtualKey.F12)
+						{
+							Application.Current.Exit();
+						}
+					};
+				}
+
+				return new AppHead();
+			});
 			host.Run();
 		}
 		finally
