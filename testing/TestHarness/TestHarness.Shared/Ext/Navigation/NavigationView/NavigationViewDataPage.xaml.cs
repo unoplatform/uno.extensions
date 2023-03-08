40ff50ac4dbf55307fd08626d09611e90935b0d0
Nick Randolph <nick@builttoroam.com>
2023-03-08 18:50:28 +1100

    feat: Adding test pages for flyout to data navigation

---
+++ testing/TestHarness/TestHarness.Shared/Ext/Navigation/NavigationView/NavigationViewDataPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿
+namespace TestHarness.Ext.Navigation.NavigationView;
+
+public sealed partial class NavigationViewDataPage : Page
+{
+	public NavigationViewDataViewModel? ViewModel => DataContext as NavigationViewDataViewModel;
+	public NavigationViewDataPage()
+	{
+		this.InitializeComponent();
+	}
+
+}
