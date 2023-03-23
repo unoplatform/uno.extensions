2d9ac83b25eb43227b09748f6ef5fcb2ca48a2b8
Shimmy Weitzhandler <2716316+weitzhandler@users.noreply.github.com>
2023-03-23 02:27:26 +0000

    fix: Removed sample - to be moved to the Uno.Samples project

--- samples/MvuxHowTos/MvuxHowTos/WeatherApp/WeatherApp/WeatherModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WeatherApp;
-
-using Uno.Extensions.Reactive;
-
-public partial record WeatherModel(WeatherService WeatherService)
-{
-	public IFeed<WeatherInfo> CurrentWeather => Feed.Async(WeatherService.GetCurrentWeatherAsync);
-}
