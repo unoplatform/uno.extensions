79072869886c90d1bfedd5322347fc1936a6fc93
Dan Siegel <me@dansiegel.net>
2023-03-27 16:24:05 -0600

    chore: remove Uno.Extensions.Templates

--- src/Uno.Extensions.Templates/content/unoapp-extensions/MyExtensionsApp.1.Server/GlobalUsings.cs
+++
@@ -1,3 +0,0 @@
-global using Microsoft.AspNetCore.Mvc;
-global using MyExtensionsApp._1.DataContracts;
-global using MyExtensionsApp._1.Server.Apis;
--- src/Uno.Extensions.Templates/content/unoapp-extensions/MyExtensionsApp.1/Business/Models/AppConfig.cs
+++
@@ -1,8 +0,0 @@
-//-:cnd:noEmit
-
-namespace MyExtensionsApp._1.Business.Models;
-
-public record AppConfig
-{
-	public string? Title { get; init; }
-}
