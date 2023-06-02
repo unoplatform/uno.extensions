e9af9f057d0220abd6cba22fa454fee0d27ef80a
Nick Randolph <nick@builttoroam.com>
2023-06-02 17:08:52 +1000

    chore: Tidy up

--- src/Uno.Extensions.Toolkit.UI/GlobalUsings.cs
+++ src/Uno.Extensions.Toolkit.UI/GlobalUsings.cs
@@ -1,12 +1,12 @@
 ﻿global using System;
+global using System.Threading;
 global using System.Threading.Tasks;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Hosting;
 global using Microsoft.Extensions.Logging;
-global using Uno.Extensions.Configuration;
 global using Uno.Extensions.Logging;
 global using Uno.Extensions.Toolkit;
-global using Uno.Toolkit.UI;
+global using Windows.Storage;
 
 #if WINUI
 	global using Microsoft.UI.Xaml;
---
+++ src/Uno.Extensions.Validation/ValidatorTypedInstance.cs
@@ -0,0 +1,9 @@
+﻿namespace Uno.Extensions.Validation;
+
+internal record ValidatorTypedInstance<T>(IServiceProvider Services) : IValidatorTypedInstance
+{
+	public Type InstanceType => typeof(T);
+	public IValidator<T> Validator => Services.GetRequiredService<IValidator<T>>();
+	public ValueTask<IEnumerable<ValidationResult>> ValidateAsync(object instance, ValidationContext? context = null, CancellationToken cancellationToken = default) => Validator.ValidateAsync(instance, context, cancellationToken);
+}
+
