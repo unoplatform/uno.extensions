69175a190d06110d8052be2a3836d8840417093a
Nick Randolph <nick@builttoroam.com>
2023-06-02 17:04:47 +1000

    chore(docs): Xml docs for toolkit

--- src/Uno.Extensions.Toolkit.UI/UIElementExtensions.cs
+++ src/Uno.Extensions.Toolkit.UI/UIElementExtensions.cs
@@ -1,7 +1,16 @@
 ﻿namespace Uno.Extensions;
 
+/// <summary>
+/// Extension methods for <see cref="UIElement"/>
+/// </summary>
 public static class UIElementExtensions
 {
+	/// <summary>
+	/// Returns a theme service for the given element
+	/// </summary>
+	/// <param name="element">The UIElement to use to build the theme service</param>
+	/// <param name="logger">[Optional] logger for logging</param>
+	/// <returns>Theme service for controlling application theme</returns>
 	public static IThemeService GetThemeService(this UIElement element, ILogger? logger = default) =>
 		new ThemeService(element, new Dispatcher(element), logger);
 }
--- src/Uno.Extensions.Toolkit.UI/WindowExtensions.cs
+++ src/Uno.Extensions.Toolkit.UI/WindowExtensions.cs
@@ -1,7 +1,16 @@
 ﻿namespace Uno.Extensions;
 
+/// <summary>
+/// Extensions for <see cref="Window"/>
+/// </summary>
 public static class WindowExtensions
 {
+	/// <summary>
+	/// Gets the theme service for the window
+	/// </summary>
+	/// <param name="window">The window to use to build the theme service</param>
+	/// <param name="logger">[Optional]The logger for log output</param>
+	/// <returns>The theme service for controlling application theme</returns>
 	public static IThemeService GetThemeService(this Window window, ILogger? logger = default) =>
 		new ThemeService(window, new Dispatcher(window), logger);
 }
