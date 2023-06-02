2123c4e8e2cb181498355cc1b27009acf1b19492
Nick Randolph <nick@builttoroam.com>
2023-06-03 00:02:36 +1000

    chore(docs): Xml docs

--- src/Uno.Extensions.Storage/IDataFolderProvider.cs
+++ src/Uno.Extensions.Storage/IDataFolderProvider.cs
@@ -1,6 +1,12 @@
 ﻿namespace Uno.Extensions.Storage;
 
+/// <summary>
+/// Interface for providing the data folder path for the application
+/// </summary>
 public interface IDataFolderProvider
 {
+	/// <summary>
+	/// The path to the application data folder
+	/// </summary>
 	string? AppDataPath { get; }
 }
