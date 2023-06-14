42082d8619510fe6ab70319a328595ccfb52f453
Pedro Jesus <pedrojesus.cefet@gmail.com>
2023-06-14 16:31:10 -0300

    feat: added Maui Essentials API page
    (cherry picked from commit e2fa2cadc678e2b5b121abe88575632dea4126cc)

--- samples/MauiEmbedding/MauiEmbedding/Presentation/MauiControlsViewModel.cs
+++ samples/MauiEmbedding/MauiEmbedding/Presentation/MauiControlsViewModel.cs
@@ -1,17 +1,13 @@
 ﻿namespace MauiEmbedding.Presentation;
 partial class MauiControlsViewModel : ObservableObject
 {
-	private INavigator _navigator;
-
 	[ObservableProperty]
 	private string? name;
 
 	public MauiControlsViewModel(
 		IStringLocalizer localizer,
-		IOptions<AppConfig> appInfo,
-		INavigator navigator)
+		IOptions<AppConfig> appInfo)
 	{
-		_navigator = navigator;
 		Title = "Main";
 		Title += $" - {localizer["ApplicationName"]}";
 		Title += $" - {appInfo?.Value?.Environment}";
---
+++ samples/MauiEmbedding/MauiEmbedding/Presentation/MauiEssentialsViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Maui.Devices;
+
+namespace MauiEmbedding.Presentation;
+partial class MauiEssentialsViewModel : ObservableObject
+{
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(nameof(BatteryLevelText))]
+	double _batteryLevel;
+
+	public string BatteryLevelText => $"Battery level: {BatteryLevel * 100}%";
+
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(nameof(DisplayInfo))]
+	(double width, double height) _displaySize;
+
+	public string DisplayInfo => $"Display info: {DisplaySize.width}x{DisplaySize.height}";
+
+	public MauiEssentialsViewModel()
+	{
+		BatteryLevel = Battery.ChargeLevel;
+		DisplaySize = (DeviceDisplay.MainDisplayInfo.Width, DeviceDisplay.MainDisplayInfo.Height);
+
+		Battery.BatteryInfoChanged += OnBatteryBatteryInfoChanged;
+		DeviceDisplay.MainDisplayInfoChanged += OnDeviceDisplayChanged;
+	}
+
+	private void OnDeviceDisplayChanged(object? sender, DisplayInfoChangedEventArgs e) =>
+		DisplaySize = (e.DisplayInfo.Width, e.DisplayInfo.Height);
+	private void OnBatteryBatteryInfoChanged(object? sender, BatteryInfoChangedEventArgs e) =>
+		BatteryLevel = e.ChargeLevel;
+}
