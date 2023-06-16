d4aecd567cf0df41c7a0575d30d7c65567579a8d
Jerome Laban <jerome.laban@nventive.com>
2023-06-16 14:40:43 -0400

    ci: Adjust test harness

--- build/ci/scripts/wasm-uitest-run.sh
+++ build/ci/scripts/wasm-uitest-run.sh
@@ -9,13 +9,10 @@ export UNO_UITEST_SCREENSHOT_PATH=$BUILD_ARTIFACTSTAGINGDIRECTORY/screenshots/wa
 export BIN_LOG_PATH=$BUILD_ARTIFACTSTAGINGDIRECTORY/wasm-uitest.binlog
 export UNO_UITEST_PLATFORM=Browser
 export UNO_UITEST_CHROME_CONTAINER_MODE=true
-export UNO_UITEST_PROJECT=$BUILD_SOURCESDIRECTORY/testing/TestHarness/TestHarness.UITest/TestHarness.UITest.csproj
-export UNO_UITEST_BINARY=$BUILD_SOURCESDIRECTORY/testing/TestHarness/TestHarness.UITest/bin/Release/net48/TestHarness.UITest.dll
+export UNO_UITEST_PROJECT=$BUILD_SOURCESDIRECTORY/testing/TestHarness/TestHarness.UITest
 export UNO_UITEST_LOGFILE=$BUILD_ARTIFACTSTAGINGDIRECTORY/screenshots/wasm/nunit-log.txt
 export UNO_UITEST_WASM_PROJECT=$BUILD_SOURCESDIRECTORY/testing/TestHarness/TestHarness.Wasm/TestHarness.Wasm.csproj
 export UNO_UITEST_WASM_OUTPUT_PATH=$BUILD_SOURCESDIRECTORY/testing/TestHarness/TestHarness.Wasm/bin/Release/net7.0/dist/
-export UNO_UITEST_NUNIT_VERSION=3.13.2
-export UNO_UITEST_NUGET_URL=https://dist.nuget.org/win-x86-commandline/v5.7.0/nuget.exe
 export UNO_UITEST_WASM_PROJECT_BUILD_OPTIONS="/p:UnoExtensionsDisableNet7=true /p:GeneratePackageOnBuild=false"
 
 cd $BUILD_SOURCESDIRECTORY
@@ -30,11 +27,15 @@ cd $BUILD_SOURCESDIRECTORY/build
 
 npm i chromedriver@102.0.0
 npm i puppeteer@14.1.0
-wget $UNO_UITEST_NUGET_URL
-mono nuget.exe install NUnit.ConsoleRunner -Version $UNO_UITEST_NUNIT_VERSION
 
 mkdir -p $UNO_UITEST_SCREENSHOT_PATH
 
-mono $BUILD_SOURCESDIRECTORY/build/NUnit.ConsoleRunner.$UNO_UITEST_NUNIT_VERSION/tools/nunit3-console.exe \
-  --trace=Verbose --inprocess --agents=1 --workers=1 \
-  $UNO_UITEST_BINARY || true
+cd $UNO_UITEST_PROJECT
+
+## Run the tests
+dotnet test \
+	-c Release \
+	-l:"console;verbosity=normal" \
+	--logger "nunit;LogFileName=$BUILD_SOURCESDIRECTORY/build/TestResult.xml" \
+	-v m \
+	|| true
