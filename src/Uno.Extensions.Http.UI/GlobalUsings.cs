194027e6475f553cf70455c414e619751dd60028
Nick Randolph <nick@builttoroam.com>
2023-06-17 02:30:44 +1000

    feat: Splitting out UI part of Http

---
+++ src/Uno.Extensions.Http.UI/GlobalUsings.cs
@@ -0,0 +1,12 @@
+﻿global using System;
+global using System.IO;
+global using System.Linq;
+global using System.Net.Http;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Http;
+global using Microsoft.Extensions.Logging;
+global using Uno.Extensions.Http;
