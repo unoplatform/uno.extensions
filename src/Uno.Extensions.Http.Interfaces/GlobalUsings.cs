ac10905f5f92245f29bf8e496431662a3c53fd8e
Jerome Laban <jerome.laban@nventive.com>
2023-06-19 15:45:35 -0400

    chore: Adjust for unused http interfaces projet

--- src/Uno.Extensions.Http.Interfaces/GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-﻿global using System;
-global using System.IO;
-global using System.Linq;
-global using System.Net.Http;
-global using System.Threading;
-global using System.Threading.Tasks;
-global using Uno.Extensions.Http;
