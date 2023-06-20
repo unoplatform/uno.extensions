39725f6dbf1c16c3b4dd4442d5e9be9f4f848a22
Pedro Jesus <pedrojesus.cefet@gmail.com>
2023-06-20 20:26:36 -0300

    chore: changed the sample to make sure controls are showing
    (cherry picked from commit 71b04dbe6a43e61117ea060d9348e2c4cae23575)

--- samples/MauiEmbedding/MauiEmbedding/Presentation/MauiControlsPage.xaml.cs
+++ samples/MauiEmbedding/MauiEmbedding/Presentation/MauiControlsPage.xaml.cs
@@ -1,18 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Microsoft.Maui.Controls;
+﻿using Microsoft.Maui.Controls;
 using Page = Microsoft.UI.Xaml.Controls.Page;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -26,8 +12,23 @@ public sealed partial class MauiControlsPage : Page
 	public MauiControlsPage()
 	{
 		this.InitializeComponent();
-		//DataContext = new MauiControlsViewModel();
-		var lbl = new Label();
+
+		var lbl = new Label()
+		{
+			// If we don't specify the size , the label will not be visible
+			// when the binding changes it doesn't recalculate its size
+			HeightRequest = 20,
+		};
+
+		var b = new BoxView
+		{
+			HeightRequest = 300,
+			WidthRequest = 300
+		};
+
+		var source = new List<string> { "Uno", "Maui", "WinUI" };
+
+		this.picker.ItemsSource = source; 
 
 		var mauiBinding = new Microsoft.Maui.Controls.Binding
 		{
@@ -37,6 +38,13 @@ public sealed partial class MauiControlsPage : Page
 
 		lbl.SetBinding(Label.TextProperty, mauiBinding);
 
+		this.stack.BindingContextChanged +=(_,__) =>
+		{
+			_ = this.stack.BindingContext;
+			_ = lbl.BindingContext;
+		};
+
 		this.stack.Add(lbl);
+		this.stack.Add(b);
 	}
 }
