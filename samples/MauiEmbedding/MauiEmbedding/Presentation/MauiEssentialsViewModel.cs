2c6596027971e4ddf4417df1e2500fd3aa45b776
Nick Randolph <nick@builttoroam.com>
2023-06-21 22:12:50 +1000

    fix: Threading issue for accessing essential properties

--- samples/MauiEmbedding/MauiEmbedding/Presentation/MauiEssentialsViewModel.cs
+++ samples/MauiEmbedding/MauiEmbedding/Presentation/MauiEssentialsViewModel.cs
@@ -6,23 +6,23 @@ partial class MauiEssentialsViewModel : ObservableObject
 {
 	[ObservableProperty]
 	[NotifyPropertyChangedFor(nameof(BatteryLevelText))]
-	double _batteryLevel;
+	private double _batteryLevel;
 
 	public string BatteryLevelText => $"Battery level: {BatteryLevel * 100}%";
 
 	[ObservableProperty]
 	[NotifyPropertyChangedFor(nameof(DisplayInfo))]
-	(double width, double height) _displaySize;
+	private (double width, double height) _displaySize;
 
 	public string DisplayInfo => $"Display info: {DisplaySize.width}x{DisplaySize.height}";
 
 	public MauiEssentialsViewModel(IDispatcher dispatcher)
 	{
-		BatteryLevel = Battery.ChargeLevel;
-		DisplaySize = (DeviceDisplay.MainDisplayInfo.Width, DeviceDisplay.MainDisplayInfo.Height);
-
 		_ = dispatcher.ExecuteAsync(() =>
 		{
+			BatteryLevel = Battery.ChargeLevel;
+			DisplaySize = (DeviceDisplay.MainDisplayInfo.Width, DeviceDisplay.MainDisplayInfo.Height);
+
 			Battery.BatteryInfoChanged += OnBatteryBatteryInfoChanged;
 			DeviceDisplay.MainDisplayInfoChanged += OnDeviceDisplayChanged;
 		});
