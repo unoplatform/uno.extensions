e5d37813659dce503d72295f6f55093448b087b4
pedrojesus <pedrojesus.cefet@gmail.com>
2023-06-21 11:10:45 -0300

    chore: added C# controls

--- samples/MauiEmbedding/MauiEmbedding/Presentation/MauiControlsPage.xaml.cs
+++ samples/MauiEmbedding/MauiEmbedding/Presentation/MauiControlsPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
 using Page = Microsoft.UI.Xaml.Controls.Page;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -13,38 +14,43 @@ public sealed partial class MauiControlsPage : Page
 	{
 		this.InitializeComponent();
 
-		//var lbl = new Label()
-		//{
-		//	// If we don't specify the size , the label will not be visible
-		//	// when the binding changes it doesn't recalculate its size
-		//	HeightRequest = 20,
-		//};
+		var lbl = new Label()
+		{
+			// If we don't specify the size , the label will not be visible
+			// when the binding changes it doesn't recalculate its size
+			// This control doesn't show up on iOS but it's on the visual tree
+			HeightRequest = 20,
+			BackgroundColor = Colors.Pink
+		};
 
-		//var b = new BoxView
-		//{
-		//	HeightRequest = 300,
-		//	WidthRequest = 300
-		//};
+		lbl.HandlerChanged += (_, __) => lbl.Layout(new Rect(0,0,lbl.WidthRequest,lbl.HeightRequest));
 
-		//var source = new List<string> { "Uno", "Maui", "WinUI" };
 
-		//this.picker.ItemsSource = source; 
+		var b = new BoxView
+		{
+			HeightRequest = 300,
+			WidthRequest = 300
+		};
 
-		//var mauiBinding = new Microsoft.Maui.Controls.Binding
-		//{
-		//	Path = "Title",
-		//	Source = DataContext
-		//};
+		var source = new List<string> { "Uno", "Maui", "WinUI" };
 
-		//lbl.SetBinding(Label.TextProperty, mauiBinding);
+		this.picker.ItemsSource = source;
+		var mauiBinding = new Microsoft.Maui.Controls.Binding
+		{
+			Path = "Title",
+			Source = DataContext
+		};
 
-		//this.stack.BindingContextChanged +=(_,__) =>
-		//{
-		//	_ = this.stack.BindingContext;
-		//	_ = lbl.BindingContext;
-		//};
+		lbl.SetBinding(Label.TextProperty, mauiBinding);
 
-		//this.stack.Add(lbl);
-		//this.stack.Add(b);
+		this.stack.BindingContextChanged +=(_,__) =>
+		{
+			_ = this.stack.BindingContext;
+			_ = lbl.BindingContext;
+		};
+
+		this.stack.Add(lbl);
+		this.stack.Add(b);
+
 	}
 }
