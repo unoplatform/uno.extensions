44d5b2ff024d0bb2264b83827ae7c93430f59c9c
Pedro Jesus <pedrojesus.cefet@gmail.com>
2023-06-21 20:51:18 -0300

    feat: added telerik
    we can remove this in the future to not block devs to run our sample

---
+++ samples/MauiEmbedding/MauiEmbedding/Presentation/TelerikControlsPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiEmbedding.Presentation;
+partial class TelerikControlsPage : Page
+{
+	public TelerikControlsPage()
+	{
+		this.InitializeComponent();
+
+		this.indicator.HeightRequest = 100;
+	}
+}
