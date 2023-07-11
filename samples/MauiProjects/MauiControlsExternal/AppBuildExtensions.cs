3202d7ce57fab4036b13e1def3c6c4f0e6f880b9
Pedro Jesus <pedrojesus.cefet@gmail.com>
2023-07-11 18:10:21 -0300

    chore: added External Maui lib to the Maui project

---
+++ samples/MauiProjects/MauiControlsExternal/AppBuildExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiControlsExternal;
+public static class AppBuildExtensions
+{
+	public static MauiAppBuilder UseCustomLibrary(this MauiAppBuilder builder)
+	{
+		CustomEntry.Init();
+
+		return builder;
+	}
+}
