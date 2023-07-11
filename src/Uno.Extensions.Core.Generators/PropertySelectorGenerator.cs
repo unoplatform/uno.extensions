cccdf8e3d1edd3b378523ec955e4c047e9d2e92d
Youssef Victor <youssefvictor00@gmail.com>
2023-07-11 11:42:19 +0300

    perf: Alternative incremental implementation

--- src/Uno.Extensions.Core.Generators/PropertySelectorGenerator.cs
+++ src/Uno.Extensions.Core.Generators/PropertySelectorGenerator.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using System.Collections.Immutable;
+using System.Runtime.CompilerServices;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Uno.Extensions.Generators.PropertySelector;
 
@@ -11,6 +13,8 @@ namespace Uno.Extensions.Core.Generators;
 public partial class PropertySelectorGenerator : IIncrementalGenerator
 {
 	private readonly PropertySelectorsGenerationTool _tool;
+	private static readonly ConditionalWeakTable<AssemblyIdentity, object> s_assemblyToNamesMap = new();
+	private static readonly ConditionalWeakTable<SyntaxTree, object> s_TreeToIsCandidate = new();
 
 	/// <summary>
 	/// Creates a new instance of the PropertySelectorGenerator
@@ -32,16 +36,167 @@ public partial class PropertySelectorGenerator : IIncrementalGenerator
 		}
 #endif
 		var assemblyNameProvider = context.CompilationProvider.Select((compilation, _) => compilation.AssemblyName);
-		var syntaxProvider = context.SyntaxProvider
-			.CreateSyntaxProvider(
-				(node, ct) => IsCandidate(node),
-				(ctx, ct) => new PropertySelectorCandidate(ctx, ct))
-			.Where(candidate => candidate.IsValid).WithTrackingName("syntaxProvider_PropertySelectorGenerator");
+		var interestingMethodNames = context.CompilationProvider.SelectMany((compilation, _) =>
+		{
+			var propertySelectorSymbol = compilation.GetTypeByMetadataName("Uno.Extensions.Edition.PropertySelector`2");
+			var callerLineNumberSymbol = compilation.GetTypeByMetadataName("System.Runtime.CompilerServices.CallerLineNumberAttribute");
+			var callerFilePathSymbol = compilation.GetTypeByMetadataName("System.Runtime.CompilerServices.CallerFilePathAttribute");
 
-		var provider = syntaxProvider.Combine(assemblyNameProvider).WithTrackingName("combinedProvider_PropertySelectorGenerator");
+			var builder = ImmutableArray.CreateBuilder<string>();
+			foreach (var reference in compilation.References)
+			{
+				if (compilation.GetAssemblyOrModuleSymbol(reference) is IAssemblySymbol assembly)
+				{
+					if (!s_assemblyToNamesMap.TryGetValue(assembly.Identity, out var interestingNames))
+					{
+						interestingNames = GetFromNamespaceExpensive(assembly.GlobalNamespace, propertySelectorSymbol, callerFilePathSymbol, callerLineNumberSymbol).ToImmutableArray();
+						s_assemblyToNamesMap.Add(assembly.Identity, interestingNames);
+					}
+
+					builder.AddRange((ImmutableArray<string>)interestingNames);
+				}
+			}
+
+			return builder.ToImmutableArray();
+		});
+
+		// Create a provider over all the syntax trees in the compilation.  This is better than CreateSyntaxProvider as
+		// using SyntaxTrees is purely syntax and will not update the incremental node for a tree when another tree is
+		// changed. CreateSyntaxProvider will have to rerun all incremental nodes since it passes along the
+		// SemanticModel, and that model is updated whenever any tree changes (since it is tied to the compilation).
+		var syntaxTreesProvider = context.CompilationProvider
+			.SelectMany((compilation, cancellationToken) => GetInterestingTrees(compilation, cancellationToken));
+
+		var filteredSyntaxTreesProvider = syntaxTreesProvider.Combine(interestingMethodNames.Collect()).SelectMany((pair, ct) =>
+		{
+			var (tree, interestingNames) = pair;
+			var filteredNodes = ImmutableArray.CreateBuilder<InvocationExpressionSyntax>();
+			foreach (var node in tree.GetRoot().DescendantNodes())
+			{
+				if (!IsCandidate(node))
+				{
+					continue;
+				}
+
+				var invocationSyntax = (InvocationExpressionSyntax)node;
+				if (invocationSyntax.Expression is MemberAccessExpressionSyntax { Name.Identifier.ValueText: string calledMethodName } &&
+					interestingNames.Contains(calledMethodName))
+				{
+					filteredNodes.Add(invocationSyntax);
+				}
+			}
+
+			return filteredNodes;
+		});
+
+		var finalProvider = filteredSyntaxTreesProvider.Combine(context.CompilationProvider).Select((pair, ct) =>
+		{
+			var (node, compilation) = pair;
+			return new PropertySelectorCandidate(node, compilation.GetSemanticModel(node.SyntaxTree), ct);
+		}).WithTrackingName("syntaxProvider_PropertySelectorGenerator").Where(candidate => candidate.IsValid);
 
 		// We use the Implementation as the generated code does not alter the SemanticModel (only generates a registry).
-		context.RegisterImplementationSourceOutput(provider, (context, souce) => _tool.Generate(context, souce.Left, souce.Right));
+		context.RegisterImplementationSourceOutput(
+			finalProvider.Combine(assemblyNameProvider).WithTrackingName("combinedProvider_PropertySelectorGenerator"),
+			(context, souce) => _tool.Generate(context, souce.Left, souce.Right));
+	}
+
+	private static ImmutableArray<SyntaxTree> GetInterestingTrees(Compilation compilation, CancellationToken cancellationToken)
+	{
+		// Get the count up front so we can allocate without waste.
+		var count = 0;
+		foreach (var tree in compilation.SyntaxTrees)
+		{
+			cancellationToken.ThrowIfCancellationRequested();
+			if (IsCandidateTree(tree, cancellationToken))
+				count++;
+		}
+
+		var builder = ImmutableArray.CreateBuilder<SyntaxTree>(count);
+
+		// Iterate again.  This will be free as the values from before will already be cached on the syntax tree.
+		foreach (var tree in compilation.SyntaxTrees)
+		{
+			if (IsCandidateTree(tree, cancellationToken))
+				builder.Add(tree);
+		}
+
+		return builder.MoveToImmutable();
+	}
+
+	private static bool IsCandidateTree(SyntaxTree tree, CancellationToken ct)
+	{
+		if (s_TreeToIsCandidate.TryGetValue(tree, out var candidate))
+		{
+			return (bool)candidate;
+		}
+
+		var root =  (CompilationUnitSyntax)tree.GetRoot(ct);
+		foreach (var member in root.Members)
+		{
+			foreach (var node in member.DescendantNodesAndSelf())
+			{
+				if (IsCandidate(node))
+				{
+					s_TreeToIsCandidate.Add(tree, true);
+					return true;
+				}
+			}
+		}
+
+		s_TreeToIsCandidate.Add(tree, false);
+		return false;
+	}
+
+	internal static bool IsCandidate(IMethodSymbol method, INamedTypeSymbol? propertySelectorSymbol, INamedTypeSymbol? callerFilePathSymbol, INamedTypeSymbol? callerLineNumberSymbol)
+	{
+		bool foundPropertySelector = false;
+		bool foundCallerFilePath = false;
+		bool foundCallerLineNumber = false;
+		foreach (var parameter in method.Parameters)
+		{
+			if (parameter.Type.OriginalDefinition.Equals(propertySelectorSymbol, SymbolEqualityComparer.Default))
+			{
+				foundPropertySelector = true;
+			}
+			else if (parameter.FindAttribute(callerFilePathSymbol) is not null)
+			{
+				foundCallerFilePath = true;
+			}
+			else if (parameter.FindAttribute(callerLineNumberSymbol) is not null)
+			{
+				foundCallerLineNumber = true;
+			}
+		}
+
+		return foundPropertySelector && foundCallerFilePath && foundCallerLineNumber;
+	}
+
+	private static IEnumerable<string> GetFromNamespaceExpensive(INamespaceSymbol @namespace, INamedTypeSymbol? propertySelectorSymbol, INamedTypeSymbol? callerFilePathSymbol, INamedTypeSymbol? callerLineNumberSymbol)
+	{
+		foreach (var member in @namespace.GetMembers())
+		{
+			if (member is INamespaceSymbol innerNamespace)
+			{
+				foreach (var inner in GetFromNamespaceExpensive(innerNamespace, propertySelectorSymbol, callerFilePathSymbol, callerLineNumberSymbol))
+				{
+					yield return inner;
+				}
+			}
+			else if (member is INamedTypeSymbol { DeclaredAccessibility: Accessibility.Public })
+			{
+				foreach (var method in member.GetMembers())
+				{
+					if (method is IMethodSymbol methodSymbol)
+					{
+						if (IsCandidate(methodSymbol, propertySelectorSymbol, callerFilePathSymbol, callerLineNumberSymbol))
+						{
+							yield return methodSymbol.Name;
+						}
+					}
+				}
+			}
+		}
 	}
 
 	private static bool IsCandidate(SyntaxNode node)
