3b8b9ec043aaee9d3e45c2f0682b8b8b0dde858b
Pedro Jesus <pedrojesus.cefet@gmail.com>
2023-07-11 18:10:21 -0300

    chore: added External Maui lib to the Maui project
    (cherry picked from commit 3202d7ce57fab4036b13e1def3c6c4f0e6f880b9)

--- samples/MauiProjects/MauiApp1/MauiProgram.cs
+++ samples/MauiProjects/MauiApp1/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using MauiControlsExternal;
 
 namespace MauiApp1;
 public static class MauiProgram
@@ -12,7 +13,8 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+			})
+			.UseCustomLibrary();
 
 #if DEBUG
 		builder.Logging.AddDebug();
