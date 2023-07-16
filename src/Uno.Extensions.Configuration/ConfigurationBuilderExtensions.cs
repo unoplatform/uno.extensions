0e8240c71f922ea78678261f61d4ac9b2d8eae99
Luke Blevins <lukeblevins15@outlook.com>
2023-07-16 11:18:03 -0400

    chore(cfg)!: Don't expose inner impl
    BREAKING CHANGE: Adjusts the access modifier of AddEmbeddedConfigurationFile to private
    
    The AddEmbeddedConfigurationFile method was exposing an inner
    implementation method that is likely not intended for external use.
    This commit changes the access modifier of the method to private.

--- src/Uno.Extensions.Configuration/ConfigurationBuilderExtensions.cs
+++ src/Uno.Extensions.Configuration/ConfigurationBuilderExtensions.cs
@@ -67,7 +67,7 @@ public static class ConfigurationBuilderExtensions
 		return configurationBuilder.AddEnvironmentConfiguration(hostingContext, default);
 	}
 
-	public static IConfigurationBuilder AddEmbeddedConfigurationFile<TApplicationRoot>(this IConfigurationBuilder configurationBuilder, string configurationFileName)
+	private static IConfigurationBuilder AddEmbeddedConfigurationFile<TApplicationRoot>(this IConfigurationBuilder configurationBuilder, string configurationFileName)
 		where TApplicationRoot : class
 	{
 		var generalAppConfiguration =
