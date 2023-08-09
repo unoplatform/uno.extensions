5357341bbbf17d9283e952135d90d5399199e599
Luke Blevins <lukeblevins15@outlook.com>
2023-08-09 16:40:47 -0400

    chore(xml): Revert changes to Uno logging helper
    This reverts commit 37d4833f249990dee87ee32feac0c73d5d7b1d2b.

--- src/Uno.Extensions.Logging/HostExtensions.cs
+++ src/Uno.Extensions.Logging/HostExtensions.cs
@@ -5,7 +5,7 @@
 /// </summary>
 public static class HostExtensions
 {
-	internal static IHost ConnectUnoLogging(this IHost host, bool enableUnoLogging = true)
+	public static IHost ConnectUnoLogging(this IHost host, bool enableUnoLogging = true)
 	{
 		if (!enableUnoLogging)
 		{
