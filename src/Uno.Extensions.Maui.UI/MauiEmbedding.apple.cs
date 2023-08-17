7af0fbe9a114f49dbd7578ee783582f144d417ad
Dan Siegel <me@dansiegel.net>
2023-08-17 13:45:33 -0600

    feat: initialize Application Handler

---
+++ src/Uno.Extensions.Maui.UI/MauiEmbedding.apple.cs
@@ -0,0 +1,27 @@
+﻿#if IOS || MACCATALYST
+using UIKit;
+
+namespace Uno.Extensions.Maui;
+
+partial class MauiEmbedding
+{
+	private static MauiAppBuilder RegisterPlatformServices(this MauiAppBuilder builder, Application app)
+	{
+		builder.Services.AddTransient<UIWindow>(sp => sp.GetRequiredService<Application>().Window!)
+			.AddSingleton<IUIApplicationDelegate>(sp => sp.GetRequiredService<Application>());
+
+		return builder;
+	}
+
+	private static void InitializeMauiEmbeddingApp(this MauiApp mauiApp)
+	{
+		var rootContext = new MauiContext(mauiApp.Services);
+		rootContext.InitializeScopedServices();
+
+		var iApp = mauiApp.Services.GetRequiredService<IApplication>();
+		IPlatformApplication.Current = new EmbeddingApp(mauiApp.Services, iApp);
+		var appDelegate = mauiApp.Services.GetRequiredService<IUIApplicationDelegate>();
+		appDelegate.SetApplicationHandler(iApp, rootContext);
+	}
+}
+#endif
