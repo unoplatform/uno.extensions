243ba453d91a0d921c7be542bc1bc6eda0686a23
Dan Siegel <me@dansiegel.net>
2023-08-23 10:48:08 -0600

    feat: expose created Maui view on the Maui Host
    (cherry picked from commit ced57607bcd5956ac648cd4de80ea0253afef6f6)

---
+++ src/Uno.Extensions.Maui.UI/MauiContentCreatedEventArgs.cs
@@ -0,0 +1,11 @@
+﻿namespace Uno.Extensions.Maui;
+
+public class MauiContentCreatedEventArgs : EventArgs
+{
+	public MauiContentCreatedEventArgs(VisualElement content)
+	{
+		Content = content;
+	}
+
+	public VisualElement Content { get; }
+}
