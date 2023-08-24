0af3d7b35f09d324be3313341edaf2d27839e5c9
Dan Siegel <me@dansiegel.net>
2023-08-24 09:49:00 -0600

    fix: adding window handler initialization

---
+++ src/Uno.Extensions.Maui.UI/Internals/EmbeddedWindowHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace Uno.Extensions.Maui.Internals;
+
+internal class EmbeddedWindowHandler : IElementHandler
+{
+	public object? PlatformView { get; set; }
+	public IElement? VirtualView { get; set; }
+	public IMauiContext? MauiContext { get; set; }
+
+	public void DisconnectHandler() { }
+	public void Invoke(string command, object? args = null) { }
+	public void SetMauiContext(IMauiContext mauiContext) => MauiContext = mauiContext;
+	public void SetVirtualView(IElement view) => VirtualView = view;
+	public void UpdateValue(string property) { }
+}
