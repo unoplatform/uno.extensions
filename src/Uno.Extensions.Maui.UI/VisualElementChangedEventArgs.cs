d5af85511462a9b862c4839603e938d88d1f2040
Dan Siegel <me@dansiegel.net>
2023-08-24 10:13:14 -0600

    chore: update naming

---
+++ src/Uno.Extensions.Maui.UI/VisualElementChangedEventArgs.cs
@@ -0,0 +1,11 @@
+﻿namespace Uno.Extensions.Maui;
+
+public class VisualElementChangedEventArgs : EventArgs
+{
+	public VisualElementChangedEventArgs(VisualElement content)
+	{
+		Content = content;
+	}
+
+	public VisualElement Content { get; }
+}
