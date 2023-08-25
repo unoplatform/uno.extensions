1f46e15c7d8bcd51b965807f16cdaddff62adeaa
Dan Siegel <me@dansiegel.net>
2023-08-25 08:20:58 -0600

    chore: set the page after initializing the Handler

--- src/Uno.Extensions.Maui.UI/MauiEmbedding.cs
+++ src/Uno.Extensions.Maui.UI/MauiEmbedding.cs
@@ -124,7 +124,7 @@ public static partial class MauiEmbedding
 		_ = page.ToPlatform(context);
 
 		// Create a Maui Window and initialize a Handler shim. This will expose the actual Application Window
-		var virtualWindow = new Microsoft.Maui.Controls.Window(page);
+		var virtualWindow = new Microsoft.Maui.Controls.Window();
 		virtualWindow.Handler = new EmbeddedWindowHandler
 		{
 #if IOS || MACCATALYST
@@ -137,6 +137,7 @@ public static partial class MauiEmbedding
 			VirtualView = virtualWindow,
 			MauiContext = context
 		};
+		virtualWindow.Page = page;
 
 		app.SetCoreWindow(virtualWindow);
 	}
