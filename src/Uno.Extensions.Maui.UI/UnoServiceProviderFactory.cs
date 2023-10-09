d03127113c297050b4c35cae7d7d67ecf730480e
Nick Randolph <nick@builttoroam.com>
2023-10-09 17:48:16 +1100

    chore: Fix registrations

--- src/Uno.Extensions.Maui.UI/UnoServiceProviderFactory.cs
+++ src/Uno.Extensions.Maui.UI/UnoServiceProviderFactory.cs
@@ -16,30 +16,34 @@ internal class UnoServiceProviderFactory : IServiceProviderFactory<IServiceProvi
 	// We delay calling build on the MauiHostBuilder until the Uno Host is built
 	public IServiceProvider CreateBuilder(IServiceCollection services)
 	{
-		// NOTE: TryAdd prevents duplicates from overriding what was registered with Uno's Host Builder such as IConfiguration
-		var instanceDescriptors = new List<ServiceDescriptor>();
-		_mauiAppBuilder.Services.ForEach(x =>
-		{
-			if (!services.TryAddService(x) )
-			{
-				if (x.ImplementationInstance is not null)
-				{
-					instanceDescriptors.Add(x);
-				}
-			}
-		});
+		//// NOTE: TryAdd prevents duplicates from overriding what was registered with Uno's Host Builder such as IConfiguration
+		//var instanceDescriptors = new List<ServiceDescriptor>();
+		//_mauiAppBuilder.Services.ForEach(x =>
+		//{
+		//	if (!services.TryAddService(x) )
+		//	{
+		//		if (x.ImplementationInstance is not null)
+		//		{
+		//			instanceDescriptors.Add(x);
+		//		}
+		//	}
+		//});
 
+		//var serviceProvider = services.BuildServiceProvider();
+		//instanceDescriptors.ForEach(x =>
+		//{
+		//	if (serviceProvider is not null &&
+		//		x.ImplementationInstance is not null)
+		//	{
+		//		var implType = x.ImplementationType ?? x.ImplementationInstance.GetType();
+
+		//		serviceProvider.AddSingletonInstance(implType, x.ImplementationInstance);
+		//	}
+		//});
+
+		_mauiAppBuilder.Services.ForEach(x => services.Add(x));
 		var serviceProvider = services.BuildServiceProvider();
-		instanceDescriptors.ForEach(x =>
-		{
-			if (serviceProvider is not null &&
-				x.ImplementationInstance is not null)
-			{
-				var implType = x.ImplementationType ?? x.ImplementationInstance.GetType();
 
-				serviceProvider.AddSingletonInstance(implType, x.ImplementationInstance);
-			}
-		});
 
 		_mauiAppBuilder.ConfigureContainer(new MauiServiceProviderFactory(serviceProvider));
 		_buildAppBuilderCallback();
