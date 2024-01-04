5f646592a7b222e40bcebaedd31ea17296a6c877
Nick Randolph <nick@builttoroam.com>
2024-01-04 17:42:46 +1100

    feat: Add mapping caching when using reflection

--- testing/TestHarness/TestHarness.Shared/Ext/Navigation/TabBar/TabBarHomeViewModel.cs
+++ testing/TestHarness/TestHarness.Shared/Ext/Navigation/TabBar/TabBarHomeViewModel.cs
@@ -1,6 +1,22 @@
 ﻿namespace TestHarness.Ext.Navigation.TabBar;
 
-public record TabBarHomeViewModel(INavigator Navigator)
+public partial class TabBarHomeViewModel : ObservableObject
 {
+	private RouteResolverDefault routeResolver;
+	public TabBarHomeViewModel(IServiceProvider services)
+	{
+		routeResolver = services.GetRequiredService<RouteResolverDefault>();
+		ImplicitMappingEnabled = routeResolver.ReturnImplicitMapping;
+	}
+
+	[ObservableProperty]
+	private bool _implicitMappingEnabled;
+
+	[RelayCommand]
+	public void ToggleIsChecked()
+	{
+		routeResolver.ReturnImplicitMapping = !routeResolver.ReturnImplicitMapping;
+		ImplicitMappingEnabled = routeResolver.ReturnImplicitMapping;
+	}
 }
 
