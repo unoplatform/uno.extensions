e217a9742d225dfa0d9c9fa67ad468ced0c0d79c
Nick Randolph <nick@builttoroam.com>
2024-06-11 19:47:20 +1000

    chore: Adding Chefs sample

---
+++ testing/TestHarness/TestHarness.Shared/Ext/Navigation/Apps/Chefs/ChefsHostInit.cs
@@ -0,0 +1,62 @@
+﻿namespace TestHarness.Ext.Navigation.Apps.Chefs;
+
+public class ChefsHostInit : BaseHostInitialization
+{
+	//protected override IHostBuilder Custom(IHostBuilder builder)
+	//{
+	//	return builder.ConfigureServices(services =>
+	//				services
+	//						.AddSingleton<IAuthenticationRouteInfo>(
+	//								_ => new AuthenticationRouteInfo<
+	//										ChefsLoginViewModel,
+	//										ChefsHomeViewModel>())
+	//			);
+	//}
+
+	//protected override void RegisterRoutes(IViewRegistry views, IRouteRegistry routes)
+	//{
+
+	//	views.Register(
+	//			new ViewMap(ViewModel: typeof(ChefsShellViewModel)),
+	//			new ViewMap<ChefsLoginPage, ChefsLoginViewModel>(ResultData: typeof(ChefsCredentials)),
+	//			new DataViewMap<ChefsHomePage,ChefsHomeViewModel, ChefsCredentials>(),
+	//			new ViewMap<ChefsProductsPage, ChefsProductsViewModel>(),
+	//			new DataViewMap<ChefsProductDetailsPage, ChefsProductDetailsViewModel, ChefsProduct>(),
+	//			new ViewMap<ChefsDealsPage, ChefsDealsViewModel>(),
+	//			new ViewMap<ChefsProfilePage, ChefsProfileViewModel>(),
+	//			new ViewMap<ChefsSettingsPage, ChefsSettingsViewModel>()
+	//			);
+
+
+	//	routes
+	//		.Register(
+	//			new RouteMap("", View: views.FindByViewModel<ChefsShellViewModel>(),
+	//					Nested: new RouteMap[]
+	//					{
+	//						new RouteMap("Login", View: views.FindByViewModel<ChefsLoginViewModel>()),
+	//						new RouteMap("Home", View: views.FindByView<ChefsHomePage>(),
+	//								Nested: new RouteMap[]{
+	//									new RouteMap("Deals",
+	//										View: views.FindByViewModel<ChefsDealsViewModel>(),
+	//										IsDefault: true,
+	//											Nested: new RouteMap[]{
+	//												new RouteMap("DealsTab"),
+	//												new RouteMap("FavoritesTab", IsDefault: true)
+	//											}),
+	//									new RouteMap("DealsProduct",
+	//											View: views.FindByViewModel<ChefsProductDetailsViewModel>(),
+	//											DependsOn:"Deals"),
+	//									new RouteMap("Products",
+	//											View: views.FindByViewModel<ChefsProductsViewModel>()),
+	//									new RouteMap("Product",
+	//											View: views.FindByViewModel<ChefsProductDetailsViewModel>(),
+	//											DependsOn:"Products"),
+
+	//									new RouteMap("Profile", View: views.FindByViewModel<ChefsProfileViewModel>())
+	//								}),
+	//						new RouteMap("Settings", View: views.FindByViewModel<ChefsSettingsViewModel>())
+	//					}));
+	//}
+}
+
+
---
+++ testing/TestHarness/TestHarness.Shared/Ext/Navigation/Apps/Chefs/ChefsMainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace TestHarness.Ext.Navigation.Apps.Chefs;
+
+[TestSectionRoot("Sample App: Chefs", TestSections.Apps_Chefs, typeof(ChefsHostInit))]
+public sealed partial class ChefsMainPage : BaseTestSectionPage
+{
+	public ChefsMainPage()
+	{
+		this.InitializeComponent();
+	}
+
+	public async void ShowAppClick(object sender, RoutedEventArgs e)
+	{
+		await Navigator.NavigateRouteAsync(this, "");
+	}
+	public async void NarrowClick(object sender, RoutedEventArgs e)
+	{
+		VisualStateManager.GoToState(this, nameof(NarrowWindow), true);
+	}
+
+	public async void WideClick(object sender, RoutedEventArgs e)
+	{
+		VisualStateManager.GoToState(this, nameof(WideWindow), true);
+	}
+}
