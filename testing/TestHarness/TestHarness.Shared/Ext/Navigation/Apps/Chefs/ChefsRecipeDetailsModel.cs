7add2917987cc953eff919edcc8a7ef8d5c3afb5
eriklimakc <erik.lima@knowcode.app>
2024-07-03 17:58:55 +0100

    chore: Add LiveCooking navigation

--- testing/TestHarness/TestHarness.Shared/Ext/Navigation/Apps/Chefs/ChefsRecipeDetailsModel.cs
+++ testing/TestHarness/TestHarness.Shared/Ext/Navigation/Apps/Chefs/ChefsRecipeDetailsModel.cs
@@ -1,8 +1,25 @@
-﻿using TestHarness.Ext.Navigation.Apps.Chefs.Models;
+﻿using System.Collections.Immutable;
+using TestHarness.Ext.Navigation.Apps.Chefs.Models;
 
 namespace TestHarness.Ext.Navigation.Apps.Chefs;
 
-public partial class ChefsRecipeDetailsModel(ChefsRecipe recipe)
+public partial class ChefsRecipeDetailsModel(ChefsRecipe recipe, INavigator navigator)
 {
 	public ChefsRecipe Recipe { get; } = recipe;
+
+	private readonly INavigator _navigator = navigator;
+
+	public async ValueTask LiveCooking()
+	{
+		var route = _navigator?.Route?.Base switch
+		{
+			"ChefsRecipeDetails" => "ChefsLiveCooking",
+			"ChefsSearchRecipeDetails" => "ChefsSearchLiveCooking",
+			"ChefsFavoriteRecipeDetails" => "ChefsFavoriteLiveCooking",
+			"ChefsCookbookRecipeDetails" => "ChefsCookbookLiveCooking",
+			_ => throw new InvalidOperationException("Navigating from unknown route")
+		};
+
+		await _navigator.NavigateRouteAsync(this, route, data: new ChefsLiveCookingParameter(Recipe, ImmutableList<ChefsStep>.Empty));
+	}
 }
