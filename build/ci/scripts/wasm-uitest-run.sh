c8f6d5019fc885ed473fcf351e182b49cb9a21e6
Nick Randolph <nick@builttoroam.com>
2024-07-31 12:01:35 +1000

    chore: Switch to Debug

--- build/ci/scripts/wasm-uitest-run.sh
+++ build/ci/scripts/wasm-uitest-run.sh
@@ -13,15 +13,15 @@ export UNO_UITEST_PROJECT=$BUILD_SOURCESDIRECTORY/testing/TestHarness/TestHarnes
 export UNO_UITEST_LOGFILE=$BUILD_ARTIFACTSTAGINGDIRECTORY/screenshots/wasm/nunit-log.txt
 export UNO_UITEST_WASM_SOLUTION=$BUILD_SOURCESDIRECTORY/testing/TestHarness/TestHarness.sln
 export UNO_UITEST_WASM_PROJECT=$BUILD_SOURCESDIRECTORY/testing/TestHarness/TestHarness/TestHarness.csproj
-export UNO_UITEST_WASM_OUTPUT_PATH=$BUILD_SOURCESDIRECTORY/testing/TestHarness/TestHarness/bin/Release/net8.0-browserwasm/dist/
+export UNO_UITEST_WASM_OUTPUT_PATH=$BUILD_SOURCESDIRECTORY/testing/TestHarness/TestHarness/bin/Debug/net8.0-browserwasm/dist/
 export UNO_UITEST_WASM_PROJECT_BUILD_OPTIONS=" /p:Build_Android=false /p:Build_iOS=false /p:Build_Windows=false /p:Build_Desktop=false /p:GeneratePackageOnBuild=false"
 
 cd $BUILD_SOURCESDIRECTORY
 
-dotnet build -c Release $UNO_UITEST_WASM_SOLUTION /p:IsUiAutomationMappingEnabled=True /p:UseWebAssemblyAOT=false /p:Build_MacCatalyst=false /p:Build_Android=false /p:Build_iOS=false /p:Build_Windows=false /p:Build_Desktop=false /p:GeneratePackageOnBuild=false /bl:$BIN_LOG_PATH
+dotnet build -c Debug $UNO_UITEST_WASM_SOLUTION /p:IsUiAutomationMappingEnabled=True /p:UseWebAssemblyAOT=false /p:Build_MacCatalyst=false /p:Build_Android=false /p:Build_iOS=false /p:Build_Windows=false /p:Build_Desktop=false /p:GeneratePackageOnBuild=false /bl:$BIN_LOG_PATH
 
 # Start the server
-dotnet run --project $UNO_UITEST_WASM_PROJECT -f net8.0-browserwasm /p:Build_MacCatalyst=false /p:Build_Android=false /p:Build_iOS=false /p:Build_Windows=false /p:Build_Desktop=false  -c Release --no-build &
+dotnet run --project $UNO_UITEST_WASM_PROJECT -f net8.0-browserwasm /p:Build_MacCatalyst=false /p:Build_Android=false /p:Build_iOS=false /p:Build_Windows=false /p:Build_Desktop=false  -c Debug --no-build &
 
 cd $BUILD_SOURCESDIRECTORY/build
 
@@ -34,7 +34,7 @@ cd $UNO_UITEST_PROJECT
 
 ## Run the tests
 dotnet test \
-	-c Release \
+	-c Debug \
 	-l:"console;verbosity=normal" \
 	--logger "nunit;LogFileName=$BUILD_SOURCESDIRECTORY/build/TestResult.xml" \
 	-v m \
