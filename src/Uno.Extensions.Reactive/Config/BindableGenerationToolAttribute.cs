780e8d15846aaf8e52823e8a0d6adc937b97ae8d
Andres Pineda <pineda.andres@gmail.com>
2024-08-22 16:02:57 -0400

    fix(mvux): update default bindable generation tool version

--- src/Uno.Extensions.Reactive/Config/BindableGenerationToolAttribute.cs
+++ src/Uno.Extensions.Reactive/Config/BindableGenerationToolAttribute.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 namespace Uno.Extensions.Reactive.Config;
 
@@ -9,9 +8,21 @@ namespace Uno.Extensions.Reactive.Config;
 [AttributeUsage(AttributeTargets.Assembly)]
 public class BindableGenerationToolAttribute : Attribute
 {
+	// Latest version is 3, keeping version 2 as default for backward compatibility
+	private const int DefaultVersion = 2;
+
+	/// <summary>
+	/// Creates a new BindableGenerationTool object.
+	/// </summary>
+	/// <param name="version">The version of the tool to use when generating the bindables</param>
+	public BindableGenerationToolAttribute(int version = DefaultVersion)
+	{
+		Version = version;
+	}
+
 	/// <summary>
 	/// Gets or sets the version of tool that should be used to generate bindables.
 	/// </summary>
 	/// <remarks>Set this to 1 to use code gen used in Uno.Extensions.Reactive versions below 2.3</remarks>
-	public int Version { get; init; } = 3;
+	public int Version { get; init; }
 }
