23982525e7c5b4f8603be7407d29449ac2b58b8a
Matthew Rajala <matthew.rajala@polymtl.ca>
2024-09-13 16:01:43 -0400

    chore: start UI test

---
+++ testing/TestHarness/TestHarness.UITest/Ext/Navigation/Apps/Chefs/Given_Apps_Chefs.cs
@@ -0,0 +1,15 @@
+﻿namespace TestHarness.UITest;
+
+public class Given_Apps_Chefs : NavigationTestBase
+{
+	[Test]
+	public async Task When_Chefs_FavoriteRecipes_RecipeDetails()
+	{
+		InitTestSection(TestSections.Apps_Chefs);
+
+		App.WaitThenTap("ShowAppButton");
+		App.WaitThenTap("NextButton");
+		App.WaitElement("LoginNavigationBar");
+		App.WaitThenTap("LoginButton");
+	}
+}
