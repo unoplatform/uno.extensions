99fde1b019055b6e5ed23d05ce255c11a5fc8f77
eriklimakc <erik.lima@knowcode.app>
2024-09-18 18:24:11 +0100

    chore: reset to aafe6327cbfc047e78ccd7ae2463526427b27192 version

--- build/ci/scripts/android-uitest-run.sh
+++ build/ci/scripts/android-uitest-run.sh
@@ -6,7 +6,6 @@ IFS=$'\n\t'
 set -x
 
 export UNO_UITEST_SCREENSHOT_PATH=$BUILD_ARTIFACTSTAGINGDIRECTORY/screenshots/android
-export UNO_UITEST_SCREENSHOT_TOCOPY_PATH=$BUILD_SOURCESDIRECTORY/testing/TestHarness/TestHarness.UITest/bin/Release/net8.0
 export UNO_UITEST_PLATFORM=Android
 export UNO_UITEST_ANDROIDAPK_PATH=$BUILD_SOURCESDIRECTORY/testing/TestHarness/TestHarness/bin/Release/net8.0-android/android-x64/com.companyname.TestHarness-Signed.apk
 export UNO_UITEST_PROJECT=$BUILD_SOURCESDIRECTORY/testing/TestHarness/TestHarness.UITest
@@ -83,9 +82,3 @@ dotnet test \
 	--blame-hang-timeout $UITEST_TEST_TIMEOUT \
 	-v m \
 	|| true
-
-cd $BUILD_SOURCESDIRECTORY/build
-
-mkdir -p $UNO_UITEST_SCREENSHOT_PATH/screenshots
-
-cp $UNO_UITEST_SCREENSHOT_TOCOPY_PATH $UNO_UITEST_SCREENSHOT_PATH/screenshots
