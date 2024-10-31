0f87e408c53a8634af19fc643d0772eaae69e729
Nick Randolph <nick@builttoroam.com>
2024-10-31 23:51:54 +1100

    fix: Adjusting activation to occur once the content loads

---
+++ src/Uno.Extensions.Navigation.UI/WindowExtensions.cs
@@ -0,0 +1,34 @@
+﻿namespace Uno.Extensions;
+
+/// <summary>
+/// Extension methods on <see cref="Window" />.
+/// </summary>
+public static class WindowExtensions
+{
+	/// <summary>
+	/// Activates the window when the content is ready
+	/// </summary>
+	/// <param name="window">The <see cref="Window" /> to activate</param>
+	public static void ActivateWhenReady(this Window window)
+	{
+
+		if (window.Content is FrameworkElement content)
+		{
+			if (content.IsLoaded)
+			{
+				// Fallback to make sure the window is activated
+				window.Activate();
+			}
+			else
+			{
+				content.Loaded += (_, _) => window.Activate();
+				content.SizeChanged += (_, _) => window.Activate();
+				content.Loading += (_, _) => window.Activate();
+			}
+		}
+		else
+		{
+			window.Activate();
+		}
+	}
+}
