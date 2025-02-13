95f39129f1f1e6b2eaf203175c72e29c4d171f21
Érik Lima <114886335+eriklimakc@users.noreply.github.com>
2025-02-13 15:25:36 +0000

    test: Add UI test for NavView and TabBar sending Data

---
+++ testing/TestHarness/TestHarness.UITest/Ext/Navigation/Apps/Regions/Given_Apps_Regions.cs
@@ -0,0 +1,48 @@
+﻿namespace TestHarness.UITest;
+
+public class Given_Apps_Regions : NavigationTestBase
+{
+	[Test]
+	public async Task When_Regions_Send_Data_NavView()
+	{
+		InitTestSection(TestSections.Apps_Regions);
+
+		App.WaitThenTap("ShowAppButton");
+
+		App.WaitElement("RegionsHomePageTextBox");
+
+		var textToSet = "Hello, World!";
+
+		App.SetText("RegionsHomePageTextBox", textToSet);
+
+		App.WaitThenTap("RegionsHomePageThirdPage");
+
+		App.WaitElement("RegionsThirdPageTextBock");
+
+		var textFromTb = App.GetText("RegionsThirdPageTextBock");
+
+		Assert.AreEqual(textToSet, textFromTb);
+	}
+
+	[Test]
+	public async Task When_Regions_Send_Data_TabBar()
+	{
+		InitTestSection(TestSections.Apps_Regions);
+
+		App.WaitThenTap("ShowAppButton");
+
+		App.WaitThenTap("RegionsHomePageRegionsTbData");
+
+		App.WaitThenTap("RegionsTbDataPageTabOne");
+
+		var textToSet = "Hello, World!";
+
+		App.SetText("RegionsFirstTbiDataPageTextBox", textToSet);
+
+		App.WaitThenTap("RegionsTbDataPageTabTwo");
+
+		var textFromTb = App.GetText("RegionsSecondTbiDataPageTextBox");
+
+		Assert.AreEqual(textToSet, textFromTb);
+	}
+}
