9c9c7783eb8402c0cb5fe68ed6f5112a4522d091
DevTKSS <info@technische-konstruktion.com>
2025-05-02 17:19:25 +0200

    chore: add xml documentation to public members

--- src/Uno.Extensions.Storage/KeyValueStorage/KeyValueStorageConfigurationExtensions.cs
+++ src/Uno.Extensions.Storage/KeyValueStorage/KeyValueStorageConfigurationExtensions.cs
@@ -2,6 +2,23 @@
 
 internal static class KeyValueStorageConfigurationExtensions
 {
+	/// <summary>
+	/// Retrieves the <see cref="KeyValueStorageSettings"/> associated with the specified name
+	/// from the given <see cref="KeyValueStorageConfiguration"/>. If no settings are found
+	/// for the specified name, the default settings are returned.
+	/// </summary>
+	/// <param name="config">
+	/// The <see cref="KeyValueStorageConfiguration"/> instance to retrieve the settings from.
+	/// This parameter can be null.
+	/// </param>
+	/// <param name="name">
+	/// The name of the settings to retrieve.
+	/// </param>
+	/// <returns>
+	/// The <see cref="KeyValueStorageSettings"/> associated with the specified name, or
+	/// the default settings if no match is found.
+	/// </returns>
+
 	public static KeyValueStorageSettings GetSettingsOrDefault(this KeyValueStorageConfiguration? config, string name)
 	{
 		if (config?.Providers.TryGetValue(name, out var settings) ?? false)
