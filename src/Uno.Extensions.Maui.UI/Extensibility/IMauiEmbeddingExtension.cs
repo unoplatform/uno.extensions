3cbf46c578a05836ff02b80db571299594663d67
Steve Bilogan <steve.bilogan@gmail.com>
2025-06-12 17:13:55 -0400

    fix: Maui - don't throw for non supported platforms
    (cherry picked from commit 6a4cd8da5195effbc34a862b3b9b6b107747a76c)

--- src/Uno.Extensions.Maui.UI/Extensibility/IMauiEmbeddingExtension.cs
+++ src/Uno.Extensions.Maui.UI/Extensibility/IMauiEmbeddingExtension.cs
@@ -18,7 +18,7 @@ public interface IMauiEmbeddingExtension
 	/// Initializes the Maui embedding extension with the specified application.  
 	/// </summary>  
 	/// <param name="iApp">The application to initialize.</param>  
-	void Initialize(IApplication iApp) => throw new PlatformNotSupportedException();
+	void Initialize(IApplication iApp) { }
 
 	/// <summary>  
 	/// Registers platform-specific services for the Maui application builder.  
@@ -26,14 +26,14 @@ public interface IMauiEmbeddingExtension
 	/// <param name="builder">The Maui application builder.</param>  
 	/// <param name="app">The application instance.</param>  
 	/// <returns>The updated Maui application builder.</returns>  
-	MauiAppBuilder RegisterPlatformServices(MauiAppBuilder builder, Application app) => throw new PlatformNotSupportedException();
+	MauiAppBuilder RegisterPlatformServices(MauiAppBuilder builder, Application app) => builder;
 
 	/// <summary>  
 	/// Initializes the Maui embedding application with the specified parameters.  
 	/// </summary>  
 	/// <param name="mauiApp">The Maui application instance.</param>  
 	/// <param name="app">The application instance.</param>  
-	void InitializeMauiEmbeddingApp(MauiApp mauiApp, Application app) => throw new PlatformNotSupportedException();
+	void InitializeMauiEmbeddingApp(MauiApp mauiApp, Application app) { }
 
 	/// <summary>  
 	/// Builds the Maui application using the specified builder, application, and window.  
@@ -42,18 +42,18 @@ public interface IMauiEmbeddingExtension
 	/// <param name="app">The application instance.</param>  
 	/// <param name="window">The platform-specific window instance.</param>  
 	/// <returns>The built Maui application.</returns>  
-	MauiApp BuildMauiApp(MauiAppBuilder builder, Application app, Microsoft.UI.Xaml.Window window) => throw new PlatformNotSupportedException();
+	MauiApp BuildMauiApp(MauiAppBuilder builder, Application app, Microsoft.UI.Xaml.Window window) => default!;
 
 	/// <summary>  
 	/// Handles changes to the source of a dependency object.  
 	/// </summary>  
 	/// <param name="dependencyObject">The dependency object whose source has changed.</param>  
 	/// <param name="args">The event arguments containing details of the change.</param>  
-	void OnSourceChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args) => throw new PlatformNotSupportedException();
+	void OnSourceChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args) { }
 
 	/// <summary>  
 	/// Handles size changes for a visual element.  
 	/// </summary>  
 	/// <param name="element">The visual element whose size has changed.</param>  
-	void OnSizeChanged(VisualElement? element) => throw new PlatformNotSupportedException();
+	void OnSizeChanged(VisualElement? element) { }
 }
