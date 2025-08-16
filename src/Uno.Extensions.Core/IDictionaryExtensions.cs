b88cf9caf10f4f13438a3a477265bae0345e96d2
DevTKSS <info@technische-konstruktion.com>
2025-08-16 19:55:51 +0200

    feat(DictionaryExtension): AddOrReplace extension for IDictionary
    This is good to avoid eventually not existing Keys and get rid of duplicated logic for existance checks everywhere

---
+++ src/Uno.Extensions.Core/IDictionaryExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+
+namespace Uno.Extensions;
+/// <summary>
+/// Extension methods for the <see cref="IDictionary"/>
+/// </summary>
+public static class IDictionaryExtensions
+{
+	/// <summary>
+	/// Adds or replaces the element to the <see cref="IDictionary"/> instance.
+	/// </summary>
+	/// <typeparam name="TKey"> The type of the key</typeparam>
+	/// <typeparam name="TValue">The type of the value</typeparam>
+	/// <param name="dictionary">The dictionary to add to.</param>
+	/// <param name="key">The key parameter.</param>
+	/// <param name="value">The value to add.</param>
+	/// <returns>The previous value or its default, possibily <see langword="null"/> value.</returns>
+	public static TValue? AddOrReplace<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
+	{
+		if (!dictionary.TryAdd(key, value))
+		{
+			var oldValue = dictionary[key];
+			dictionary[key] = value;
+			return oldValue;
+		}
+		return default(TValue);
+	}
+
+}
